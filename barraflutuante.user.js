efeb09f7037ff347288d5c2824fc7a32a06a6d7b
Paulo <paulo@nadameu.com.br>
2016-10-11 19:13:37 -0300

    Correção de problemas na tela de marcação de audiência com videoconferência

--- barraflutuante.user.js
+++ barraflutuante.user.js
@@ -5,21 +5,24 @@
 // @include     https://eproc.jfsc.jus.br/eprocV2/*
 // @include     https://eproc.jfrs.jus.br/eprocV2/*
 // @include     https://eproc.trf4.jus.br/eproc2trf4/*
-// @version     4
+// @version     5
 // @grant       none
 // ==/UserScript==
 
 var $ = window.jQuery;
 
 $('head').append($('<style type="text/css"></style>').html([
-  '#divInfraAreaGlobal { position: absolute; margin: 0 2.5%; }',
   '#divInfraBarraTribunal { position: fixed; top: 8px; width: 95%; z-index: 1000; }'
 ].join(' ')));
 
 function onResize() {
   var alturaBarra = $('#divInfraBarraTribunal').height();
   var alturaBordaBarra = 2;
-  $('#divInfraAreaGlobal').css('padding-top', (alturaBarra + alturaBordaBarra) + 'px');
+  $('#divInfraAreaGlobal').css({
+    'position': 'absolute',
+    'margin': '0 2.5%',
+    'padding-top': (alturaBarra + alturaBordaBarra) + 'px'
+  });
 }
 
 $(window).on('load', onResize);
