168172b7516b63372190e1dddee095d2f3762ce6
Paulo <paulo@nadameu.com.br>
2017-01-24 14:21:16 -0300

    Melhorias no código

--- pi.user.js
+++ pi.user.js
@@ -3,22 +3,23 @@
 // @namespace   http://nadameu.com.br/pi
 // @description Altera a imagem de fundo do e-Proc V2
 // @include     /^https:\/\/eproc\.(jf(pr|rs|sc)|trf4)\.jus\.br\/eproc(V2|2trf4)\//
-// @version     3
+// @version     4
 // @grant       none
 // ==/UserScript==
 
-var texto = 'π';
-var side = 48;
-var fontSize = side * 0.75;
-var rotation = Math.PI / 4
-var bgColor = $(document.body).css('background-color');
-var [trash, r, g, b] = bgColor.match(/rgb\((\d+), (\d+), (\d+)\)/);
-var bgColorAlpha = 'rgba(' + [r, g, b].join(', ') + ', 0.5)';
-var ratio = 0.95;
-var fgColor = 'rgb(' + [Math.round(r * ratio), Math.round(g * ratio), Math.round(b * ratio)].join(', ') + ')';
+const texto = 'π';
+const side = 48;
+const fontSize = side * 0.75;
+const rotation = Math.PI / 4
+const bgColor = $(document.body).css('background-color');
+const [, r, g, b] = bgColor.match(/rgb\((\d+), (\d+), (\d+)\)/);
+const components = [r,g,b];
+const bgColorAlpha = 'rgba(' + components.join(', ') + ', 0.5)';
+const ratio = 0.95;
+const fgColor = 'rgb(' + components.map(color => Math.round(color * ratio)).join(', ') + ')';
 
-var canvas = $('<canvas width="' + side + '" height="' + side + '" style="border: 2px solid white;"></canvas>').get(0);
-var context = canvas.getContext('2d');
+const canvas = $(`<canvas width="${side}" height="${side}"></canvas>`).get(0);
+const context = canvas.getContext('2d');
 
 function putText(x, y, rotation) {
   context.translate(x, y);
@@ -39,10 +40,10 @@ putText(side * 0/4, side * 2/4, +rotation);
 putText(side * 4/4, side * 2/4, +rotation);
 putText(side * 2/4, side * 4/4, -rotation);
 
-var dataURL = canvas.toDataURL();
-var style = $('<style></style>').html([
-  'body { background-image: url(\'' + dataURL + '\'); }',
+const dataURL = canvas.toDataURL();
+const style = $('<style></style>').html([
+  `body { background-image: url('${dataURL}'); }`,
   'div.infraAreaTelaD, div.infraBarraComandos, div.infraAreaDados, div.infraAviso { border-color: transparent; }',
-  'div.infraAreaTelaD { background-color: ' + bgColorAlpha + '; }'
+  `div.infraAreaTelaD { background-color: ${bgColorAlpha}; }`
 ].join('\n'));
 $('head').append(style);
