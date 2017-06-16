f306c16579318bad496f150a5301d6dd23873a97
Paulo <paulo@nadameu.com.br>
2017-06-16 17:18:29 -0300

    Simplificação

--- achei.user.js
+++ achei.user.js
@@ -4,213 +4,100 @@
 // @description Link para informações da Intra na página do Achei!
 // @include     http://centralrh.trf4.gov.br/achei/pesquisar.php?acao=pesquisar*
 // @include     http://serh.trf4.jus.br/achei/pesquisar.php?acao=pesquisar*
-// @version     5
+// @version     6
 // @grant       none
 // ==/UserScript==
 
-Array.prototype.chain = function(f) {
-	return Array.concat(...this.map(f));
-};
-Array.prototype.traverse = function(T, f) {
-	return this.reduce(
-		(acc, x) =>
-			liftA2(arr => y => arr.concat(y), acc, f(x)),
-		T.of([])
-	);
-};
-
-function Monad() {
-	throw new TypeError('Not to be called directly');
-}
-Monad.prototype = {
-	ap(a) { return a.chain(f => this.map(f)); },
-	chain() { throw new TypeError('Not implemented'); },
-	map(f) { return this.chain(x => (this.of || this.constructor.of || console.error(this))(f(x))); }
-};
-Monad.extend = (constructor, prototype = {}, static = {}) => {
-	Object.assign(constructor,
-		{
-			prototype: Object.assign(Object.create(Monad.prototype),
-				{ constructor },
-				prototype
-			),
-			of() { throw new TypeError('Not implemented'); }
+const Task = (() => {
+	function Task(fork) {
+		if (! (this instanceof Task)) return new Task(fork);
+		this.fork = fork;
+	}
+	Task.prototype = {
+		constructor: Task,
+		ap(a) {
+			return Task.all([a, this]).map(([f, x]) => f(x));
 		},
-		static
-	);
-};
-Monad.extendSum = (main, constructor, prototype) => {
-	Object.assign(constructor,
-		{
-			prototype: Object.assign(Object.create(main.prototype),
-				{
-					constructor,
-					of: main.of
-				},
-				prototype
-			)
-		}
-	);
-};
-
-function IO(f) {
-	if (! (this instanceof IO)) return new IO(f);
-	this.performUnsafeIO = f;
-}
-Monad.extend(IO,
-	{ chain(f) {
-		return IO(() =>
-			f(this.performUnsafeIO()).performUnsafeIO());
-	} },
-	{ of: x => IO(() => x) }
-);
-
-function Identity(x) {
-	if (! (this instanceof Identity)) return new Identity(x);
-	this.x = x;
-}
-Monad.extend(Identity,
-	{
-		chain(f) { return f(this.x); },
-		fold(f) { return f(this.x); },
-		traverse(T, f) { return T.of(this.x).chain(f); }
-	},
-	{ of: Identity }
-);
-
-function Maybe() {
-	throw new TypeError('Not to be called directly');
-}
-function Just(x) {
-	if (! (this instanceof Just)) return new Just(x);
-	this.x = x;
-}
-function Nothing() {
-	if (! (this instanceof Nothing)) return new Nothing();
-}
-Monad.extend(Maybe, {}, { Just, Nothing, of: Just });
-Monad.extendSum(Maybe, Just,
-	{
-		chain(f) { return f(this.x); },
-		fold(f) { return f(this.x); }
-	}
-);
-Monad.extendSum(Maybe, Nothing, {
-	chain() { return this; }
-});
-
-function Task(fork) {
-	if (! (this instanceof Task)) return new Task(fork);
-	this.fork = fork;
-}
-Monad.extend(Task, {
-	chain(f) { return Task((rej, res) => this.fork(rej, x => f(x).fork(rej, res))); }
-}, {
-	of: x => Task((rej, res) => res(x)),
-	rejected: e => Task(rej => rej(e))
-});
-
-const id = i => i;
-const liftA2 = (f, a, b) => b.ap(a.map(f));
-const fromNullable = x => x == null ? Nothing() : Just(x);
-const tryCatchIO = io => Task((rej, res) => {
-	try {
-		return res(io.performUnsafeIO());
-	} catch (e) {
-		return rej(e);
-	}
-});
-
-const taskify = gen => {
-	const translate = x => {
-		switch (x.constructor.name) {
-			case 'Array': return x.traverse(Task, id);
-			case 'IO': return tryCatchIO(x);
-			case 'Identity': return Task.of(x.fold(id));
-			case 'Just': return Task.of(x.fold(id));
-			case 'Nothing': return Task.rejected('Maybe failed');
-			case 'Task': return x;
-			default: return Task.rejected(['Could not convert:', x]);
+		chain(f) {
+			return Task((rej, res) => this.fork(rej, x => f(x).fork(rej, res)));
+		},
+		map(f) {
+			return Task((rej, res) => this.fork(rej, x => res(f(x))));
 		}
 	};
-
-	return Task((rej, res) => {
-		const g = gen();
-		const step = x => {
-			const yielded = g.next(x);
-			if (yielded.done) return res(yielded.value);
-			return translate(yielded.value).fork(rej, step);
+	Task.all = ts => Task((rej, res) => {
+		const state = new Array(ts.length);
+		const go = i => x => {
+			state[i] = x;
+			if (Array.from(state.keys()).every(i => i in state)) res(state);
 		};
-		return step();
+		ts.forEach((t, i) => t.fork(rej, go(i)));
 	});
-};
+	Task.of = x => Task((rej, res) => res(x));
+	Task.rejected = e => Task(rej => rej(e));
 
-const main = function *() {
-	const form = yield yield obterFormulario();
+	return Task;
+})();
 
-	const urlSigla = yield Maybe.of(form)
-		.chain(localFormulario)
-		.chain(dominioLocal)
-		.map(urlDominioSigla);
+const main = function() {
+	const form = Task.of(document)
+		.map(doc => doc.querySelector('form[name="formulario"]'))
+		.chain(fromNullable('Formulário não encontrado'));
 
-	const nodes = Identity.of(form)
-		.fold(nodesNextFormulario)
-		.filter(contemSigla);
-
-	const adicionarLinkFactory = node =>
-		IO.of(node)
-			.map(siglaNode)
-			.map(urlSigla)
-			.chain(linkUrl)
-			.chain(envolverEmColchetes);
-
-	const adicionarLink = node => adicionarLinkFactory(node).map(inserirApos(node));
-
-	const transformed = yield nodes.traverse(IO, adicionarLink);
-
-	return `${transformed.length} link(s) adicionado(s).`;
-};
-
-const obterFormulario = () =>
-	IO(() =>
-		fromNullable(document.querySelector('form[name="formulario"]'))
-	);
-const localFormulario = form =>
-	fromNullable(form.local)
-	.chain(local => fromNullable(local.value));
-const dominioLocal = local =>
-	Maybe.of(Number(local))
+	const urlSigla = form
+		.map(f => f.local || {})
+		.map(l => l.value)
+		.chain(fromNullable('Não foi possível obter o local'))
+		.map(Number)
 		.map(i => i - 1)
 		.map(i => ['trf4', 'jfrs', 'jfsc', 'jfpr'][i])
-		.chain(fromNullable)
-		.map(s => `${s}.jus.br`);
-const nodesNextFormulario = form => {
-	const Any = x => ({
-		x,
-		concat: ({ x: y }) => Any(x || y)
-	});
-	Any.empty = () => Any(false);
-	const AfterFound = (found, list) => ({
-		found,
-		list,
-		concat: ({ found: f2, list: l2 }) => AfterFound(found.concat(f2), found.x ? list.concat(l2) : list)
-	});
-	AfterFound.empty = () => AfterFound(Any.empty(), []);
+		.chain(fromNullable('Não foi possível estabelecer o domínio'))
+		.map(s => `${s}.jus.br`)
+		.map(urlDominioSigla);
 
-	const afterFound = form => reducer => (acc, x) => reducer(acc, AfterFound(Any(x === form), [x]));
-	const initial = AfterFound.empty();
-	const reducer = (acc, x) => acc.concat(x);
-	const nodes = siblings(form).reduce(afterFound(form)(reducer), initial).list;
+	const nodes = form
+		.map(nodesNextFormulario)
+		.map(nodes => nodes
+			.filter(contemSigla)
+			.map(node => Task.of(node)
+				.map(siglaNode)
+				.ap(urlSigla)
+				.map(linkUrl)
+				.map(envolverEmColchetes)
+				.map(inserirApos(node))
+				.map(() => node)
+			)
+		)
+		.chain(Task.all);
+
+	nodes.fork(
+		err => console.error(err),
+		nodes => console.log(`${nodes.length} link(s) adicionado(s).`)
+	);
+};
+
+const fromNullable = errorMsg => x =>
+	x == null ?
+		Task.rejected(new Error(errorMsg)) :
+		Task.of(x);
+
+const nodesNextFormulario = form => {
+	const nodes = siblings(form).reduce(({ found, list }, node) => ({
+		found: found || node === form,
+		list: found ? list.concat(node) : list
+	}), { found: false, list: [] }).list;
 	const tables = nodes.filter(isTable);
+
 	return tables.length === 0 ?
 		nodes :
-		tables.reduce((acc, x) => reducer(acc,
-			Array.from(x.querySelectorAll('td:nth-child(2)'))
-				.chain(c => [...c.childNodes])
-		), []);
+		Array.concat(...tables.map(childNodes));
 };
+
 const siblings = element => Array.from(element.parentNode.childNodes);
 const isTable = x => 'tagName' in x && (/^table$/i).test(x.tagName);
+const childNodes = table =>
+	Array.from(table.querySelector('td:nth-child(2)').childNodes);
+
 const { isSigla, siglaTexto } = (() => {
 	const re = /^\s*Sigla:\s*(\w*)\s*$/;
 	return {
@@ -218,40 +105,35 @@ const { isSigla, siglaTexto } = (() => {
 		siglaTexto: x => x.match(re)[1]
 	};
 })();
+
 const contemSigla = node => isSigla(node.textContent);
 const urlDominioSigla = dominio => sigla =>
-	liftA2(
-		sigla => dominio => `https://intra.trf4.jus.br/membros/${sigla}${dominio}`,
-		Identity.of(sigla).map(s => s.toLowerCase()),
-		Identity.of(dominio).map(d => d.replace(/\./g, '-'))
-	).fold(id);
-const siglaNode = node =>
-	Identity.of(node.textContent)
-		.fold(siglaTexto);
-const linkUrl = url =>
-	IO(() =>
-		Identity.of(document.createElement('a'))
-			.fold(l => Object.assign(l, {
-				href: url,
-				target: '_blank',
-				textContent: 'Abrir na Intra'
-			}))
-	);
-const envolverEmColchetes = link =>
-	IO(() =>
-		[
-			createText(' [ '),
-			link,
-			createText(' ]')
-		].reduce((acc, x) => {
-			acc.appendChild(x);
-			return acc;
-		}, document.createDocumentFragment())
-	);
-const createText = text => document.createTextNode(text);
-const inserirApos = antigo => novo => antigo.parentElement.insertBefore(novo, antigo.nextSibling);
+	[
+		'https://intra.trf4.jus.br/membros/',
+		sigla.toLowerCase(),
+		dominio.replace(/\./g, '-')
+	].join('');
 
-taskify(main).fork(
-	e => console.error(e),
-	x => console.log(x)
-);
+const siglaNode = node => siglaTexto(node.textContent);
+const linkUrl = url =>
+	Object.assign(document.createElement('a'), {
+		href: url,
+		target: '_blank',
+		textContent: 'Abrir na Intra'
+	});
+
+const envolverEmColchetes = link =>
+	[
+		createText(' [ '),
+		link,
+		createText(' ]')
+	].reduce((acc, x) => {
+		acc.appendChild(x);
+		return acc;
+	}, document.createDocumentFragment());
+
+const createText = text => document.createTextNode(text);
+const inserirApos = antigo => novo =>
+	antigo.parentElement.insertBefore(novo, antigo.nextSibling);
+
+main();
