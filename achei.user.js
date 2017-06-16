a8f291265e693310a73746405677c4b82eb15f12
Paulo <paulo@nadameu.com.br>
2017-06-16 17:51:42 -0300

    Separando dependências externas

--- achei.user.js
+++ achei.user.js
@@ -4,40 +4,10 @@
 // @description Link para informações da Intra na página do Achei!
 // @include     http://centralrh.trf4.gov.br/achei/pesquisar.php?acao=pesquisar*
 // @include     http://serh.trf4.jus.br/achei/pesquisar.php?acao=pesquisar*
-// @version     6
+// @require     https://github.com/nadameu/greasemonkey/raw/Task-v1.0.0/lib/Task.js
+// @version     7
 // @grant       none
 // ==/UserScript==
-
-const Task = (() => {
-	function Task(fork) {
-		if (! (this instanceof Task)) return new Task(fork);
-		this.fork = fork;
-	}
-	Task.prototype = {
-		constructor: Task,
-		ap(a) {
-			return Task.all([a, this]).map(([f, x]) => f(x));
-		},
-		chain(f) {
-			return Task((rej, res) => this.fork(rej, x => f(x).fork(rej, res)));
-		},
-		map(f) {
-			return Task((rej, res) => this.fork(rej, x => res(f(x))));
-		}
-	};
-	Task.all = ts => Task((rej, res) => {
-		const state = new Array(ts.length);
-		const go = i => x => {
-			state[i] = x;
-			if (Array.from(state.keys()).every(i => i in state)) res(state);
-		};
-		ts.forEach((t, i) => t.fork(rej, go(i)));
-	});
-	Task.of = x => Task((rej, res) => res(x));
-	Task.rejected = e => Task(rej => rej(e));
-
-	return Task;
-})();
 
 const main = function() {
 	const form = Task.of(document)
