315c4a99ef22bb6c0b23e9a0b95b76271bc6740d
Paulo <paulo@nadameu.com.br>
2017-07-19 18:13:41 -0300

    Uso de objetos nativos Javascript

--- achei.user.js
+++ achei.user.js
@@ -4,106 +4,104 @@
 // @description Link para informações da Intra na página do Achei!
 // @include     http://centralrh.trf4.gov.br/achei/pesquisar.php?acao=pesquisar*
 // @include     http://serh.trf4.jus.br/achei/pesquisar.php?acao=pesquisar*
-// @require     https://github.com/nadameu/greasemonkey/raw/Task-v1.0.0/lib/Task.js
-// @version     7
+// @version     8
 // @grant       none
 // ==/UserScript==
 
-const main = function() {
-	const form = Task.of(document)
-		.map(doc => doc.querySelector('form[name="formulario"]'))
-		.chain(fromNullable('Formulário não encontrado'));
+Promise.prototype.ap = function(a) {
+	return Promise.all([this, a]).then(([x, f]) => f(x));
+};
+Promise.prototype.tap = function(f) {
+	return this.then(x => { f(x); return x; });
+};
 
-	const urlSigla = form
-		.map(f => f.local || {})
-		.map(l => l.value)
-		.chain(fromNullable('Não foi possível obter o local'))
-		.map(Number)
-		.map(i => i - 1)
-		.map(i => ['trf4', 'jfrs', 'jfsc', 'jfpr'][i])
-		.chain(fromNullable('Não foi possível estabelecer o domínio'))
-		.map(s => `${s}.jus.br`)
-		.map(urlDominioSigla);
+const doc = Promise.resolve(document);
 
-	const nodes = form
-		.map(nodesNextFormulario)
-		.map(nodes => nodes
-			.filter(contemSigla)
-			.map(node => Task.of(node)
-				.map(siglaNode)
-				.ap(urlSigla)
-				.map(linkUrl)
-				.map(envolverEmColchetes)
-				.map(inserirApos(node))
-				.map(() => node)
-			)
-		)
-		.chain(Task.all);
-
-	nodes.fork(
-		err => console.error(err),
-		nodes => console.log(`${nodes.length} link(s) adicionado(s).`)
+const createFragment = doc
+	.then(doc => doc.createElement('template'))
+	.tap(templ => templ.innerHTML = ` [ <a href="" target="_blank">Abrir na Intra</a> ]`)
+	.then(templ => url => doc
+		.then(doc => doc.importNode(templ.content, true))
+		.tap(frag => frag.querySelector('a').href = url)
 	);
-};
 
-const fromNullable = errorMsg => x =>
-	x == null ?
-		Task.rejected(new Error(errorMsg)) :
-		Task.of(x);
+const form = doc
+	.then(doc => doc.querySelector('form[name="formulario"]'))
+	.then(form => form == null ?
+		Promise.reject('Formulário não encontrado') :
+		Promise.resolve(form)
+	);
 
-const nodesNextFormulario = form => {
-	const nodes = siblings(form).reduce(({ found, list }, node) => ({
-		found: found || node === form,
-		list: found ? list.concat(node) : list
-	}), { found: false, list: [] }).list;
-	const tables = nodes.filter(isTable);
+const urlSigla = form
+	.then(form => form.local || {})
+	.then(elt => elt.value)
+	.then(local => local == null ?
+		Promise.reject('Local não disponível') :
+		Promise.resolve(local)
+	)
+	.then(Number)
+	.then(l => l - 1)
+	.then(l => ['trf4', 'jfrs', 'jfsc', 'jfpr'][l])
+	.then(dominio => dominio == null ?
+		Promise.reject('Local desconhecido') :
+		Promise.resolve(dominio)
+	)
+	.then(d => `${d}-jus-br`)
+	.then(dominio => sigla => Promise.resolve(sigla)
+		.then(sigla => sigla.toLowerCase())
+		.then(sigla => `https://intra.trf4.jus.br/membros/${sigla}${dominio}`)
+	);
 
-	return tables.length === 0 ?
-		nodes :
-		Array.concat(...tables.map(childNodes));
-};
+const main = form
+	.then(function *(form) {
+		let next = form.nextSibling;
+		while (next !== null) {
+			yield next;
+			next = next.nextSibling;
+		}
+		return next;
+	})
+	.then(function *(nodes) {
+		for (let node of nodes) {
+			if (/^table$/i.test(node.tagName)) {
+				yield *node.querySelector('td:nth-child(2)').childNodes;
+			}
+			yield node;
+		}
+	})
+	.then(function *(nodes) {
+		for (let node of nodes) {
+			const [maybeSigla] = [node]
+				.map(node => node.textContent)
+				.map(text => text.trim())
+				.map(text => text.match(/^Sigla:\s*(.+)$/) || [])
+				.map(match => match.slice(1));
 
-const siblings = element => Array.from(element.parentNode.childNodes);
-const isTable = x => 'tagName' in x && (/^table$/i).test(x.tagName);
-const childNodes = table =>
-	Array.from(table.querySelector('td:nth-child(2)').childNodes);
+			maybeSigla.forEach(sigla =>
+				Promise.resolve(sigla)
+					.ap(urlSigla)
+					.ap(createFragment)
+					.then(frag => node.parentNode.insertBefore(frag, node.nextSibling))
+					.then(() => 1)
+			);
 
-const { isSigla, siglaTexto } = (() => {
-	const re = /^\s*Sigla:\s*(\w*)\s*$/;
-	return {
-		isSigla: x => re.test(x),
-		siglaTexto: x => x.match(re)[1]
-	};
-})();
-
-const contemSigla = node => isSigla(node.textContent);
-const urlDominioSigla = dominio => sigla =>
-	[
-		'https://intra.trf4.jus.br/membros/',
-		sigla.toLowerCase(),
-		dominio.replace(/\./g, '-')
-	].join('');
-
-const siglaNode = node => siglaTexto(node.textContent);
-const linkUrl = url =>
-	Object.assign(document.createElement('a'), {
-		href: url,
-		target: '_blank',
-		textContent: 'Abrir na Intra'
+			if (maybeSigla.length > 0) yield 1;
+		}
+	})
+	.then(([...results]) => results.length)
+	.then(qtd => {
+		const s = qtd > 1 ? 's' : '';
+		return `${qtd} link${s} criado${s}.`;
 	});
 
-const envolverEmColchetes = link =>
-	[
-		createText(' [ '),
-		link,
-		createText(' ]')
-	].reduce((acc, x) => {
-		acc.appendChild(x);
-		return acc;
-	}, document.createDocumentFragment());
-
-const createText = text => document.createTextNode(text);
-const inserirApos = antigo => novo =>
-	antigo.parentElement.insertBefore(novo, antigo.nextSibling);
-
-main();
+console.time('main');
+main.then(
+	x => {
+		console.log('Resultado:', x);
+		console.timeEnd('main');
+	},
+	e => {
+		console.error('Erro:', e);
+		console.timeEnd('main');
+	}
+);
