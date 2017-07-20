429176f4e4ac6ae067fa97b978ad196d2ae9c238
Paulo <paulo@nadameu.com.br>
2017-07-20 15:17:02 -0300

    Retorno dos ADTs

--- achei.user.js
+++ achei.user.js
@@ -4,104 +4,146 @@
 // @description Link para informações da Intra na página do Achei!
 // @include     http://centralrh.trf4.gov.br/achei/pesquisar.php?acao=pesquisar*
 // @include     http://serh.trf4.jus.br/achei/pesquisar.php?acao=pesquisar*
-// @version     8
+// @require     https://github.com/nadameu/greasemonkey/raw/List-v1.0.0/lib/List.js
+// @require     https://github.com/nadameu/greasemonkey/raw/Task-v1.0.1/lib/Task.js
+// @version     9
 // @grant       none
 // ==/UserScript==
 
-Promise.prototype.ap = function(a) {
-	return Promise.all([this, a]).then(([x, f]) => f(x));
-};
-Promise.prototype.tap = function(f) {
-	return this.then(x => { f(x); return x; });
-};
+/* global List, Task */
 
-const doc = Promise.resolve(document);
+'use strict';
 
-const createFragment = doc
-	.then(doc => doc.createElement('template'))
-	.tap(templ => templ.innerHTML = ` [ <a href="" target="_blank">Abrir na Intra</a> ]`)
-	.then(templ => url => doc
-		.then(doc => doc.importNode(templ.content, true))
-		.tap(frag => frag.querySelector('a').href = url)
+console.time('total');
+console.time('setup');
+
+Function.prototype.compose = function(f) { return x => f(this(x)); };
+Function.prototype.composeK = function(f) { return x => this(x).chain(f); };
+Function.id = x => x;
+
+const doc = Task.of(document);
+
+const docToForm = Function
+	.id(doc => doc.querySelector('form[name="formulario"]'))
+	.compose(form => form == null ?
+		Task.rejected('Formulário não encontrado') :
+		Task.of(form)
 	);
 
-const form = doc
-	.then(doc => doc.querySelector('form[name="formulario"]'))
-	.then(form => form == null ?
-		Promise.reject('Formulário não encontrado') :
-		Promise.resolve(form)
+const form = doc.chain(docToForm);
+
+const formToLocal = Function
+	.id(form => form.local || {})
+	.compose(elt => elt.value)
+	.compose(local => local == null ?
+		Task.rejected('Local não disponível') :
+		Task.of(local)
 	);
+
+const localToDominio = Function
+	.id(Number)
+	.compose(l => l - 1)
+	.compose(l => ['trf4', 'jfrs', 'jfsc', 'jfpr'][l])
+	.compose(dominio => dominio == null ?
+		Task.rejected('Local desconhecido') :
+		Task.of(dominio)
+	);
+
+const dominioToUrlSigla = Function
+	.id(d => `${d}-jus-br`)
+	.compose(dominio => Task
+		.of(sigla => sigla.toLowerCase())
+		.map(f => x => `https://intra.trf4.jus.br/membros/${f(x)}${dominio}`)
+	);
+
+const formToUrlSigla = Function
+	.id(formToLocal)
+	.composeK(localToDominio)
+	.composeK(dominioToUrlSigla);
 
 const urlSigla = form
-	.then(form => form.local || {})
-	.then(elt => elt.value)
-	.then(local => local == null ?
-		Promise.reject('Local não disponível') :
-		Promise.resolve(local)
-	)
-	.then(Number)
-	.then(l => l - 1)
-	.then(l => ['trf4', 'jfrs', 'jfsc', 'jfpr'][l])
-	.then(dominio => dominio == null ?
-		Promise.reject('Local desconhecido') :
-		Promise.resolve(dominio)
-	)
-	.then(d => `${d}-jus-br`)
-	.then(dominio => sigla => Promise.resolve(sigla)
-		.then(sigla => sigla.toLowerCase())
-		.then(sigla => `https://intra.trf4.jus.br/membros/${sigla}${dominio}`)
+	.chain(formToUrlSigla);
+
+const createFragment = doc
+	.map(doc => doc.createElement('template'))
+	.map(templ => (templ.innerHTML = ` [ <a href="" target="_blank">Abrir na Intra</a> ]`, templ))
+	.chain(templ => doc
+		.map(doc => () => doc.importNode(templ.content, true))
+		.map(create => url => {
+			const frag = create();
+			frag.querySelector('a').href = url;
+			return frag;
+		})
 	);
 
+const siglaToFragment = urlSigla
+	.ap(createFragment
+		.map(create => urlSigla => url =>
+			create(urlSigla(url))
+		)
+	);
+
+const re = /^Sigla:\s*(.+)$/;
+
+const nodeToMaybeSigla = Function
+	.id(node => node.textContent)
+	.compose(text => text.trim())
+	.compose(text => text.match(re))
+	.compose(match => match === null ? [] : [match[1]]);
+
+const nodeSiglaToTask = (node, sigla) =>
+	Task.of(sigla)
+		.ap(siglaToFragment)
+		.map(frag => node.parentNode.insertBefore(frag, node.nextSibling))
+		.map(() => 1);
+
+const siblings = elt => List.chainRec(
+	(next, done, value) => {
+		const node = value.nextSibling;
+		return node === null ? done(null) : next(node);
+	},
+	elt
+);
+
+const formToNodes = Function
+	.id(siblings)
+	.composeK(node => {
+		if (/^table$/i.test(node.tagName)) {
+			return node.querySelector('td:nth-child(2)').childNodes;
+		}
+		return [node];
+	});
+
+const nodeToMaybeTask = node => {
+	const maybeSigla = nodeToMaybeSigla(node);
+	const maybeTask = maybeSigla.map(sigla => nodeSiglaToTask(node, sigla));
+	return maybeTask;
+};
+
+const formToTask = Function
+	.id(formToNodes)
+	.composeK(nodeToMaybeTask)
+	.compose(Task.all);
+
 const main = form
-	.then(function *(form) {
-		let next = form.nextSibling;
-		while (next !== null) {
-			yield next;
-			next = next.nextSibling;
-		}
-		return next;
-	})
-	.then(function *(nodes) {
-		for (let node of nodes) {
-			if (/^table$/i.test(node.tagName)) {
-				yield *node.querySelector('td:nth-child(2)').childNodes;
-			}
-			yield node;
-		}
-	})
-	.then(function *(nodes) {
-		for (let node of nodes) {
-			const [maybeSigla] = [node]
-				.map(node => node.textContent)
-				.map(text => text.trim())
-				.map(text => text.match(/^Sigla:\s*(.+)$/) || [])
-				.map(match => match.slice(1));
-
-			maybeSigla.forEach(sigla =>
-				Promise.resolve(sigla)
-					.ap(urlSigla)
-					.ap(createFragment)
-					.then(frag => node.parentNode.insertBefore(frag, node.nextSibling))
-					.then(() => 1)
-			);
-
-			if (maybeSigla.length > 0) yield 1;
-		}
-	})
-	.then(([...results]) => results.length)
-	.then(qtd => {
+	.chain(formToTask)
+	.map(results => results.length)
+	.map(qtd => {
 		const s = qtd > 1 ? 's' : '';
 		return `${qtd} link${s} criado${s}.`;
 	});
 
+console.timeEnd('setup');
 console.time('main');
-main.then(
-	x => {
-		console.log('Resultado:', x);
-		console.timeEnd('main');
-	},
+main.fork(
 	e => {
 		console.error('Erro:', e);
 		console.timeEnd('main');
+		console.timeEnd('total');
+	},
+	x => {
+		console.log('Resultado:', x);
+		console.timeEnd('main');
+		console.timeEnd('total');
 	}
 );
