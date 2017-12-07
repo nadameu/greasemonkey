379b41dcb4c62ab26a0aa7f67b26b4907437a9dc
Paulo <paulo@nadameu.com.br>
2017-12-07 16:00:53 -0200

    Simplificação do código

--- achei.user.js
+++ achei.user.js
@@ -4,146 +4,116 @@
 // @description Link para informações da Intra na página do Achei!
 // @include     http://centralrh.trf4.gov.br/achei/pesquisar.php?acao=pesquisar*
 // @include     http://serh.trf4.jus.br/achei/pesquisar.php?acao=pesquisar*
-// @require     https://github.com/nadameu/greasemonkey/raw/List-v1.0.0/lib/List.js
-// @require     https://github.com/nadameu/greasemonkey/raw/Task-v1.1.0/lib/Task.js
-// @version     10
+// @version     11
 // @grant       none
 // ==/UserScript==
 
-/* global List, Task */
-
-'use strict';
-
-console.time('total');
-console.time('setup');
-
-Function.prototype.compose = function(f) { return x => f(this(x)); };
-Function.prototype.composeK = function(f) { return x => this(x).chain(f); };
-Function.id = x => x;
-
-const doc = Task.of(document);
-
-const docToForm = Function
-	.id(doc => doc.querySelector('form[name="formulario"]'))
-	.compose(form => form == null ?
-		Task.rejected('Formulário não encontrado') :
-		Task.of(form)
-	);
-
-const form = doc.chain(docToForm);
-
-const formToLocal = Function
-	.id(form => form.local || {})
-	.compose(elt => elt.value)
-	.compose(local => local == null ?
-		Task.rejected('Local não disponível') :
-		Task.of(local)
-	);
-
-const localToDominio = Function
-	.id(Number)
-	.compose(l => l - 1)
-	.compose(l => ['trf4', 'jfrs', 'jfsc', 'jfpr'][l])
-	.compose(dominio => dominio == null ?
-		Task.rejected('Local desconhecido') :
-		Task.of(dominio)
-	);
-
-const dominioToUrlSigla = Function
-	.id(d => `${d}-jus-br`)
-	.compose(dominio => Task
-		.of(sigla => sigla.toLowerCase())
-		.map(f => x => `https://intra.trf4.jus.br/membros/${f(x)}${dominio}`)
-	);
-
-const formToUrlSigla = Function
-	.id(formToLocal)
-	.composeK(localToDominio)
-	.composeK(dominioToUrlSigla);
-
-const urlSigla = form
-	.chain(formToUrlSigla);
-
-const createFragment = doc
-	.map(doc => doc.createElement('template'))
-	.map(templ => (templ.innerHTML = ` [ <a href="" target="_blank">Abrir na Intra</a> ]`, templ))
-	.chain(templ => doc
-		.map(doc => () => doc.importNode(templ.content, true))
-		.map(create => url => {
-			const frag = create();
-			frag.querySelector('a').href = url;
-			return frag;
-		})
-	);
-
-const siglaToFragment = urlSigla
-	.ap(createFragment
-		.map(create => urlSigla => url =>
-			create(urlSigla(url))
-		)
-	);
-
-const re = /^Sigla:\s*(.+)$/;
-
-const nodeToMaybeSigla = Function
-	.id(node => node.textContent)
-	.compose(text => text.trim())
-	.compose(text => text.match(re))
-	.compose(match => match === null ? [] : [match[1]]);
-
-const nodeSiglaToTask = (node, sigla) =>
-	Task.of(sigla)
-		.ap(siglaToFragment)
-		.map(frag => node.parentNode.insertBefore(frag, node.nextSibling))
-		.map(() => 1);
-
-const siblings = elt => List.chainRec(
-	(next, done, value) => {
-		const node = value.nextSibling;
-		return node === null ? done(null) : next(node);
-	},
-	elt
-);
-
-const formToNodes = Function
-	.id(siblings)
-	.composeK(node => {
-		if (/^table$/i.test(node.tagName)) {
-			return node.querySelector('td:nth-child(2)').childNodes;
+const Sequence = {
+	chain: f => function *(iterable) {
+		for (let item of iterable) {
+			yield *f(item);
 		}
-		return [node];
-	});
-
-const nodeToMaybeTask = node => {
-	const maybeSigla = nodeToMaybeSigla(node);
-	const maybeTask = maybeSigla.map(sigla => nodeSiglaToTask(node, sigla));
-	return maybeTask;
+	},
+	filter: p => Sequence.chain(item => p(item) ? [item] : []),
+	map: f => Sequence.chain(item => [f(item)]),
+	reduce: (reducer, acc) => iterable => {
+		for (let item of iterable) {
+			acc = reducer(acc, item);
+		}
+		return acc;
+	}
 };
 
-const formToTask = Function
-	.id(formToNodes)
-	.composeK(nodeToMaybeTask)
-	.compose(Task.all);
+const fromNullable = msg => obj => obj == null ?
+	Promise.reject(msg) :
+	Promise.resolve(obj)
+;
 
-const main = form
-	.chain(formToTask)
-	.map(results => results.length)
-	.map(qtd => {
-		const s = qtd > 1 ? 's' : '';
-		return `${qtd} link${s} criado${s}.`;
+const queryFormulario = doc => fromNullable('Formulário não encontrado!')(
+	doc.querySelector('form[name="formulario"]')
+);
+
+const makeCreateFragment = doc => {
+	const template = doc.createElement('template');
+	template.innerHTML = ' [ <a href="" target="_blank">Abrir na Intra</a> ]';
+	const link = template.content.querySelector('a');
+	return url => {
+		link.href = url;
+		const fragment = doc.importNode(template.content, true);
+		return fragment;
+	};
+};
+
+const queryDominio = doc => fromNullable('Não foi selecionado local!')(
+	doc.querySelector('[name="local"]:checked')
+)
+	.then(input => input.nextSibling)
+	.then(fromNullable('Local selecionado não possui texto!'))
+	.then(textNode => textNode.textContent)
+	.then(text => text.trim())
+	.then(text => text.toLowerCase());
+
+const makeSiglaToUrl = dominio => sigla => [
+	'https://intra.trf4.jus.br/membros/',
+	sigla.toLowerCase(),
+	dominio,
+	'-jus-br'
+].join('');
+
+const nextSiblingsOf = function *(elt) {
+	for (
+		let sibling = elt.nextSibling;
+		sibling !== null;
+		sibling = sibling.nextSibling
+	) {
+		yield sibling;
+	}
+};
+
+const queryTableChildNodes = function *(node) {
+	if ('tagName' in node && /^table$/i.test(node.tagName)) {
+		yield* node.querySelector('td:nth-child(2)').childNodes;
+	} else {
+		yield node;
+	}
+};
+
+const withRegExp = re => {
+	const matches = node => re.test(node.textContent);
+	const extract = node => node.textContent.match(re)[1];
+	return { matches, extract };
+};
+
+const {
+	matches: matchesSigla,
+	extract: extractSigla
+} = withRegExp(/^Sigla:\s*(\S+)\s*$/);
+
+const makeAppendFragment = (doc, dominio) => {
+	const createFragment = makeCreateFragment(doc);
+	const siglaToUrl = makeSiglaToUrl(dominio);
+	return node => [
+		extractSigla,
+		siglaToUrl,
+		createFragment,
+		fragment => node.parentNode.insertBefore(fragment, node.nextSibling)
+	].reduce((x, f) => f(x), node);
+};
+
+const main = doc => queryDominio(doc)
+	.then(dominio => {
+		const appendFragment = makeAppendFragment(doc, dominio);
+		return queryFormulario(doc)
+			.then(nextSiblingsOf)
+			.then(Sequence.chain(queryTableChildNodes))
+			.then(Sequence.filter(matchesSigla))
+			.then(Sequence.map(node => appendFragment(node)))
+			.then(xs => [...xs].length)
+			.then(qtd => ({ qtd, plural: qtd > 1 ? 's' : '' }))
+			.then(({ qtd, plural }) => `${qtd} link${plural} criado${plural}.`);
 	});
 
-console.timeEnd('setup');
-console.time('main');
-main.fork(
-	e => {
-		console.error('Erro:', e);
-		console.timeEnd('main');
-		console.timeEnd('total');
-	},
-	x => {
-		console.log('Resultado:', x);
-		console.timeEnd('main');
-		console.timeEnd('total');
-	}
+main(document).then(
+	x => { console.log('Resultado:', x); },
+	e => { console.error('Erro:', e); }
 );
