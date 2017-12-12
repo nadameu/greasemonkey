c42f005b764f888c52aed50a74e6210ad8ed328c
Paulo <paulo@nadameu.com.br>
2017-12-12 18:45:58 -0200

    Simplificação

--- achei.user.js
+++ achei.user.js
@@ -4,117 +4,38 @@
 // @description Link para informações da Intra na página do Achei!
 // @include     http://centralrh.trf4.gov.br/achei/pesquisar.php?acao=pesquisar*
 // @include     http://serh.trf4.jus.br/achei/pesquisar.php?acao=pesquisar*
-// @version     12
+// @version     13
 // @grant       none
 // ==/UserScript==
 
-const Sequence = {
-	chain: f => function *(iterable) {
-		for (let item of iterable) {
-			yield *f(item);
-		}
-	},
-	filter: p => Sequence.chain(item => p(item) ? [item] : []),
-	map: f => Sequence.chain(item => [f(item)]),
-};
-
-const fromNullable = msg => obj => obj == null ?
-	Promise.reject(msg) :
-	Promise.resolve(obj)
-;
-
-const queryFormulario = doc => fromNullable('Formulário não encontrado!')(
-	doc.querySelector('form[name="formulario"]')
-);
-
-const makeCreateFragment = doc => {
-	const template = doc.createElement('template');
-	template.innerHTML = ' [ <a href="" target="_blank">Abrir na Intra</a> ]';
-	const link = template.content.querySelector('a');
-	return url => {
-		link.href = url;
-		const fragment = doc.importNode(template.content, true);
-		return fragment;
-	};
-};
-
-const queryDominio = doc => fromNullable('Não foi selecionado local!')(
-	doc.querySelector('[name="local"]:checked')
-)
-	.then(input => input.nextSibling)
-	.then(fromNullable('Local selecionado não possui texto!'))
-	.then(textNode => textNode.textContent)
-	.then(text => text.trim())
-	.then(text => text.toLowerCase());
-
-const makeSiglaToUrl = dominio => sigla => [
-	'https://intra.trf4.jus.br/membros/',
-	sigla.toLowerCase(),
-	dominio,
-	'-jus-br'
-].join('');
-
-const nextSiblingsOf = function *(elt) {
-	for (
-		let sibling = elt.nextSibling;
-		sibling !== null;
-		sibling = sibling.nextSibling
-	) {
-		yield sibling;
+const reSigla = /^Sigla:\s*(\S+)\s*$/;
+const reTable = /^table$/i;
+const formularioPromise = Promise.resolve(document.querySelector('form[name="formulario"]') || Promise.reject('Formul\u00e1rio n\u00e3o encontrado!'));
+const dominioPromise = Promise.resolve(document.querySelector('[name="local"]:checked') || Promise.reject('N\u00e3o foi selecionado local!'))
+	.then(inputLocal => Promise.resolve(inputLocal.nextSibling || Promise.reject('Local selecionado n\u00e3o possui texto!')))
+	.then(node => node.textContent.trim().toLowerCase());
+const template = document.createElement('template');
+template.innerHTML = ' [ <a href="" target="_blank">Abrir na Intra</a> ]';
+const content = template.content;
+const link = content.querySelector('a');
+const makeReducer = dominio => (soma, node) => {
+	if (reSigla.test(node.textContent)) {
+		link.href = 'https://intra.trf4.jus.br/membros/' + node.textContent.match(reSigla)[1].toLowerCase() + dominio + '-jus-br';
+		const fragment = document.importNode(content, true);
+		node.parentNode.insertBefore(fragment, node.nextSibling);
+		return soma + 1;
 	}
+	return soma;
 };
-
-const flattenTable = function *(node) {
-	if ('tagName' in node && /^table$/i.test(node.tagName)) {
-		yield *node.querySelector('td:nth-child(2)').childNodes;
-	} else {
-		yield node;
+Promise.all([dominioPromise, formularioPromise]).then(pair => {
+	const reducer = makeReducer(pair[0]);
+	let qtd = 0;
+	for (let node = pair[1].nextSibling; node !== null; node = node.nextSibling) {
+		qtd = ('tagName' in node && reTable.test(node.tagName) ? Array.from(node.querySelector('td:nth-child(2)').childNodes) : [node]).reduce(reducer, qtd);
 	}
-};
-
-const withRegExp = re => {
-	const matches = node => re.test(node.textContent);
-	const extract = node => node.textContent.match(re)[1];
-	return { matches, extract };
-};
-
-const {
-	matches: matchesSigla,
-	extract: extractSigla
-} = withRegExp(/^Sigla:\s*(\S+)\s*$/);
-
-const makeAppendFragment = (doc, dominio) => {
-	const createFragment = makeCreateFragment(doc);
-	const siglaToUrl = makeSiglaToUrl(dominio);
-	return node => [
-		extractSigla,
-		siglaToUrl,
-		createFragment,
-		fragment => node.parentNode.insertBefore(fragment, node.nextSibling)
-	].reduce((x, f) => f(x), node);
-};
-
-const main = doc => queryDominio(doc)
-	.then(dominio => queryFormulario(doc)
-		.then(form => [
-			nextSiblingsOf,
-			Sequence.chain(flattenTable),
-			Sequence.filter(matchesSigla),
-			Sequence.map(makeAppendFragment(doc, dominio)),
-			fragments => Array.from(fragments).length,
-			qtd => {
-				const s = qtd > 1 ? 's' : '';
-				const links = `link${s}`;
-				const criados = `criado${s}`;
-				return `${qtd} ${links} ${criados}`;
-			}
-		].reduce(
-			(result, f) => f(result),
-			form
-		))
-	);
-
-main(document).then(
+	const s = qtd > 1 ? 's' : '';
+	return qtd + ' ' + ('link' + s) + ' ' + ('criado' + s);
+}).then(
 	x => { console.log('Resultado:', x); },
 	e => { console.error('Erro:', e); }
 );
