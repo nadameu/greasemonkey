967b5c67f4f3498438c7714c3f72699320e4e835
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2018-08-09 18:40:40 -0300

    Precatórios/RPVs versão 21 - novas funcionalidades

---
+++ precatorios-rpv.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name Precatórios/RPVs
+// @description Auxilia a conferência de RPVs e precatórios.
+// @version 21.0.0
+// @author nadameu
+// @namespace http://nadameu.com.br/precatorios-rpv
+// @include /^https:\/\/eproc\.(trf4|jf(pr|rs|sc))\.jus\.br\/eproc(2trf4|V2)\/controlador\.php\?acao=processo_selecionar\&/
+// @include /^https:\/\/eproc\.(trf4|jf(pr|rs|sc))\.jus\.br\/eproc(2trf4|V2)\/controlador\.php\?acao=processo_precatorio_rpv\&/
+// @include /^https:\/\/eproc\.(trf4|jf(pr|rs|sc))\.jus\.br\/eproc(2trf4|V2)\/controlador\.php\?acao=oficio_requisitorio_visualizar\&/
+// @include /^https:\/\/eproc\.(trf4|jf(pr|rs|sc))\.jus\.br\/eproc(2trf4|V2)\/controlador\.php\?acao=oficio_requisitorio_listar\&/
+// @website https://www.nadameu.com.br/
+// @downloadURL https://github.com/nadameu/greasemonkey/raw/master/precatorios-rpv.user.js
+// @updateURL https://github.com/nadameu/greasemonkey/raw/master/precatorios-rpv.meta.js
+// @supportURL https://github.com/nadameu/precatorios-rpv/issues
+// @grant GM_xmlhttpRequest
+// ==/UserScript==
