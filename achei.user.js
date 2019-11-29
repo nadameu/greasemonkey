101f3536676fb65820d643e6ed6bade9362e3431
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-11-29 17:38:58 -0300

    Refatoração

--- achei.user.js
+++ achei.user.js
@@ -10,49 +10,43 @@
 
 const reSigla = /^Sigla:\s*(\S+)\s*$/;
 const reTable = /^table$/i;
-const formularioPromise = Promise.resolve(
-  document.querySelector('form[name="formulario"]') ||
-    Promise.reject('Formul\u00e1rio n\u00e3o encontrado!')
-);
-const dominioPromise = Promise.resolve(
-  document.querySelector('[name="local"]:checked') ||
-    Promise.reject('N\u00e3o foi selecionado local!')
-)
-  .then(inputLocal =>
-    Promise.resolve(
-      inputLocal.nextSibling || Promise.reject('Local selecionado n\u00e3o possui texto!')
-    )
-  )
+const ifNull = (val, msg) => (val == null ? Promise.reject(msg) : Promise.resolve(val));
+const query = (selector, msg) => ifNull(document.querySelector(selector), msg);
+const formularioPromise = query('form[name="formulario"]', 'Formulário não encontrado!');
+const dominioPromise = query('[name="local"]:checked', 'Não foi selecionado local!')
+  .then(inputLocal => ifNull(inputLocal.nextSibling, 'Local selecionado não possui texto!'))
   .then(node => node.textContent.trim().toLowerCase());
 const template = document.createElement('template');
 template.innerHTML = ' [ <a href="" target="_blank">Abrir na Intra</a> ]';
 const content = template.content;
 const link = content.querySelector('a');
-const makeReducer = dominio => (soma, node) => {
-  if (reSigla.test(node.textContent)) {
-    link.href =
-      'https://intra.trf4.jus.br/membros/' +
-      node.textContent.match(reSigla)[1].toLowerCase() +
-      dominio +
-      '-jus-br';
-    const fragment = document.importNode(content, true);
-    node.parentNode.insertBefore(fragment, node.nextSibling);
-    return soma + 1;
-  }
-  return soma;
+const inserirNodeSeTiverSigla = dominio => node => {
+  const match = node.textContent.match(reSigla);
+
+  if (match === null) return false;
+
+  link.href = `https://intra.trf4.jus.br/membros/${match[1].toLowerCase()}${dominio}-jus-br`;
+  const fragment = document.importNode(content, true);
+  node.parentNode.insertBefore(fragment, node.nextSibling);
+  return true;
+};
+const siblings = function*(node) {
+  for (let current = node.nextSibling; current !== null; current = current.nextSibling)
+    yield current;
 };
 Promise.all([dominioPromise, formularioPromise])
-  .then(pair => {
-    const reducer = makeReducer(pair[0]);
-    let qtd = 0;
-    for (let node = pair[1].nextSibling; node !== null; node = node.nextSibling) {
-      qtd = ('tagName' in node && reTable.test(node.tagName)
-        ? Array.from(node.querySelector('td:nth-child(2)').childNodes)
-        : [node]
-      ).reduce(reducer, qtd);
-    }
+  .then(([dominio, formulario]) => {
+    const mapper = inserirNodeSeTiverSigla(dominio);
+    const qtd = Array.from(siblings(formulario))
+      .flatMap(node => {
+        if (node instanceof HTMLTableElement)
+          return Array.from(node.querySelector('td:nth-child(2)').childNodes);
+        return [node];
+      })
+      .map(mapper)
+      .filter(resultado => resultado === true).length;
     const s = qtd > 1 ? 's' : '';
-    return qtd + ' ' + ('link' + s) + ' ' + ('criado' + s);
+    return `${qtd} link${s} criado${s}`;
   })
   .then(
     x => {
