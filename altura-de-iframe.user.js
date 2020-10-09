30770d9ef739555051b76b477aff791a7eb8c303
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2020-10-09 17:56:42 -0300

    Create altura-de-iframe.user.js

---
+++ altura-de-iframe.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name        altura-de-iframe
+// @name:pt-BR  Altura do iframe
+// @namespace   http://nadameu.com.br
+// @match       https://eproc.jfsc.jus.br/eprocV2/controlador.php
+// @grant       none
+// @version     1.0.0
+// @author      nadameu
+// @description Limita a altura do iframe para que não ultrapasse o espaço disponível na tela
+// ==/UserScript==
+
+if (window.exibirSubFrm) {
+  const funcaoOriginal = window.exibirSubFrm;
+  window.exibirSubFrm = function exibirSubFrmCabivel() {
+    const valorRetornado = funcaoOriginal.apply(this, arguments);
+    const iframe = document.getElementById('ifrSubFrm');
+    if (iframe) {
+      iframe.style.position = 'fixed';
+      iframe.style.top = '6px';
+      iframe.style.borderWidth = '2px';
+      iframe.style.height = 'calc(100% - 16px)';
+    }
+    return valorRetornado;
+  };
+}
