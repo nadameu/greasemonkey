f41db702c170490432f9351713b69a586312518d
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2020-10-15 15:27:15 -0300

    Create focar-documento.user.js

---
+++ focar-documento.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name        focar-documento
+// @name:pt-BR  Focar documento ao abrir
+// @namespace   http://nadameu.com.br
+// @match       https://eproc.jfsc.jus.br/eprocV2/controlador.php?acao=acessar_documento&*
+// @grant       none
+// @version     1.0
+// @author      nadameu
+// @description Facilita o uso do teclado, focando o documento ao abri-lo em uma janela.
+// ==/UserScript==
+
+if (window.frames.length) window.frames[0].focus();
+
+document.body.addEventListener('keydown', evt => {
+  switch (evt.key) {
+    case 'j':
+    case 'J':
+      window.scrollByPages(1);
+      break;
+      
+    case 'k':
+    case 'K':
+      window.scrollByPages(-1);
+      break;
+      
+    case 'g':
+      window.scrollTo(undefined, 0);
+      break;
+
+    case 'G':
+      window.scrollTo(undefined, window.scrollMaxY);
+      break;
+      
+    default:
+      break;
+  }
+});
