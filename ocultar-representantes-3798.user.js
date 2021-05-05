c0145b9b0763f2b9e29f5f13bf5c662d76a9a214
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2021-05-05 18:46:00 -0300

    Desativação - funcionalidade integrada ao eproc

--- ocultar-representantes-3798.user.js
+++ ocultar-representantes-3798.user.js
@@ -2,29 +2,10 @@
 // @name        ocultar-representantes-3798
 // @name:pt-BR  Ocultar representantes 3798
 // @namespace   http://nadameu.com.br
-// @match       https://eproc.jfpr.jus.br/eprocV2/controlador.php?acao=processo_selecionar&*
-// @match       https://eproc.jfrs.jus.br/eprocV2/controlador.php?acao=processo_selecionar&*
-// @match       https://eproc.jfsc.jus.br/eprocV2/controlador.php?acao=processo_selecionar&*
-// @match       https://eproc.trf4.jus.br/eproc2trf4/controlador.php?acao=processo_selecionar&*
 // @grant       none
-// @version     2.0.0
+// @version     3.0.0
 // @author      Paulo R. Maurici Jr.
 // @description Ocultar representantes da Agência 3798 do Banco do Brasil
 // ==/UserScript==
 
-document
-  .querySelectorAll('[id="exibirPartes"]')
-  .forEach(img => {
-    const partes = img.parentNode.getElementsByClassName('ocultarPartes');
-    img.addEventListener('click', update);
-    update();
-  
-    function update() {
-      const match = img.src.match(/\/ver_(tudo|resumo).gif$/);
-      if (! match) return;
-      const exibir = match[1] === 'resumo';
-      partes.forEach(parte => {
-        parte.style.display = exibir ? null : 'none';
-      });
-    }
-  });
+// Funcionalidade integrada ao sistema eproc.
