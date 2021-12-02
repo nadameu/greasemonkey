ec2fa51b4a37e1f3e6da2b8887e0792aede01121
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2021-12-02 01:13:56 -0300

    Simplificação da lógica

--- aguardar-carregamento-formulario.user.js
+++ aguardar-carregamento-formulario.user.js
@@ -2,52 +2,56 @@
 // @name        aguardar-carregamento-formulario
 // @name:pt-BR  Aguardar carregamento do formulário
 // @namespace   http://nadameu.com.br
+// @match       https://eproc.jfpr.jus.br/eprocV2/controlador.php?acao=relatorio_geral_listar&*
+// @match       https://eproc.jfrs.jus.br/eprocV2/controlador.php?acao=relatorio_geral_listar&*
 // @match       https://eproc.jfsc.jus.br/eprocV2/controlador.php?acao=relatorio_geral_listar&*
+// @match       https://eproc.trf4.jus.br/eproc2trf4/controlador.php?acao=relatorio_geral_listar&*
 // @grant       none
-// @version     1.1.0
+// @version     2.0.0
 // @author      nadameu
 // @description Aguardar o carregamento completo do formulário do relatório geral antes de poder clicar em "Consultar"
-// @run-at      document-start
 // ==/UserScript==
 
-let head, body, tela, localizador;
-
-const observer = new MutationObserver(() => {
-  if (! head) {
-    if (! document.head) return;
-    head = document.head;
-    adicionarEstilos();
-  }
-  if (! body) {
-    if (! document.body) return;
-    body = document.body;
-  }
-  if (! tela) {
-    tela = body.insertBefore(document.createElement('div'), body.firstChild);
-    tela.id = 'gm-tela-ocultar-formulario';
-    tela.textContent = 'Carregando formulário...'
-  }
-  if (! localizador) {
-    const el = document.getElementById('selLocalizadorPrincipal');
-    if (! el) return;
-    localizador = el;
-  }
-  observer.disconnect();
-  
-  const observerLoc = new MutationObserver(() => {
-    if (localizador.style.display !== 'none') return;
-    
-    body.removeChild(tela);
-    
-    observerLoc.disconnect();
-  });
-  observerLoc.observe(localizador, { attributes: true });
+main().catch(error => {
+  console.error(error);
 });
 
-observer.observe(document, { childList: true, subtree: true });
+async function main() {
+  adicionarEstilos();
+
+  const tela = document.body.appendChild(document.createElement('div'));
+  tela.id = 'gm-tela-ocultar-formulario';
+  tela.textContent = 'Carregando formulário...';
+
+  let timeout, observer;
+  
+  try {
+    const localizador = document.getElementById('selLocalizadorPrincipal');
+    if (! localizador) return;
+    
+    await Promise.race([
+      new Promise((_,rej) => {
+        timeout = window.setTimeout(rej, 3000);
+      }),
+      new Promise((res) => {
+        observer = new MutationObserver(() => {
+          if (localizador.style.display !== 'none') return;
+          else res();
+        });
+        observer.observe(localizador, { attributes: true });
+      }),
+    ])
+  } catch (error) {
+    throw error;
+  } finally {
+    if (observer) observer.disconnect(); 
+    if (timeout) window.clearTimeout(timeout);
+    document.body.removeChild(tela);
+  }
+}
 
 function adicionarEstilos() {
-  const style = head.appendChild(document.createElement('style'));
+  const style = document.head.appendChild(document.createElement('style'));
     
   style.textContent = /* css */ `
 #gm-tela-ocultar-formulario {
