8147b809b51b40cd0237a8018b93017b267dc7d5
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2022-02-08 17:08:39 -0300

    Update acoes-flutuantes.user.js

--- acoes-flutuantes.user.js
+++ acoes-flutuantes.user.js
@@ -7,103 +7,44 @@
 // @match       https://eproc.jfsc.jus.br/eprocV2/controlador.php?acao=processo_selecionar&*
 // @match       https://eproc.trf4.jus.br/eproc2trf4/controlador.php?acao=processo_selecionar&*
 // @grant       GM_addStyle
-// @version     4.0.0
+// @version     5.0.0
 // @author      nadameu
 // ==/UserScript==
 
-let debounceTimer;
-let debounceFn;
-function debounce(fn) {
-  return function () {
-    debounceFn = fn;
-    if (!debounceTimer)
-      debounceTimer = window.setTimeout(() => {
-        window.clearTimeout(debounceTimer);
-        debounceTimer = undefined;
-        debounceFn();
-      }, 100);
-  };
+main();
+
+function main() {
+  const backTop = document.getElementById('backTop');
+  if (! backTop) return;
+  const fldAcoes = document.getElementById('fldAcoes');
+  if (! fldAcoes) return;
+  adicionarEstilos();
+  const button = document.createElement('button');
+  button.id = 'gm-scroll-acoes'
+  button.className = 'btn btn-light shadow-sm';
+  button.textContent = 'Ações';
+  button.addEventListener('click', evt => {
+    evt.preventDefault();
+    fldAcoes.scrollIntoView({
+      behavior: 'smooth',
+      block: 'center',
+    });
+  });
+  backTop.insertAdjacentElement('afterend', button);
 }
 
-const div = $('<div id="divAcoesFlutuantes"></div>').insertBefore('#fldAcoes');
-$('#fldAcoes').appendTo(div);
-$('#nav-acoes-processo').parent().appendTo(div);
-const placeholder = $('<div></div>').insertBefore(div);
-placeholder
-  .css({ height: `${div.height()}px` })
-  .hide();
-
-let yPosition = 0;
-let current = div.get(0);
-do {
-  yPosition += current.offsetTop + current.clientTop;
-  current = current.offsetParent;
-} while (current);
-let navbarHeight = $('#navbar').height();
-yPosition -= navbarHeight;
-let areaDadosWidth = $('#divInfraAreaDados').width();
-
-let flutuante = false;
-function onscroll() {
-  if (window.scrollY > yPosition) {
-    if (!flutuante) {
-      flutuante = true;
-      placeholder.show();
-      div.toggleClass('flutuante', true);
-    }
-  } else {
-    if (flutuante) {
-      flutuante = false;
-      placeholder.hide();
-      div.toggleClass('flutuante', false);
-    }
-  }
-}
-
-window.addEventListener('scroll', debounce(onscroll));
-
-onscroll();
-
-$('#lblDemaisAcoes, #lblDemaisAcoesPref').css({ display: 'block' });
-
-GM_addStyle(/*css*/ `
-.infra-styles #fldAcoes a.infraButton {
-  line-height: 18px;
-  height: 18px;
-}
-
-.infra-styles #divMovPref a.infraButton {
-  line-height: 16px;
-  height: 16px;
-}
-
-#divAcoesFlutuantes.flutuante {
+function adicionarEstilos() {
+  GM_addStyle(/* css */ `
+#gm-scroll-acoes {
   position: fixed;
-  top: ${navbarHeight}px;
-  width: ${areaDadosWidth}px;
-  z-index: 1;
-  background-color: white;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.25),
-              0 4px 8px rgba(0, 0, 0, 0.125),
-              0 0 1.2em 0.6em white;
-  border-color: #ccc;
+  bottom: 1rem;
+  right: 9rem;
+  width: 50px;
+  height: 50px;
+  padding: 0;
+  border: 1px solid #6c757d57;
+  border-radius: 50px;
+  color: #6c757d;
 }
-
-#divAcoesFlutuantes.flutuante fieldset {
-  border-width: 1px 0 0;
-  padding-top: 0;
-  padding-bottom: 0;
+  `);
 }
-
-#divAcoesFlutuantes.flutuante > #divMovPref > br:first-child {
-  display: none;
-}
-
-#lblDemaisAcoes, #lblDemaisAcoesPref {
-  margin-bottom: -0.5em;
-}
-
-#lblOcultarAcoes, #lblOcultarAcoesPref {
-  display: block;
-}
-`);
