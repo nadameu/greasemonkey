fbf503de24b41c62fbecf413c82b670e24744bc7
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2022-08-19 16:23:10 -0300

    Correção

--- userscripts/atualizar-saldo/src/NumProc.ts
+++ userscripts/atualizar-saldo/src/NumProc.ts
@@ -1,5 +1,5 @@
 import { Opaque } from '@nadameu/opaque';
 import * as p from '@nadameu/predicates';
 
-export type NumProc = Opaque<string, { readonly numproc: never }>;
+export type NumProc = Opaque<string, { readonly numproc: unique symbol }>;
 export const isNumproc = p.refine(p.isString, (x): x is NumProc => /^\d{20}$/.test(x));
