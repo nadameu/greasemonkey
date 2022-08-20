e5f51ec7130bfa4d15ded926ae06a6b9f7f4f9fd
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2022-08-20 11:32:18 -0300

    Simplificação dos testes

--- lib/resolvable/index.test.ts
+++ lib/resolvable/index.test.ts
@@ -1,39 +1,17 @@
 import { describe, expect, test, vitest } from 'vitest';
 import { createResolvable } from '.';
 
-interface Ok<T> {
-  isOk: true;
-  value: T;
-}
-
-interface Err {
-  isOk: false;
-  reason: any;
-}
-
-type Result<T> = Ok<T> | Err;
-
-function Ok<T>(value: T): Result<T> {
-  return { isOk: true, value };
-}
-function Err<T = never>(reason: any): Result<T> {
-  return { isOk: false, reason };
-}
-
-function toResult<T>(promise: Promise<T>): Promise<Result<T>> {
-  return promise.then(Ok, Err);
-}
 describe('basic functionality', () => {
   test('resolve', async () => {
     const [promise, resolvable] = createResolvable<number>();
     resolvable.resolve(42);
-    expect(await toResult(promise)).toEqual(Ok(42));
+    await expect(promise).resolves.toEqual(42);
   });
 
   test('reject', async () => {
     const [promise, resolvable] = createResolvable<number>();
     resolvable.reject('Error');
-    expect(await toResult(promise)).toEqual(Err('Error'));
+    await expect(promise).rejects.toEqual('Error');
   });
 });
 
@@ -49,7 +27,7 @@ describe('multiple resolutions or rejections', () => {
     resolvable.resolve(999);
     resolvable.reject('reason');
 
-    expect(await toResult(promise)).toEqual(Ok(42));
+    await expect(promise).resolves.toEqual(42);
 
     expect(f).toHaveBeenCalledOnce();
     expect(f).toHaveBeenCalledWith(42);
@@ -67,7 +45,7 @@ describe('multiple resolutions or rejections', () => {
     resolvable.reject('other reason');
     resolvable.resolve(999);
 
-    expect(await toResult(promise)).toEqual(Err('Error'));
+    await expect(promise).rejects.toEqual('Error');
 
     expect(f).not.toHaveBeenCalled();
     expect(g).toHaveBeenCalledOnce();
