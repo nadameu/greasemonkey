e7f7c975c6ec11be184aa673074fcac15ecf45d0
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2022-08-19 23:40:22 -0300

    Testes comparativos com asyn generator

--- lib/create-dispatcher/index.test.ts
+++ lib/create-dispatcher/index.test.ts
@@ -1,62 +1,176 @@
-import { expect, test, vitest } from 'vitest';
+import { expect, test } from 'vitest';
 import { createDispatcher } from '.';
 
-test('dispatch before iteration', async () => {
+function createExampleDispatcher() {
   const d = createDispatcher<number>();
+  for (let i = 0; i < 5; i++) d.dispatch(i);
+  d.end();
+  return d;
+}
+async function* createExampleGenerator() {
+  for (let i = 0; i < 5; i++) yield await Promise.resolve(i);
+}
 
-  d.dispatch(0);
-  d.dispatch(1);
-  d.dispatch(2);
-
+test.each([
+  ['dispatcher', createExampleDispatcher()],
+  ['async generator', createExampleGenerator()],
+])('dispatch, next, return - %s', async (_, d) => {
   const it = d[Symbol.asyncIterator]();
   const p = it.next();
   const q = it.next();
-  const r = it.next();
+  const r = it.return(-1);
+  const s = it.next();
 
-  expect(await p).toEqual({ done: false, value: 0 });
-  expect(await q).toEqual({ done: false, value: 1 });
-  expect(await r).toEqual({ done: false, value: 2 });
+  await expect(p).resolves.toEqual({ done: false, value: 0 });
+  await expect(q).resolves.toEqual({ done: false, value: 1 });
+  await expect(r).resolves.toEqual({ done: true, value: -1 });
+  await expect(s).resolves.toEqual({ done: true, value: undefined });
 });
 
-test('dispatch after iteration', async () => {
-  const d = createDispatcher<number>();
-
+test.each([
+  ['dispatcher', createExampleDispatcher()],
+  ['async generator', createExampleGenerator()],
+])('dispatch, next, throw - %s', async (_, d) => {
   const it = d[Symbol.asyncIterator]();
   const p = it.next();
   const q = it.next();
-  const r = it.next();
+  const r = it.throw('Error');
+  const s = it.next();
 
-  d.dispatch(0);
-  d.dispatch(1);
-  d.dispatch(2);
-
-  expect(await p).toEqual({ done: false, value: 0 });
-  expect(await q).toEqual({ done: false, value: 1 });
-  expect(await r).toEqual({ done: false, value: 2 });
+  await expect(p).resolves.toEqual({ done: false, value: 0 });
+  await expect(q).resolves.toEqual({ done: false, value: 1 });
+  await expect(r).rejects.toEqual('Error');
+  await expect(s).resolves.toEqual({ done: true, value: undefined });
 });
 
-test('iterate twice', async () => {
+test('next, return, dispatch', async () => {
   const d = createDispatcher<number>();
 
   const it = d[Symbol.asyncIterator]();
   const p = it.next();
   const q = it.next();
-  const r = it.next();
+  const r = it.return(-1);
+  const s = it.next();
 
   d.dispatch(0);
   d.dispatch(1);
   d.dispatch(2);
 
-  expect(await p).toEqual({ done: false, value: 0 });
-  expect(await q).toEqual({ done: false, value: 1 });
-  expect(await r).toEqual({ done: false, value: 2 });
+  await expect(p).resolves.toEqual({ done: false, value: 0 });
+  await expect(q).resolves.toEqual({ done: false, value: 1 });
+  await expect(r).resolves.toEqual({ done: true, value: -1 });
+  await expect(s).resolves.toEqual({ done: true, value: undefined });
+});
+
+test('next, throw, dispatch', async () => {
+  const d = createDispatcher<number>();
+
+  const it = d[Symbol.asyncIterator]();
+  const p = it.next();
+  const q = it.next();
+  const r = it.throw('Error');
+  const s = it.next();
+
+  d.dispatch(0);
+  d.dispatch(1);
+  d.dispatch(2);
+
+  await expect(p).resolves.toEqual({ done: false, value: 0 });
+  await expect(q).resolves.toEqual({ done: false, value: 1 });
+  await expect(r).rejects.toEqual('Error');
+  await expect(s).resolves.toEqual({ done: true, value: undefined });
+});
+
+test.each([
+  ['dispatch', createExampleDispatcher()],
+  ['async generator', createExampleGenerator()],
+])('iterate twice - %s', async (_, d) => {
+  const it = d[Symbol.asyncIterator]();
+  const p = it.next();
+  const q = it.return(4);
+  const r = it.next();
+
+  await expect(p).resolves.toEqual({ done: false, value: 0 });
+  await expect(q).resolves.toEqual({ done: true, value: 4 });
+  await expect(r).resolves.toEqual({ done: true, value: undefined });
 
   const it2 = d[Symbol.asyncIterator]();
   const u = it2.next();
-  const v = it2.next();
+  const v = it2.throw('Error');
   const w = it2.next();
 
-  expect(await u).toEqual({ done: false, value: 0 });
-  expect(await v).toEqual({ done: false, value: 1 });
-  expect(await w).toEqual({ done: false, value: 2 });
+  await expect(u).resolves.toEqual({ done: true, value: undefined });
+  await expect(v).rejects.toEqual('Error');
+  await expect(w).resolves.toEqual({ done: true, value: undefined });
+});
+
+test.each([
+  ['dispatch', createExampleDispatcher()],
+  ['async generator', createExampleGenerator()],
+])('for await (break) - %s', async (_, d) => {
+  let actual: number[] = [];
+  let err = '';
+  let fin = '';
+  try {
+    for await (const x of d) {
+      actual.push(x);
+      if (actual.length === 3) break;
+    }
+  } catch (e) {
+    err = e as string;
+  } finally {
+    fin = 'cleaned up';
+  }
+
+  expect(actual).toEqual([0, 1, 2]);
+  expect(err).toEqual('');
+  expect(fin).toEqual('cleaned up');
+});
+
+test.each([
+  ['dispatch', createExampleDispatcher()],
+  ['async generator', createExampleGenerator()],
+])('for await (throw) - %s', async (_, d) => {
+  let actual: number[] = [];
+  let err = '';
+  let fin = '';
+  try {
+    for await (const x of d) {
+      actual.push(x);
+      if (actual.length === 3) throw 'Error';
+    }
+  } catch (e) {
+    err = e as string;
+  } finally {
+    fin = 'cleaned up';
+  }
+
+  expect(actual).toEqual([0, 1, 2]);
+  expect(err).toEqual('Error');
+  expect(fin).toEqual('cleaned up');
+});
+
+test.each([
+  ['dispatch', createExampleDispatcher()],
+  ['async generator', createExampleGenerator()],
+])('for await (exhaust) - %s', async (_, d) => {
+  const actual: number[] = [];
+  for await (const x of d) {
+    actual.push(x);
+  }
+
+  expect(actual).toEqual([0, 1, 2, 3, 4]);
+});
+
+test.each([
+  ['dispatcher', createExampleDispatcher()],
+  ['async generator', createExampleGenerator()],
+])('exhaust (next) - %s', async (_, d) => {
+  const it = d[Symbol.asyncIterator]();
+  for (let i = 0; i < 5; i++) {
+    await expect(it.next()).resolves.toEqual({ done: false, value: i });
+  }
+  await expect(it.next()).resolves.toEqual({ done: true, value: undefined });
+  await expect(it.next()).resolves.toEqual({ done: true, value: undefined });
+  await expect(it.next()).resolves.toEqual({ done: true, value: undefined });
 });
--- lib/create-dispatcher/index.ts
+++ lib/create-dispatcher/index.ts
@@ -1,5 +1,7 @@
 class Dispatcher<T> {
-  private _index: number = 0;
+  private _readIndex = 0;
+  private _writeIndex = 0;
+  private _isDone = false;
   private _promises: Promise<IteratorResult<T>>[] = [];
   private _resolves: Array<(_: IteratorResult<T>) => void> = [];
 
@@ -13,22 +15,42 @@ class Dispatcher<T> {
     return promise;
   }
 
-  [Symbol.asyncIterator](): AsyncIterator<T> {
-    let i = 0;
+  [Symbol.asyncIterator](): AsyncIterator<T> & { return: {}; throw: {} } {
     return {
-      next: () => {
-        if (i < this._promises.length) return this._promises[i++]!;
-        i += 1;
+      next: async () => {
+        if (this._isDone) {
+          return { done: this._isDone, value: undefined };
+        }
+        if (this._readIndex < this._promises.length) {
+          const result = await this._promises[this._readIndex++]!;
+          if (result.done) {
+            this._isDone = true;
+          }
+          return result;
+        }
+        this._readIndex += 1;
         return this._addPending();
+      },
+      return: (value?: any) => {
+        this._isDone = true;
+        return Promise.resolve({ done: this._isDone, value });
+      },
+      throw: (reason?: any) => {
+        this._isDone = true;
+        return Promise.reject(reason);
       },
     };
   }
 
   dispatch(value: T): void {
-    this._resolves[this._index++]!({ done: false, value });
-    if (this._index >= this._promises.length) {
+    this._resolves[this._writeIndex++]!({ done: false, value });
+    if (this._writeIndex >= this._promises.length) {
       this._addPending();
     }
+  }
+
+  end() {
+    this._resolves[this._writeIndex++]!({ done: true, value: undefined });
   }
 }
 
