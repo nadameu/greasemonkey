aa84eedb9214616edb9fa343e659458bf1ced373
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2022-09-15 13:04:14 -0300

    Refatoração

--- lib/Maybe.ts
+++ lib/Maybe.ts
@@ -5,50 +5,63 @@ type AllMaybes<T, Acc extends any[] = []> = T extends [Maybe<infer U>, ...infer 
   : never;
 
 export type Maybe<a> = Just<a> | Nothing<a>;
-interface MaybeBase<a> {
-  [Symbol.iterator](): Generator<a>;
-  chain<b>(this: Maybe<a>, f: (_: a) => Maybe<b>): Maybe<b>;
-  safeMap<b>(this: Maybe<a>, f: (_: a) => b | null | undefined): Maybe<b>;
-  map<b>(this: Maybe<a>, f: (_: a) => b): Maybe<b>;
-  then<b>(Just: (value: a) => b, Nothing: () => b): b;
+abstract class _Maybe<a> {
+  *[Symbol.iterator]() {
+    if ((this as unknown as Maybe<a>).isJust) yield (this as unknown as Just<a>).value;
+  }
+  abstract match<b>(Nothing: () => b, Just: (value: a) => b): b;
+  apply<a, b>(this: Maybe<(_: a) => b>, that: Maybe<a>): Maybe<b> {
+    return this.chain(f => that.map(f));
+  }
+  chain<b>(f: (_: a) => Maybe<b>): Maybe<b>;
+  chain<b>(this: Maybe<a>, f: (_: a) => Maybe<b>): Maybe<b> {
+    return this.match(() => Nothing, f);
+  }
+  safeMap<b>(f: (_: a) => b | null | undefined): Maybe<b>;
+  safeMap<b>(this: Maybe<a>, f: (_: a) => b | null | undefined): Maybe<b> {
+    return this.chain(x => {
+      const y = f(x);
+      if (y == null) return Nothing;
+      return Just(y);
+    });
+  }
+  map<b>(f: (_: a) => b): Maybe<b>;
+  map<b>(this: Maybe<a>, f: (_: a) => b): Maybe<b> {
+    return this.chain(x => Just(f(x)));
+  }
 }
-export interface Just<a> extends MaybeBase<a> {
+
+export interface Just<a> extends _Maybe<a> {
   isJust: true;
   isNothing: false;
   value: a;
 }
-export function Just<a>(value: a): Maybe<a> {
-  return {
-    isJust: true,
-    isNothing: false,
-    value,
-    *[Symbol.iterator]() {
-      yield value;
-    },
-    chain: f => f(value),
-    map: f => Just(f(value)),
-    safeMap: f => fromNullish(f(value)),
-    then(f, _) {
-      return f(value);
-    },
-  };
+class _Just<a> extends _Maybe<a> implements Just<a> {
+  isJust: true = true;
+  isNothing: false = false;
+  constructor(public value: a) {
+    super();
+  }
+  match<b>(Nothing: () => b, Just: (value: a) => b): b {
+    return Just(this.value);
+  }
 }
-export interface Nothing<a = never> extends MaybeBase<a> {
+export function Just<a>(value: a): Just<a> {
+  return new _Just(value);
+}
+
+export interface Nothing<a = never> extends _Maybe<a> {
   isJust: false;
   isNothing: true;
 }
-const returnNothing = () => Nothing;
-export const Nothing: Maybe<never> = {
-  isJust: false,
-  isNothing: true,
-  *[Symbol.iterator]() {},
-  chain: returnNothing,
-  map: returnNothing,
-  safeMap: returnNothing,
-  then(_, f) {
-    return f();
-  },
-};
+class _Nothing<a = never> extends _Maybe<a> implements Nothing<a> {
+  isJust: false = false;
+  isNothing: true = true;
+  match<b>(Nothing: () => b, Just: (value: a) => b): b {
+    return Nothing();
+  }
+}
+export const Nothing: Nothing<never> = /* @__PURE__ */ new _Nothing();
 
 export function all<T extends Array<Maybe<any>>>(...maybes: T): AllMaybes<T>;
 export function all(...maybes: Maybe<unknown>[]): Maybe<unknown[]> {
@@ -60,7 +73,10 @@ export function all(...maybes: Maybe<unknown>[]): Maybe<unknown[]> {
   return Just(results);
 }
 
+export function lift2<a, b, c>(f: (_: a) => (_: b) => c, fx: Maybe<a>, fy: Maybe<b>): Maybe<c> {
+  return Just(f).apply(fx).apply(fy);
+}
+
 export function fromNullish<T>(value: T | null | undefined): Maybe<T> {
-  if (value == null) return Nothing;
-  return Just(value);
+  return Just(value).safeMap(x => x);
 }
