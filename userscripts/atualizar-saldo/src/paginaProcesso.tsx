9cbca76dd45df7148f5020dc50c1787fda57fdc4
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2022-09-19 10:54:47 -0300

    Lida com clique no link de abertura

--- userscripts/atualizar-saldo/src/paginaProcesso.tsx
+++ userscripts/atualizar-saldo/src/paginaProcesso.tsx
@@ -14,64 +14,6 @@ export function paginaProcesso(numproc: NumProc): Either<Error[], void> {
   );
 }
 
-const Estado = createTaggedUnion({
-  AguardaVerificacaoSaldo: (tentativas: number) => tentativas,
-  ComConta: null,
-  SemConta: null,
-  Erro: null,
-});
-type Estado = Static<typeof Estado>;
-
-const Acao = createTaggedUnion({ Tick: null, Clique: null });
-type Acao = Static<typeof Acao>;
-
-function criarArmazem({
-  link,
-  numproc,
-  url,
-}: {
-  link: HTMLAnchorElement;
-  numproc: NumProc;
-  url: string;
-}) {
-  return createStore<Estado, Acao>(
-    () => Estado.AguardaVerificacaoSaldo(0),
-    (estado, acao) =>
-      acao.match({
-        Tick: () =>
-          estado.match(
-            {
-              AguardaVerificacaoSaldo: tentativas => {
-                if (link.textContent === '') {
-                  if (tentativas > 30) return Estado.Erro;
-                  return Estado.AguardaVerificacaoSaldo(tentativas + 1);
-                }
-                if (link.textContent === 'Há conta com saldo') return Estado.ComConta;
-                return Estado.SemConta;
-              },
-              Erro: () => estado,
-            },
-            () => Estado.Erro
-          ),
-        Clique: () =>
-          estado.match(
-            {
-              ComConta: () => {
-                adicionarProcessoAguardando(numproc);
-                window.open(url);
-                return estado;
-              },
-              AguardaVerificacaoSaldo: () => Estado.Erro,
-            },
-            () => {
-              window.open(url);
-              return estado;
-            }
-          ),
-      })
-  );
-}
-
 function modificarPaginaProcesso({
   informacoesAdicionais,
   link,
@@ -83,7 +25,53 @@ function modificarPaginaProcesso({
   numproc: NumProc;
   url: string;
 }) {
-  const store = criarArmazem({ link, numproc, url });
+  const Estado = createTaggedUnion({
+    AguardaVerificacaoSaldo: (tentativas: number) => ({ tentativas }),
+    ComConta: (verificarAutomaticamente: boolean = false) => ({ verificarAutomaticamente }),
+    SemConta: null,
+    Erro: null,
+  });
+  type Estado = Static<typeof Estado>;
+
+  const Acao = createTaggedUnion({ Tick: null, Clique: (alvo: 'BOTAO' | 'LINK') => ({ alvo }) });
+  type Acao = Static<typeof Acao>;
+
+  const store = createStore<Estado, Acao>(
+    () => Estado.AguardaVerificacaoSaldo(0),
+    (estado, acao) =>
+      acao.match({
+        Tick: () =>
+          estado.match(
+            {
+              AguardaVerificacaoSaldo: ({ tentativas }) => {
+                if (link.textContent === '') {
+                  if (tentativas > 30) return Estado.Erro;
+                  return Estado.AguardaVerificacaoSaldo(tentativas + 1);
+                }
+                if (link.textContent === 'Há conta com saldo') return Estado.ComConta();
+                return Estado.SemConta;
+              },
+              Erro: () => estado,
+            },
+            () => Estado.Erro
+          ),
+        Clique: ({ alvo }) =>
+          estado.match(
+            {
+              ComConta: () => {
+                adicionarProcessoAguardando(numproc);
+                window.open(url);
+                return Estado.ComConta(alvo === 'BOTAO');
+              },
+              AguardaVerificacaoSaldo: () => Estado.Erro,
+            },
+            () => {
+              window.open(url);
+              return estado;
+            }
+          ),
+      })
+  );
 
   {
     const timer = window.setInterval(() => {
@@ -103,52 +91,57 @@ function modificarPaginaProcesso({
       div = document.createElement('div');
       div.className = 'gm-atualizar-saldo__processo';
       informacoesAdicionais.insertAdjacentElement('beforebegin', div);
+      link.addEventListener('click', onClick);
     }
     render(<App {...{ estado, onClick }} />, div);
   });
 
   function onClick(evt: Event) {
     evt.preventDefault();
-    store.dispatch(Acao.Clique);
-  }
-}
-
-function App(props: { estado: Estado; onClick: Handler<Event> }): JSX.Element;
-function App({ estado, onClick }: { estado: Estado; onClick: Handler<Event> }) {
-  if (estado.tag === 'AguardaVerificacaoSaldo') {
-    return <output>Verificando contas com saldo...</output>;
+    if (evt.target === link) {
+      store.dispatch(Acao.Clique('LINK'));
+    } else {
+      store.dispatch(Acao.Clique('BOTAO'));
+    }
   }
 
-  interface InfoMensagem {
-    classe: string;
-    mensagem: string;
-    rotuloBotao: string;
+  function App(props: { estado: Estado; onClick: Handler<Event> }): JSX.Element;
+  function App({ estado, onClick }: { estado: Estado; onClick: Handler<Event> }) {
+    if (estado.tag === 'AguardaVerificacaoSaldo') {
+      return <output>Verificando contas com saldo...</output>;
+    }
+
+    interface InfoMensagem {
+      classe: string;
+      mensagem: string;
+      rotuloBotao: string;
+    }
+    const infoMensagem: InfoMensagem = match(estado, {
+      ComConta: () => ({
+        classe: 'saldo',
+        mensagem: 'Há conta(s) com saldo.',
+        rotuloBotao: 'Atualizar',
+      }),
+      SemConta: () => ({
+        classe: 'zerado',
+        mensagem: 'Sem saldo em conta(s).',
+        rotuloBotao: 'Abrir página',
+      }),
+      Erro: () => ({
+        classe: 'erro',
+        mensagem: 'Ocorreu um erro com a atualização de saldo de RPV.',
+        rotuloBotao: 'Abrir página',
+      }),
+    });
+    return (
+      <>
+        <span class={infoMensagem.classe}>{infoMensagem.mensagem}</span>{' '}
+        <button type="button" onClick={onClick} class={infoMensagem.classe}>
+          {infoMensagem.rotuloBotao}
+        </button>
+      </>
+    );
   }
-  const infoMensagem: InfoMensagem = match(estado, {
-    ComConta: () => ({
-      classe: 'saldo',
-      mensagem: 'Há conta(s) com saldo.',
-      rotuloBotao: 'Atualizar',
-    }),
-    SemConta: () => ({
-      classe: 'zerado',
-      mensagem: 'Sem saldo em conta(s).',
-      rotuloBotao: 'Abrir página',
-    }),
-    Erro: () => ({
-      classe: 'erro',
-      mensagem: 'Ocorreu um erro com a atualização de saldo de RPV.',
-      rotuloBotao: 'Abrir página',
-    }),
-  });
-  return (
-    <>
-      <span class={infoMensagem.classe}>{infoMensagem.mensagem}</span>{' '}
-      <button type="button" onClick={onClick} class={infoMensagem.classe}>
-        {infoMensagem.rotuloBotao}
-      </button>
-    </>
-  );
 }
 
 function obterLink() {
