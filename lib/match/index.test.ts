8c8059f8eb57cbebfbe77a0a7b74242d3fddee27
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2022-09-23 15:31:43 -0300

    Testes melhores

--- lib/match/index.test.ts
+++ lib/match/index.test.ts
@@ -87,6 +87,119 @@ test('no matcher', () => {
   );
 });
 
+describe('Unusual tagName and tag', () => {
+  test('number as tagName', () => {
+    const MyType = createTaggedUnion({ a: null, b: (value: string) => ({ value }) }, 0);
+    const typeA = MyType.a;
+    const typeB = MyType.b('abc');
+    type MyType = Static<typeof MyType>;
+    const resultA = MyType.match(typeA as MyType, { a: () => true }, () => false);
+    expect(resultA).toBe(true);
+    const resultB = MyType.match(typeB as MyType, { b: x => x.value === '3' }, () => true);
+    expect(resultB).toBe(false);
+
+    type OriginalType = [tag: 'v1', value: string] | [tag: 'v2', value: number];
+    const match = matchBy(0);
+    const variant1: OriginalType = ['v1', 'abc'];
+    const variant2: OriginalType = ['v2', 123];
+    const res1 = match(variant1 as OriginalType, { v1: x => x[1].length, v2: x => x[1] });
+    expect(res1).toBe(3);
+    const res2 = match(variant2 as OriginalType, { v1: x => x[1].length, v2: x => x[1] });
+    expect(res2).toBe(123);
+  });
+  test('numbers as tags', () => {
+    const T = createTaggedUnion({ 0: null, 1: (value: string) => ({ value }) }, 'discriminant');
+    type T = Static<typeof T>;
+    const t0 = T[0];
+    const t1 = T[1]('hi');
+    const tRes0 = T.match(t0 as T, [x => x.discriminant.toString(2), x => x.value] as const);
+    expect(tRes0).toBe('0');
+    const tRes1 = T.match(t1 as T, [x => x.discriminant.toString(2), x => x.value] as const);
+    expect(tRes1).toBe('hi');
+
+    type U = { tag: 0 } | { tag: 1; value: string };
+    const u0: U = { tag: 0 };
+    const u1: U = { tag: 1, value: 'hi' };
+    const match = matchBy('tag');
+    const uRes0 = match(u0 as U, [x => x.tag.toString(2), x => x.value] as const);
+    expect(uRes0).toBe('0');
+    const uRes1 = match(u1 as U, [x => x.tag.toString(2), x => x.value] as const);
+    expect(uRes1).toBe('hi');
+  });
+  test('enums as tags', () => {
+    enum Tags {
+      FIRST,
+      SECOND,
+    }
+    const MyType = createTaggedUnion(
+      { [Tags.FIRST]: null, [Tags.SECOND]: (value: string) => ({ value }) },
+      'discriminant'
+    );
+    type MyType = Static<typeof MyType>;
+    const first = MyType[Tags.FIRST];
+    const res1 = MyType.match(first as MyType, {
+      [Tags.FIRST]: () => 8,
+      [Tags.SECOND]: x => x.value.length,
+    });
+    expect(res1).toEqual(8);
+    const second = MyType[Tags.SECOND]('xyz');
+    const res2 = MyType.match(second as MyType, {
+      [Tags.FIRST]: () => 8,
+      [Tags.SECOND]: x => x.value.length,
+    });
+    expect(res2).toEqual(3);
+  });
+  test('const enums as tags', () => {
+    const enum Tags {
+      FIRST,
+      SECOND,
+    }
+    const MyType = createTaggedUnion(
+      { [Tags.FIRST]: null, [Tags.SECOND]: (value: string) => ({ value }) },
+      'discriminant'
+    );
+    type MyType = Static<typeof MyType>;
+    const first = MyType[Tags.FIRST];
+    const res1 = MyType.match(first as MyType, {
+      [Tags.FIRST]: () => 8,
+      [Tags.SECOND]: x => x.value.length,
+    });
+    expect(res1).toEqual(8);
+    const second = MyType[Tags.SECOND]('xyz');
+    const res2 = MyType.match(second as MyType, {
+      [Tags.FIRST]: () => 8,
+      [Tags.SECOND]: x => x.value.length,
+    });
+    expect(res2).toEqual(3);
+  });
+  test('symbol as tagName', () => {
+    const Sym = Symbol();
+    const MyType = createTaggedUnion({ a: null, b: (value: string) => ({ value }) }, Sym);
+    const typeA = MyType.a;
+    const typeB = MyType.b('abc');
+    type MyType = Static<typeof MyType>;
+    const resultA = MyType.match(typeA as MyType, { a: () => true }, () => false);
+    expect(resultA).toBe(true);
+    const resultB = MyType.match(typeB as MyType, { b: x => x.value === '3' }, () => true);
+    expect(resultB).toBe(false);
+  });
+  test('symbols as tags', () => {
+    const a = Symbol();
+    const b = Symbol();
+    const MyType = createTaggedUnion(
+      { [a]: null, [b]: (value: string) => ({ value }) },
+      'myTagName'
+    );
+    const typeA = MyType[a];
+    const typeB = MyType[b]('abc');
+    type MyType = Static<typeof MyType>;
+    const resultA = MyType.match(typeA as MyType, { [a]: () => true }, () => false);
+    expect(resultA).toBe(true);
+    const resultB = MyType.match(typeB as MyType, { [b]: x => x.value === '3' }, () => true);
+    expect(resultB).toBe(false);
+  });
+});
+
 test.skip('typescript (constructors)', () => {
   const MaybeString = createTaggedUnion({ Nothing: null, Just: (x: string) => ({ value: x }) });
   type MaybeString = Static<typeof MaybeString>;
@@ -153,23 +266,30 @@ test.skip('typescript (manual creation)', () => {
   const y3: number = x3;
 });
 
-test.skip('TypeScript - correct usage', () => {
-  const StringOrEmpty = createTaggedUnion({ Str: (value: string) => ({ value }), Empty: null });
-});
-
-test.skip('TypeScript - result must be object', () => {
+test('result must be object', () => {
   // @ts-expect-error
-  const Err = createTaggedUnion({ Str: (value: string) => value });
-});
-
-test.skip('TypeScript - match not allowed', () => {
+  const MyType = createTaggedUnion({ Str: (value: string) => value });
   // @ts-expect-error
-  const Err = createTaggedUnion({ match: null });
+  expect(() => MyType.Str('hello')).toThrow();
 });
 
-test.skip('TypeScript - tag in result not allowed', () => {
+test('match not allowed', () => {
+  // @ts-expect-error
+  expect(() => createTaggedUnion({ match: null })).toThrow();
+});
+
+test('tag in result not allowed', () => {
   // @ts-expect-error
   const Err1 = createTaggedUnion({ Str: (x?: string) => ({ tag: x }) });
   // @ts-expect-error
+  const err1 = Err1.Str('hey');
+  expect(err1).not.toHaveProperty('tag', 'hey');
+  expect(err1).toHaveProperty('tag', 'Str');
+
+  // @ts-expect-error
   const Err2 = createTaggedUnion({ Str: (x?: string) => ({ variant: x }) }, 'variant');
+  // @ts-expect-error
+  const err2 = Err2.Str('hey');
+  expect(err2).not.toHaveProperty('variant', 'hey');
+  expect(err2).toHaveProperty('variant', 'Str');
 });
--- lib/match/index.ts
+++ lib/match/index.ts
@@ -1,60 +1,37 @@
+type ValidTagName = string | number | symbol;
+type ValidTag = string | number | symbol;
 type CreatorFn<
-  Tag extends keyof any,
+  Tag extends ValidTagName,
   Args extends any[] = any[],
   R extends Record<keyof any, unknown> = Record<keyof any, unknown>
-> = (...args: Args) => Result<Tag, R>;
-type Definitions<Tag extends keyof any, D> = {
+> = (...args: Args) => ValidResult<Tag, R>;
+type Definitions<Tag extends ValidTagName, D extends Record<keyof D, unknown>> = {
   [K in keyof D]: K extends 'match' ? never : CreatorFn<Tag> | null;
 };
-type Result<
-  Tag extends keyof any,
+type ValidResult<
+  Tag extends ValidTagName,
   R extends Record<keyof any, unknown> = Record<keyof any, unknown>
-> = R & {
-  [T in Tag]?: never;
-};
-type CVal<Name extends keyof any = keyof any> = [Name];
-type CFn<
-  Name extends keyof any = keyof any,
-  Args extends any[] = any[],
-  Data extends [keyof any, unknown] = [keyof any, unknown]
-> = [Name, Args, Data];
-type Def<
-  Name extends keyof any = keyof any,
-  Data extends [keyof any, unknown] = [keyof any, unknown]
-> = CFn<Name, any[], Data>;
-type Join<T> = (T extends never ? never : (_: T) => void) extends (_: infer U) => void
-  ? Simplify<U>
-  : never;
-type Tagged<
-  TagName extends keyof any,
-  Tag extends keyof any,
-  Extra extends [keyof any, unknown]
-> = {
+> = R & { [T in Tag]?: never };
+type Tagged<TagName extends ValidTagName, Tag extends ValidTag, Value> = Value & {
   [K in TagName]: Tag;
-} & { [K in Extra[0]]: Extra extends [K, infer R] ? R : never };
-type Simplify<T> = { result: { [K in keyof T]: T[K] } }['result'];
-type Constructors<TagName extends keyof any, D extends CFn | CVal> = {
-  [Tag in 'match' | D[0]]: Tag extends 'match'
-    ? MatchBy<TagName>
-    : D extends CFn<Tag, infer A, infer R>
-    ? (...args: A) => Tagged<TagName, Tag, R>
-    : D extends CVal<Tag>
-    ? Tagged<TagName, Tag, never>
-    : never;
 };
-type FromDef<TagName extends keyof any, D extends CFn | CVal> = D extends CFn<
-  infer K,
-  any[],
-  infer R
->
-  ? Tagged<TagName, K, R>
-  : D extends CVal<infer K>
-  ? Tagged<TagName, K, never>
-  : never;
-type Tagged__<TagName extends keyof any> = Tagged_<TagName, keyof any>;
-type Tagged_<TagName extends keyof any, Tag extends keyof any> = Tagged<TagName, Tag, never>;
+type Constructors<TagName extends ValidTagName, D extends Definitions<TagName, D>> = {
+  result: {
+    [Tag in Extract<keyof D, ValidTag> as Exclude<Tag, 'match'>]: D[Tag] extends CreatorFn<
+      TagName,
+      infer A,
+      infer R
+    >
+      ? (...args: A) => Tagged<TagName, Tag, R>
+      : D[Tag] extends null
+      ? Tagged<TagName, Tag, {}>
+      : never;
+  } & MatchBy<TagName>;
+}['result'];
+type Tagged__<TagName extends ValidTagName> = Tagged_<TagName, ValidTag>;
+type Tagged_<TagName extends ValidTagName, Tag extends ValidTag> = Tagged<TagName, Tag, {}>;
 type Matchers<
-  TagName extends keyof any,
+  TagName extends ValidTagName,
   T extends Tagged__<TagName>,
   Tags extends T[TagName],
   U
@@ -62,17 +39,16 @@ type Matchers<
   [Tag in Tags]: (tagged: Extract<T, Tagged_<TagName, Tag>>) => U;
 };
 type Matchers_<
-  TagName extends keyof any,
+  TagName extends ValidTagName,
   T extends Tagged__<TagName>,
   Tags extends T[TagName]
 > = Matchers<TagName, T, Tags, unknown>;
-type Matchers__<TagName extends keyof any, T extends Tagged__<TagName>> = Matchers_<
+type Matchers__<TagName extends ValidTagName, T extends Tagged__<TagName>> = Matchers_<
   TagName,
   T,
   T[TagName]
 >;
-type Matchers___<TagName extends keyof any> = Matchers__<TagName, Tagged__<TagName>>;
-type MatchBy<TagName extends keyof any> = {
+interface MatchBy<TagName extends ValidTagName> {
   match<T extends Tagged__<TagName>, M extends Matchers__<TagName, T>>(
     tagged: T,
     matchers: M
@@ -82,29 +58,18 @@ type MatchBy<TagName extends keyof any> = {
     matchers: { [K in P]: (tagged: Extract<T, Tagged_<TagName, K>>) => U },
     otherwise: (tagged: Exclude<T, Tagged_<TagName, P>>) => U
   ): U;
-}['match'];
+}
 
-type GetResult<All extends keyof any, P extends All, U> = [
-  Exclude<All, P>
-] extends infer Remaining extends [any]
-  ? Remaining extends [never]
-    ? U
-    : { error: Remaining[0] }
-  : never;
-
-export function createTaggedUnion<D extends Definitions<Tag, D>, Tag extends keyof any = 'tag'>(
+export function createTaggedUnion<D extends Definitions<Tag, D>, Tag extends ValidTagName = 'tag'>(
   definitions: D,
   tagName: Tag = 'tag' as Tag
-): Constructors<
-  Tag,
-  {
-    [K in keyof D]: D[K] extends CreatorFn<Tag, infer A, infer R>
-      ? CFn<K, A, { [K in keyof R]: [K, R[K]] }[keyof R]>
-      : CVal<K>;
-  }[keyof D]
-> {
-  const ctors: Partial<Record<keyof D, object | Function>> = {};
-  for (const [tag, f] of Object.entries(definitions) as [keyof D, CreatorFn<Tag> | null][]) {
+): Constructors<Tag, D> {
+  const ctors: Record<keyof D, object | Function> = {} as any;
+  for (const tag of (Object.getOwnPropertyNames(definitions) as Array<keyof D>).concat(
+    Object.getOwnPropertySymbols(definitions) as Array<keyof D>
+  )) {
+    if (tag === 'match') throw new Error('Invalid tag: "match".');
+    const f = definitions[tag];
     if (f === null) {
       ctors[tag] = { [tagName]: tag };
     } else {
@@ -115,14 +80,15 @@ export function createTaggedUnion<D extends Definitions<Tag, D>, Tag extends key
       };
     }
   }
-  (ctors as any).match = matchBy(tagName);
+  (ctors as Constructors<Tag, D>).match = matchBy(tagName);
   return ctors as any;
 }
 
-export function matchBy<T extends keyof any>(tagName: T): MatchBy<T> {
+export function matchBy<T extends ValidTagName>(tagName: T): MatchBy<T>['match'] {
   return (obj: any, matchers: any, otherwise?: Function) => {
     const tag = obj[tagName];
-    if (!tag) throw new Error(`Object does not have a valid "${String(tagName)}" property.`);
+    if (tag === undefined)
+      throw new Error(`Object does not have a valid "${String(tagName)}" property.`);
     const fn = matchers[tag] ?? otherwise ?? matchNotFound;
     return fn(obj);
   };
@@ -133,8 +99,6 @@ export function matchBy<T extends keyof any>(tagName: T): MatchBy<T> {
 
 export type Static<C extends Record<keyof C, unknown>> = {
   result: {
-    [K in keyof C]: K extends 'match'
-      ? never
-      : Simplify<C[K] extends (...args: any[]) => infer R ? R : C[K]>;
+    [K in keyof C]: K extends 'match' ? never : C[K] extends (...args: any[]) => infer R ? R : C[K];
   }[keyof C];
 }['result'];
