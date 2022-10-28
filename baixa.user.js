9c51bcc4b9a307f282ddb0b46d85ba27ed4b29ff
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2022-10-28 14:48:56 -0300

    Correção de algumas janeas que não fechavam automaticamente

--- baixa.user.js
+++ baixa.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name Preenchimento dados baixa
-// @version 6.0.0
+// @version 6.0.1
 // @author nadameu
 // @namespace http://nadameu.com.br/baixa
 // @include /^https:\/\/eproc\.(jf(pr|rs|sc)|trf4)\.jus\.br\/eproc(V2|2trf4)\/controlador\.php\?acao=baixa_arquivamento_processo_etapa_(1|3)&/
 // @updateUrl https://github.com/nadameu/greasemonkey/raw/master/baixa.meta.js
 // @downloadUrl https://github.com/nadameu/greasemonkey/raw/master/baixa.user.js
-// @grant none
+// @grant window.close
 // ==/UserScript==
 
 function adicionarEstilos(estilos) {
