7eb5fc195aef974e75e3daf181af9f8bb5e073e5
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2023-08-01 17:20:52 -0300

    Configuração

--- userscripts/achei/vite.config.ts
+++ userscripts/achei/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import monkey from 'vite-plugin-monkey';
 
-const protocolos = ['http', 'https'];
 const dominios = ['centralrh.trf4.gov.br', 'serh.trf4.jus.br'];
+const protocolos = ['http', 'https'];
 const sufixos = ['', '&*'];
 
 // https://vitejs.dev/config/
@@ -14,14 +14,26 @@ export default defineConfig({
       userscript: {
         name: 'Achei',
         namespace: 'http://nadameu.com.br/achei',
-        match: dominios.flatMap(dominio =>
-          protocolos.flatMap(protocolo =>
-            sufixos.map(
-              sufixo => `${protocolo}://${dominio}/achei/pesquisar.php?acao=pesquisar${sufixo}`
-            )
-          )
+        match: zip3(
+          dominios,
+          protocolos,
+          sufixos,
+          (dominio, protocolo, sufixo) =>
+            `${protocolo}://${dominio}/achei/pesquisar.php?acao=pesquisar${sufixo}`
         ),
       },
     }),
   ],
 });
+
+function apply<A, B>(fs: Array<(_: A) => B>, xs: A[]): B[] {
+  return fs.flatMap(f => xs.map(x => f(x)));
+}
+
+function zip3<A, B, C, D>(as: A[], bs: B[], cs: C[], f: (a: A, b: B, c: C) => D): D[] {
+  return apply(apply(as.map(curry3(f)), bs), cs);
+}
+
+function curry3<A, B, C, D>(f: (a: A, b: B, c: C) => D): (_: A) => (_: B) => (_: C) => D {
+  return a => b => c => f(a, b, c);
+}
