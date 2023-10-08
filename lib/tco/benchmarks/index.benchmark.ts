9db85189614320f35d32e3892c378aeb24b7b16c
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2023-10-08 02:58:33 -0300

    Better benchmarks

--- lib/tco/benchmarks/index.benchmark.ts
+++ lib/tco/benchmarks/index.benchmark.ts
@@ -1,47 +1,28 @@
-import { beforeAll, bench, describe } from 'vitest';
-import * as recursion from '../examples/recursion';
-import * as tailCall from '../examples/tail-call';
-import * as iterative from '../examples/iterative';
-import * as trampoline from '../examples/trampoline';
+import { bench, describe } from 'vitest';
+import { generate } from '../examples/List';
 import * as auto from '../examples/auto';
-import * as step from '../examples/step';
 import * as cont from '../examples/cont';
+import * as iterative from '../examples/iterative';
 import * as recursively from '../examples/recursively';
-import * as promises from '../examples/promises';
-import * as async_ from '../examples/async';
-import { List, generate } from '../examples/List';
+import * as step from '../examples/step';
+import * as trampoline from '../examples/trampoline';
 
-describe.each([
-  ['Small # of iterations', 10_000],
-  ['Large # of iterations', 800_000],
-])('%s', (_, SIZE) => {
+describe.each([800e3])('Sum over a list with %i numbers', SIZE => {
   const list = generate(SIZE);
   bench('iterative', () => {
     iterative.sum(list);
   });
-  bench.skipIf(SIZE > 32768)('direct recursion', () => {
-    recursion.sum(list);
-  });
-  bench.skipIf(SIZE > 32768)('tail-call', () => {
-    tailCall.sum(list);
-  });
   bench('step', () => {
     step.sum(list);
-  });
-  bench('trampoline', () => {
-    trampoline.sum(list);
   });
   bench('auto-trampoline', () => {
     auto.sum(list);
   });
+  bench('trampoline', () => {
+    trampoline.sum(list);
+  });
   bench('cont', () => {
     cont.sum(list);
-  });
-  bench.skipIf(SIZE > 32768)('promises', async () => {
-    await promises.sum(list);
-  });
-  bench.skipIf(SIZE > 32768)('async', async () => {
-    await async_.sum(list);
   });
   bench('recursively', () => {
     recursively.sum(list);
