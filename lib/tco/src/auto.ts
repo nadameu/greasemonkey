6119a983b44c9db356f42297da670c7996306a2d
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2023-10-08 02:59:01 -0300

    Return early only after a number of iterations

--- lib/tco/src/auto.ts
+++ lib/tco/src/auto.ts
@@ -1,13 +1,23 @@
 import { Trampoline, loop, run } from './trampoline';
 
-export const auto = <T extends unknown[], U>(f: (...args: T) => U): { (...args: T): U } => {
-  let running = false;
+export const auto = <T extends unknown[], U>(
+  f: (...args: T) => U,
+  maxRecursion = 2048
+): { (...args: T): U } => {
+  let running = 0;
   function rec(...args: T): Trampoline<U> {
-    // TODO: return early only after a number of iterations
-    if (running) return loop(() => f(...args));
-    running = true;
+    if (running > 0) {
+      if (running < maxRecursion) {
+        running++;
+        return f(...args);
+      } else {
+        running = 1;
+        return loop(() => f(...args));
+      }
+    }
+    running = 1;
     let result = run(f(...args));
-    running = false;
+    running = 0;
     return result;
   }
   return rec as { (...args: T): U };
