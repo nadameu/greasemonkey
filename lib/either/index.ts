f80443dd8ce1f87c4542c4444c6b07f61d992ed9
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2023-10-13 00:32:35 -0300

    Refatoração

--- lib/either/index.ts
+++ lib/either/index.ts
@@ -58,52 +58,80 @@ export function Right<b, a = never>(rightValue: b): Either<a, b> {
   return new _Right(rightValue);
 }
 
-export function sequence<a, b>(eithers: [Either<a, b>]): Either<a, [b]>;
-export function sequence<a, b, c>(eithers: [Either<a, b>, Either<a, c>]): Either<a, [b, c]>;
-export function sequence<a, b, c, d>(
-  eithers: [Either<a, b>, Either<a, c>, Either<a, d>]
-): Either<a, [b, c, d]>;
-export function sequence<a, b>(eithers: Iterable<Either<a, b>>): Either<a, b[]>;
-export function sequence<a, b>(eithers: Iterable<Either<a, b>>): Either<a, b[]> {
+type Sequenced<a, es extends Array<Either<a, unknown>>, bs extends unknown[] = []> = es extends []
+  ? Either<a, bs>
+  : es extends [Either<a, infer b>, ...infer rest extends Array<Either<a, unknown>>]
+  ? Sequenced<a, rest, [...bs, b]>
+  : es extends Array<Either<a, infer b>>
+  ? Sequenced<a, [], [...bs, ...b[]]>
+  : never;
+export function sequence<a, es extends Array<Either<a, unknown>>>(
+  eithers: [...es]
+): Sequenced<a, es>;
+export function sequence<a, b>(
+  eithers: Iterable<Either<a, b>> | ArrayLike<Either<a, b>>
+): Either<a, b[]>;
+export function sequence<a, b>(
+  eithers: Iterable<Either<a, b>> | ArrayLike<Either<a, b>>
+): Either<a, b[]> {
   return traverse(eithers, x => x);
 }
 
 export function traverse<a, b, c>(
-  collection: Iterable<a>,
+  collection: Iterable<a> | ArrayLike<a>,
   transform: (x: a, i: number) => Either<b, c>
 ): Either<b, c[]> {
   const results: c[] = [];
-  let i = 0;
-  for (const value of collection) {
-    const either = transform(value, i++);
-    if (either.isLeft) return either as Left<b>;
+  for (const [index, value] of Array.from(collection).entries()) {
+    const either = transform(value, index);
+    if (either.isLeft) return either as Left<b, never>;
     results.push(either.rightValue);
   }
   return Right(results);
 }
 
-export function validateAll<a, b>(eithers: [Either<a, b>]): Either<a[], [b]>;
-export function validateAll<a, b, c>(eithers: [Either<a, b>, Either<a, c>]): Either<a[], [b, c]>;
-export function validateAll<a, b, c, d>(
-  eithers: [Either<a, b>, Either<a, c>, Either<a, d>]
-): Either<a[], [b, c, d]>;
-export function validateAll<a, b>(eithers: Iterable<Either<a, b>>): Either<a[], b[]>;
-export function validateAll<a, b>(eithers: Iterable<Either<a, b>>): Either<a[], b[]> {
+export function partition<a, es extends Array<Either<a, unknown>>>(
+  eithers: [...es]
+): Sequenced<a, es> extends Either<a, infer bs> ? { left: a[]; right: bs } : never;
+export function partition<a, b>(
+  eithers: Iterable<Either<a, b>> | ArrayLike<Either<a, b>>
+): { left: a[]; right: b[] };
+export function partition<a, b>(
+  eithers: Iterable<Either<a, b>> | ArrayLike<Either<a, b>>
+): { left: a[]; right: b[] } {
+  return partitionMap(eithers, x => x);
+}
+export function partitionMap<a, b, c>(
+  collection: Iterable<a> | ArrayLike<a>,
+  transform: (value: a, index: number) => Either<b, c>
+): { left: b[]; right: c[] } {
+  const left = [] as b[];
+  const right = [] as c[];
+  for (const [index, value] of Array.from(collection).entries()) {
+    const either = transform(value, index);
+    if (either.isLeft) left.push(either.leftValue);
+    else right.push(either.rightValue);
+  }
+  return { left, right };
+}
+
+export function validateAll<a, es extends Array<Either<a, unknown>>>(
+  eithers: [...es]
+): Sequenced<a, es> extends Either<a, infer bs> ? Either<a[], bs> : never;
+export function validateAll<a, b>(
+  eithers: Iterable<Either<a, b>> | ArrayLike<Either<a, b>>
+): Either<a[], b[]>;
+export function validateAll<a, b>(
+  eithers: Iterable<Either<a, b>> | ArrayLike<Either<a, b>>
+): Either<a[], b[]> {
   return validateMap(eithers, x => x);
 }
 
 export function validateMap<a, b, c>(
-  collection: Iterable<a>,
+  collection: Iterable<a> | ArrayLike<a>,
   transform: (x: a, i: number) => Either<b, c>
 ): Either<b[], c[]> {
-  const errors: b[] = [];
-  const results: c[] = [];
-  let i = 0;
-  for (const value of collection) {
-    const either = transform(value, i++);
-    if (either.isLeft) errors.push(either.leftValue);
-    else results.push(either.rightValue);
-  }
-  if (errors.length > 0) return Left(errors);
-  return Right(results);
+  const partitioned = partitionMap(collection, transform);
+  if (partitioned.left.length > 0) return Left(partitioned.left);
+  else return Right(partitioned.right);
 }
