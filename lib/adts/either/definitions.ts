cebb98d2c29c51c53e42cd4cc060864c5e6cf86e
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2023-11-07 15:11:38 -0300

    Tagged union

--- lib/adts/either/definitions.ts
+++ lib/adts/either/definitions.ts
@@ -5,11 +5,14 @@ type EitherU<a, b> = TaggedUnion<
   'Either',
   { Left: { left: a }; Right: { right: b } }
 >;
-const tagEither = tag<EitherF>('Either');
+const tagEither = /* #__PURE__ */ tag<EitherF>('Either');
+
 export interface Left<a> extends MemberOf<EitherU<a, never>, 'Left'> {}
 export const Left = <a>(left: a) => tagEither('Left')({ left });
-export const isLeft = isTagged<EitherF, 'Left'>('Left');
+export const isLeft = /* #__PURE__ */ isTagged<EitherF, 'Left'>('Left');
+
 export interface Right<b> extends MemberOf<EitherU<never, b>, 'Right'> {}
 export const Right = <b>(right: b) => tagEither('Right')({ right });
-export const isRight = isTagged<EitherF, 'Right'>('Right');
+export const isRight = /* #__PURE__ */ isTagged<EitherF, 'Right'>('Right');
+
 export type Either<a, b> = Left<a> | Right<b>;
--- lib/adts/list/definitions.ts
+++ lib/adts/list/definitions.ts
@@ -1,21 +1,25 @@
+import { MemberOf, TaggedUnion, isTagged, tag } from '../tagged';
+import { Kind } from '../typeclasses';
+
+type ListU<a> = TaggedUnion<
+  'List',
+  { Cons: { head: a; tail: List<a> }; Nil: {} }
+>;
+const tagList = /* #__PURE__ */ tag<ListF>('List');
+
+export interface Cons<a> extends MemberOf<ListU<a>, 'Cons'> {}
+export const Cons = <a>(head: a, tail: List<a>) =>
+  tagList('Cons')({ head, tail });
+export const isCons: <a>(list: List<a>) => list is Cons<a> =
+  /* #__PURE__ */ isTagged<ListF, 'Cons'>('Cons');
+
+export interface Nil extends MemberOf<ListU<never>, 'Nil'> {}
+export const Nil: Nil = /* #__PURE__ */ tagList('Nil')({});
+export const isNil: <a>(list: List<a>) => list is Nil =
+  /* #__PURE__ */ isTagged<ListF, 'Nil'>('Nil');
+
 export type List<a> = Cons<a> | Nil;
-export interface Cons<a> {
-  _type: 'List';
-  _tag: 'Cons';
-  head: a;
-  tail: List<a>;
+
+interface ListF extends Kind {
+  type: List<this['a']>;
 }
-export const Cons = <a>(head: a, tail: List<a>): Cons<a> => ({
-  _type: 'List',
-  _tag: 'Cons',
-  head,
-  tail,
-});
-export const isCons = <a>(list: List<a>): list is Cons<a> =>
-  list._tag === 'Cons';
-export interface Nil {
-  _type: 'List';
-  _tag: 'Nil';
-}
-export const Nil: Nil = { _type: 'List', _tag: 'Nil' };
-export const isNil = <a>(list: List<a>): list is Nil => list._tag === 'Nil';
