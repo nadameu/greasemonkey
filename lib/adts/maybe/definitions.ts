7baf72203557a2e7b6316851a3d5c3292872279c
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2023-11-07 15:15:14 -0300

    Tagged union

--- lib/adts/maybe/definitions.ts
+++ lib/adts/maybe/definitions.ts
@@ -1,20 +1,17 @@
+import { MemberOf, TaggedUnion, isTagged, tag } from '../tagged';
+import { MaybeF } from './internal';
+
+type MaybeU<a> = TaggedUnion<'Maybe', { Just: { value: a }; Nothing: {} }>;
+const tagMaybe = /* #__PURE__ */ tag<MaybeF>('Maybe');
+
+export interface Just<a> extends MemberOf<MaybeU<a>, 'Just'> {}
+export const Just = <a>(value: a) => tagMaybe('Just')({ value });
+export const isJust: <a>(maybe: Maybe<a>) => maybe is Just<a> =
+  /* #__PURE__ */ isTagged<MaybeF, 'Just'>('Just');
+
+export interface Nothing extends MemberOf<MaybeU<never>, 'Nothing'> {}
+export const Nothing: Nothing = /* #__PURE__ */ tagMaybe('Nothing')({});
+export const isNothing: <a>(maybe: Maybe<a>) => maybe is Nothing =
+  /* #__PURE__ */ isTagged<MaybeF, 'Nothing'>('Nothing');
+
 export type Maybe<a> = Just<a> | Nothing;
-export interface Just<a> {
-  _type: 'Maybe';
-  _tag: 'Just';
-  value: a;
-}
-export const Just = <a>(value: a): Just<a> => ({
-  _type: 'Maybe',
-  _tag: 'Just',
-  value,
-});
-export const isJust = <a>(maybe: Maybe<a>): maybe is Just<a> =>
-  maybe._tag === 'Just';
-export interface Nothing {
-  _type: 'Maybe';
-  _tag: 'Nothing';
-}
-export const Nothing: Nothing = { _type: 'Maybe', _tag: 'Nothing' };
-export const isNothing = <a>(maybe: Maybe<a>): maybe is Nothing =>
-  maybe._tag === 'Nothing';
