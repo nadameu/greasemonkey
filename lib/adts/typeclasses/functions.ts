5d6c53aaf42fc79fd96ad79c1261f7811316b6e5
Paulo R. Maurici Jr. <paulo@nadameu.com.br>
2023-11-07 01:45:04 -0300

    Rename

--- lib/adts/typeclasses/functions.ts
+++ lib/adts/typeclasses/functions.ts
@@ -1,20 +1,23 @@
 import { Apply, FlatMap, Functor, Kind, Of, Type } from './definitions';
 
-export const map =
+const deriveMap =
   <F extends Kind>(M: Of<F> & FlatMap<F>): Functor<F>['map'] =>
   f =>
     M.flatMap(x => M.of(f(x)));
+export { deriveMap as map };
 
-export const ap =
+const deriveAp =
   <F extends Kind>(M: Of<F> & FlatMap<F>) =>
   <e, a>(fa: Type<F, e, a>) =>
   <b>(ff: Type<F, e, (_: a) => b>): Type<F, e, b> =>
     M.flatMap<(_: a) => b, b, e>(f => M.flatMap<a, b, e>(a => M.of(f(a)))(fa))(
       ff
     );
+export { deriveAp as ap };
 
-export const lift2 =
+const deriveLift2 =
   <F extends Kind>(M: Apply<F>) =>
   <a, b, c>(f: (a: a, b: b) => c) =>
   <e>(fa: Type<F, e, a>, fb: Type<F, e, b>): Type<F, e, c> =>
     M.ap(fb)(M.map((a: a) => (b: b) => f(a, b))(fa));
+export { deriveLift2 as lift2 };
