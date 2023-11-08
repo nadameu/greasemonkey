b7cafb2fb8e384b38385fd110515d59a4e26c31a
Paulo R. Maurici Jr. <paulo@nadameu.com.br>
2023-11-08 02:11:47 -0300

    Abertura de janelas

--- userscripts/seeu-movimentacoes/src/main.ts
+++ userscripts/seeu-movimentacoes/src/main.ts
@@ -1,8 +1,20 @@
-import { D, E, M, S, Seq, isNothing } from '@nadameu/adts';
+import {
+  D,
+  E,
+  Left,
+  M,
+  Right,
+  S,
+  T,
+  applicativeEither,
+  isNothing,
+  tailRec,
+  tuple,
+} from '@nadameu/adts';
 import { Handler } from '@nadameu/handler';
 import { pipe } from '@nadameu/pipe';
+import * as P from '@nadameu/predicates';
 import css from './estilos.scss?inline';
-import { eitherBool } from '@nadameu/adts/either/functions';
 
 interface Noop {
   (): void;
@@ -41,6 +53,37 @@ export const main = () => {
   }
   const style = document.head.appendChild(document.createElement('style'));
   style.textContent = css;
+  const janelasAbertas = new Map<string, Window>();
+  document.addEventListener('click', evt => {
+    if (
+      evt.target instanceof HTMLElement &&
+      evt.target.matches('a[href][data-gm-doc-link]')
+    ) {
+      const link = evt.target as HTMLAnchorElement;
+      evt.preventDefault();
+      const id = link.dataset.gmDocLink!;
+      if (janelasAbertas.has(id)) {
+        const win = janelasAbertas.get(id)!;
+        if (!win.closed) {
+          win.focus();
+          return;
+        }
+      }
+      const win = window.open(
+        link.href,
+        `doc${id}`,
+        `top=0,left=${(window.screen.width / 6) | 0},width=${
+          ((window.screen.width * 2) / 3) | 0
+        },height=${window.screen.availHeight}`
+      );
+      janelasAbertas.set(id, win!);
+    }
+  });
+  window.addEventListener('beforeunload', () => {
+    for (const win of janelasAbertas.values()) {
+      if (!win.closed) win.close();
+    }
+  });
 };
 
 const createIntersectionObserver = () => {
@@ -117,54 +160,95 @@ const createMutationObserver = () => {
 const onTabelaAdicionada = (table: HTMLTableElement) => {
   const linhas = pipe(
     table.rows,
-    S.map(linha =>
-      pipe(
-        linha.cells,
-        S.filter((_, i) => i % 2 === 0),
-        S.flatMap((celula, c): Seq<Node | string> => {
-          if (c === 0) {
-            return pipe(
-              celula,
-              D.text,
-              M.mapNullable(txt =>
-                txt.match(/^\s*([0-9\.]+)\s+Arquivo:\s+(.+)\s*$/)
-              ),
-              M.map(([, _1, _2]) => [_1!, _2!]),
-              M.getOr([])
-            );
-          } else if (c === 1) {
-            return pipe(
-              celula,
-              D.text,
-              M.mapNullable(txt => txt.match(/^\s*Ass\.:\s+(.*)\s*$/)),
-              M.map(([, _1]) => [_1!]),
-              M.getOr([])
-            );
-          } else {
-            return pipe(
-              celula.childNodes,
-              S.map(x => (x instanceof Text ? x.nodeValue?.trim() ?? '' : x)),
-              S.filter(x => x !== '')
-            );
-          }
-        })
-      )
-    )
-  );
-  const novasLinhas = pipe(
-    linhas,
-    S.map(
-      S.foldLeft<string | Node, HTMLTableRowElement>(
-        document.createElement('tr'),
-        (tr, i) => {
-          const td = document.createElement('td');
-          td.append(i);
-          tr.append(td);
-          return tr;
+    S.traverse(applicativeEither)((linha, l) => {
+      if (linha.cells.length !== 7)
+        return Left(`Formato de linha desconhecido: ${l}.`);
+      const sequencialNome = pipe(
+        linha.cells[0]!,
+        D.text,
+        M.mapNullable(x => x.match(/^\s*(\d+\.\d+)\s+Arquivo:\s+(.*)\s*$/)),
+        M.filter((x): x is [string, string, string] => x.length === 3),
+        M.map(([, sequencial, nome]) => ({ sequencial, nome })),
+        M.toEither(() => `Sequencial e nome não reconhecidos: ${l}.`)
+      );
+      const assinatura = pipe(
+        linha.cells[2]!,
+        D.text,
+        M.mapNullable(x => x.match(/^\s*Ass\.:\s+(.*)\s*$/)),
+        M.filter((x): x is [string, string] => x.length === 2),
+        M.map(([, assinatura]) => ({ assinatura })),
+        M.toEither(() => `Assinatura não reconhecida: ${l}.`)
+      );
+      const link = pipe(
+        linha.cells[4]!.childNodes,
+        S.filter(x => !(x instanceof Text && /^\s*$/.test(x.nodeValue ?? ''))),
+        childNodes => {
+          return E.tryCatch(
+            () => {
+              P.assert(
+                P.isTuple(
+                  P.isInstanceOf(HTMLImageElement),
+                  P.isInstanceOf(HTMLDivElement),
+                  P.isInstanceOf(HTMLAnchorElement)
+                )(childNodes)
+              );
+              const [menu, popup, link] = childNodes;
+              return { menu, popup, link };
+            },
+            () => `Link para documento não reconhecido: ${l}.`
+          );
         }
-      )
-    )
+      );
+      const result = pipe(
+        tuple(sequencialNome, assinatura, link),
+        T.sequence(applicativeEither),
+        E.map(
+          ([{ sequencial, nome }, { assinatura }, { menu, popup, link }]) => {
+            // link.classList.remove('link');
+            link.textContent = nome;
+            const frag = document.createDocumentFragment();
+            frag.append(menu, popup, link);
+            const url = new URL(link.href);
+            const sp = url.searchParams.get('_tj')!;
+            const id = pipe(
+              tailRec({ acc: [] as string[], curr: sp }, ({ acc, curr }) =>
+                curr.length > 0
+                  ? Left({
+                      acc: [...acc, curr.slice(0, 8)],
+                      curr: curr.slice(8),
+                    })
+                  : Right(acc)
+              ),
+              x => x.slice(6, 8),
+              S.map(x => parseInt(x, 16)),
+              S.foldLeft(0n, (acc, x) => acc * 4294967296n + BigInt(x))
+            );
+            link.dataset.gmDocLink = id.toString();
+            return [sequencial, frag];
+          }
+        )
+      );
+      return result;
+    }),
+    E.map(linhas => {
+      table.replaceChildren(
+        ...pipe(
+          linhas,
+          S.map((linha, r) => {
+            const tr = document.createElement('tr');
+            tr.classList.add(r % 2 === 0 ? 'even' : 'odd');
+            return S.foldLeft<string | Node, HTMLTableRowElement>(
+              tr,
+              (tr, i) => {
+                const td = document.createElement('td');
+                td.append(i);
+                tr.append(td);
+                return tr;
+              }
+            )(linha);
+          })
+        )
+      );
+    })
   );
-  console.log({ linhas, table, novasLinhas });
-  table.replaceChildren(...novasLinhas);
 };
