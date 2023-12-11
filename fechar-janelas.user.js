0f3db9e4ec374fe5c3d865b79c076543e06b6f8a
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2023-12-11 13:53:04 -0300

    Correção para documentos sigilosos

--- fechar-janelas.user.js
+++ fechar-janelas.user.js
@@ -7,7 +7,7 @@
 // @match       https://eproc.jfsc.jus.br/eprocV2/controlador.php?acao=processo_selecionar&*
 // @match       https://eproc.trf4.jus.br/eproc2trf4/controlador.php?acao=processo_selecionar&*
 // @grant       none
-// @version     4.0.0
+// @version     4.0.1
 // @author      nadameu
 // @description Fecha as janelas de documentos que tenham sido abertas no processo
 // ==/UserScript==
@@ -51,7 +51,7 @@ function main() {
 }
 
 function onTabelaClick(evt) {
-  if (!evt.target?.matches?.('a.infraLinkDocumento')) return;
+  if (!evt.target?.matches?.('a.infraLinkDocumento, a.infraLinkDocumentoSigiloso')) return;
   show();
 }
 
