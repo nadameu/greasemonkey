1a41756fc8afdf79c6416bafc1a9f8b480637a90
Paulo R. Maurici Jr. <paulo@nadameu.com.br>
2024-07-01 18:23:34 -0300

    Correspondência à nova versão de @nadameu/match

--- userscripts/dados-cnis/src/Estado.ts
+++ userscripts/dados-cnis/src/Estado.ts
@@ -1,10 +1,25 @@
-import { Static, createTaggedUnion } from '@nadameu/match';
-
-export const Estado = createTaggedUnion({
-  Aguardando: null,
-  SemConsulta: null,
-  Erro: (motivo: Error) => ({ motivo }),
-  CarregandoDados: null,
-  DadosCarregados: (dados: unknown) => ({ dados }),
-});
-export type Estado = Static<typeof Estado>;
+import {
+  ExtraProperties,
+  MemberOf,
+  MemberTag,
+  TaggedUnion,
+  tag,
+} from '@nadameu/match';
+export type Estado = TaggedUnion<{
+  Aguardando: {};
+  SemConsulta: {};
+  Erro: { motivo: Error };
+  CarregandoDados: {};
+  DadosCarregados: { dados: unknown };
+}>;
+const tagEstado: <T extends MemberTag<Estado>>(
+  tag: T
+) => (props: ExtraProperties<MemberOf<Estado, T>>) => MemberOf<Estado, T> =
+  tag as unknown as any;
+export const Estado = {
+  Aguardando: tagEstado('Aguardando')({}),
+  SemConsulta: tagEstado('SemConsulta')({}),
+  Erro: (motivo: Error) => tagEstado('Erro')({ motivo }),
+  CarregandoDados: tagEstado('CarregandoDados')({}),
+  DadosCarregados: (dados: unknown) => tagEstado('DadosCarregados')({ dados }),
+} satisfies Record<MemberTag<Estado>, Estado | ((...args: never[]) => Estado)>;
--- userscripts/dados-cnis/src/Mensagem.ts
+++ userscripts/dados-cnis/src/Mensagem.ts
@@ -1,14 +1,31 @@
-import { createTaggedUnion, Static } from '@nadameu/match';
+import { MemberWith, tag, TaggedWithUnion } from '@nadameu/match';
 import * as p from '@nadameu/predicates';
 
-export const Mensagem = createTaggedUnion({
-  CarregandoDados: null,
-  ErroDesconhecido: (erro: Error) => ({ erro }),
-  SemConsulta: null,
-  MultiplosModais: null,
-});
+export type Mensagem = TaggedWithUnion<
+  'tag',
+  {
+    CarregandoDados: {};
+    ErroDesconhecido: { erro: Error };
+    SemConsulta: {};
+    MultiplosModais: {};
+  }
+>;
 
-export type Mensagem = Static<typeof Mensagem>;
+const tagMensagem = tag as <T extends Mensagem['tag']>(
+  tag: T
+) => (
+  props: Omit<MemberWith<Mensagem, 'tag', T>, 'tag'>
+) => MemberWith<Mensagem, 'tag', T>;
+
+export const Mensagem = {
+  CarregandoDados: tagMensagem('CarregandoDados'),
+  ErroDesconhecido: (erro: Error) => tagMensagem('ErroDesconhecido')({ erro }),
+  MultiplosModais: tagMensagem('MultiplosModais'),
+  SemConsulta: tagMensagem('SemConsulta'),
+} satisfies Record<
+  Mensagem['tag'],
+  Mensagem | ((...args: never[]) => Mensagem)
+>;
 
 export const isMensagem = p.isTaggedUnion('tag', {
   CarregandoDados: {},
