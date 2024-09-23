8ea42ddd38b92f575710daa525c4b37477b62193
Paulo R. Maurici Jr. <paulo@nadameu.com.br>
2024-09-23 16:49:35 -0300

    Desativação de scripts desnecessários

--- aguardar-carregamento-formulario.user.js
+++ aguardar-carregamento-formulario.user.js
@@ -2,69 +2,10 @@
 // @name        aguardar-carregamento-formulario
 // @name:pt-BR  Aguardar carregamento do formulário
 // @namespace   http://nadameu.com.br
-// @match       https://eproc.jfpr.jus.br/eprocV2/controlador.php?acao=relatorio_geral_listar&*
-// @match       https://eproc.jfrs.jus.br/eprocV2/controlador.php?acao=relatorio_geral_listar&*
-// @match       https://eproc.jfsc.jus.br/eprocV2/controlador.php?acao=relatorio_geral_listar&*
-// @match       https://eproc.trf4.jus.br/eproc2trf4/controlador.php?acao=relatorio_geral_listar&*
 // @grant       none
-// @version     3.1.1
+// @version     4.0.0
 // @author      nadameu
 // @description Aguardar o carregamento completo do formulário do relatório geral antes de poder clicar em "Consultar"
 // ==/UserScript==
 
-main().catch(error => {
-  console.error(error);
-});
-
-async function main() {
-  adicionarEstilos();
-
-  const tela = document.body.appendChild(document.createElement('div'));
-  tela.id = 'gm-tela-ocultar-formulario';
-  tela.textContent = 'Carregando formulário...';
-
-  let timeout, observer;
-
-  try {
-    const localizador = document.getElementById('selLocalizadorPrincipal');
-    if (!localizador) return;
-
-    await Promise.race([
-      new Promise((_, rej) => {
-        timeout = window.setTimeout(rej, 3000);
-      }),
-      new Promise(res => {
-        observer = new MutationObserver(() => {
-          if (localizador.style.display !== 'none') return;
-          else res();
-        });
-        observer.observe(localizador, { attributes: true });
-      }),
-    ]);
-  } catch (error) {
-    throw error;
-  } finally {
-    if (observer) observer.disconnect();
-    if (timeout) window.clearTimeout(timeout);
-    document.body.removeChild(tela);
-  }
-}
-
-function adicionarEstilos() {
-  const style = document.head.appendChild(document.createElement('style'));
-
-  style.textContent = /* css */ `
-#gm-tela-ocultar-formulario {
-  position: fixed;
-  inset: 0;
-  z-index: 1031;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: hsla(333, 25%, 5%, 0.75);
-  font-family: Arial, Verdana, Helvetica, sans-serif;
-  font-size: 2.2rem;
-  color: white;
-  text-shadow: 0px 0px 3px black;
-}`;
-}
+// Desnecessário ante alteração do sistema
