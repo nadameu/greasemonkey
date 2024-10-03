fc3a1489226cd4c9925cfedc46e73e2e5bc4ce93
Paulo R. Maurici Jr. <paulo@nadameu.com.br>
2024-10-03 19:39:40 -0300

    classList, dataset e style

--- lib/create-element/index.ts
+++ lib/create-element/index.ts
@@ -1,11 +1,36 @@
 export function h<K extends keyof HTMLElementTagNameMap>(
   tag: K,
-  props: Partial<HTMLElementTagNameMap[K]> | null = null,
-  ...children: Array<HTMLElement | string>
+  props: Partial<
+    Omit<HTMLElementTagNameMap[K], 'classList' | 'dataset' | 'style'> & {
+      classList?: Array<string | null> | Record<string, boolean>;
+      dataset?: Record<string, string>;
+      style?: Partial<CSSStyleDeclaration>;
+    }
+  > | null = null,
+  ...children: Array<Node | string>
 ): HTMLElementTagNameMap[K] {
   const element = document.createElement(tag);
-  for (const [key, value] of Object.entries(props ?? {})) {
-    (element as any)[key] = value;
+  for (const [key, value] of Object.entries<any>(props ?? {})) {
+    if (key === 'style' || key === 'dataset') {
+      for (const [k, v] of Object.entries(value)) {
+        (element as any)[key][k] = v;
+      }
+    } else if (key === 'classList') {
+      let classes: string[];
+      if (Array.isArray(value)) {
+        classes = value.filter((x): x is string => x !== null);
+      } else {
+        classes = Object.entries(value).flatMap(([k, v]) => {
+          if (!v) return [];
+          return [k];
+        });
+      }
+      for (const className of classes) {
+        element.classList.add(className);
+      }
+    } else {
+      (element as any)[key] = value;
+    }
   }
   element.append(...children);
   return element;
