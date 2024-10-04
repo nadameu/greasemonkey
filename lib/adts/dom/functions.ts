12b2555b44e0c03157912da2112b052e98bb3025
Paulo R. Maurici Jr. <paulo@nadameu.com.br>
2024-10-04 19:30:33 -0300

    xquery, xqueryAll

--- lib/adts/dom/functions.ts
+++ lib/adts/dom/functions.ts
@@ -1,5 +1,5 @@
 import { M, Maybe } from '../maybe';
-import { Seq } from '../seq';
+import { S, Seq } from '../seq';
 
 export const query =
   <T extends Element>(selector: string) =>
@@ -10,3 +10,25 @@ export const queryAll =
   (parentNode: ParentNode): Seq<T> =>
     parentNode.querySelectorAll<T>(selector);
 export const text = (node: Node) => M.fromNullable(node.textContent);
+const FIRST = XPathResult.FIRST_ORDERED_NODE_TYPE;
+export const xquery =
+  <T extends Node>(expression: string) =>
+  (contextNode: Node): Maybe<T> =>
+    M.fromNullable(
+      document.evaluate(expression, contextNode, null, FIRST)
+        .singleNodeValue as T | null
+    );
+const ITER = XPathResult.ORDERED_NODE_ITERATOR_TYPE;
+export const xqueryAll = <T extends Node>(
+  expression: string
+): ((contextNode: Node) => Seq<T>) =>
+  S.fromGen(function* (contextNode: Node) {
+    const result = document.evaluate(expression, contextNode, null, ITER);
+    for (
+      let node = result.iterateNext();
+      node !== null;
+      node = result.iterateNext()
+    ) {
+      yield node as T;
+    }
+  });
