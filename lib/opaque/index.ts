90426a3b8a857bf3b27fa91a503e31e3122ae864
Paulo R. Maurici Jr. <paulo@nadameu.com.br>
2024-10-07 12:51:21 -0300

    Documentação

--- lib/opaque/index.ts
+++ lib/opaque/index.ts
@@ -4,5 +4,9 @@ declare namespace Internal {
     private [OpaqueSymbol]: S;
   }
 }
+/**
+ * @example
+ * type Integer = Opaque<number, { Integer: Integer }>;
+ */
 export type Opaque<T, S extends Record<keyof any, unknown>> = T &
   Internal.Opaque<S>;
