ab620d5b423dcb1825a1a1d097142f40fdb77685
Paulo R. Maurici Jr. <paulo@nadameu.com.br>
2024-10-08 18:34:41 -0300

    fanout

--- lib/adts/tuple/functions.ts
+++ lib/adts/tuple/functions.ts
@@ -1,8 +1,24 @@
 import { S } from '../seq';
-import { Applicative, Type, Kind, SequenceTuple } from '../typeclasses';
+import { Applicative, Kind, SequenceTuple, Type } from '../typeclasses';
 
 export const sequence: <F extends Kind>(
   M: Applicative<F>
 ) => <T extends Type<F, unknown, unknown>[]>(
   tuple: [...T]
 ) => SequenceTuple<F, T> = S.sequence as any;
+
+type Fanout<
+  T,
+  F extends Array<(_: T) => any>,
+  R extends any[] = [],
+> = F extends []
+  ? R
+  : F extends [(_: T) => infer U, ...infer G extends Array<(_: T) => any>]
+    ? Fanout<T, G, [...R, U]>
+    : never;
+export const fanout: <T, F extends Array<(_: T) => any>>(
+  ...fns: F & Array<(_: T) => any>
+) => (value: T) => Fanout<T, F> =
+  (...fns: Function[]) =>
+  (value: unknown): any =>
+    fns.map(f => f(value));
