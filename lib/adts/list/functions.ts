6cc5f0708e2b4d93f342fb5d613754ece15d5794
Paulo R. Maurici Jr. <paulo@nadameu.com.br>
2024-10-24 17:33:28 -0300

    toIterable, better foldRight

--- lib/adts/list/functions.ts
+++ lib/adts/list/functions.ts
@@ -1,4 +1,4 @@
-import { Cons, isCons, List, Nil } from './definitions';
+import { Cons, isCons, isNil, List, Nil } from './definitions';
 
 export const fromArray = <a>(xs: ArrayLike<a>): List<a> =>
   Array.prototype.reduceRight.call<
@@ -6,6 +6,20 @@ export const fromArray = <a>(xs: ArrayLike<a>): List<a> =>
     [(tail: List<a>, head: a) => List<a>, List<a>],
     List<a>
   >(xs, (tail, head) => Cons(head, tail), Nil);
+
+export const toIterable = <a>(xs: List<a>): Iterable<a, void> => ({
+  [Symbol.iterator]() {
+    let curr = xs;
+    return {
+      next() {
+        if (isNil(curr)) return { done: true } as IteratorReturnResult<void>;
+        const value = curr.head;
+        curr = curr.tail;
+        return { done: false, value };
+      },
+    };
+  },
+});
 
 export const foldLeft =
   <a, b>(seed: b, f: (acc: b, a: a, i: number) => b) =>
@@ -24,24 +38,5 @@ export const reverse: <a>(list: List<a>) => List<a> = /* #__PURE__ */ foldLeft<
 
 export const foldRight =
   <a, b>(seed: b, f: (a: a, acc: b) => b) =>
-  (xs: List<a>): b => {
-    const prev = '_prev';
-    interface DLCons<a> extends Cons<a> {
-      [prev]: DLCons<a> | Nil;
-      tail: DLList<a>;
-    }
-    interface DLNil extends Nil {
-      [prev]: DLCons<a> | Nil;
-    }
-    type DLList<a> = DLCons<a> | DLNil;
-    let list = xs as DLList<a>;
-    list[prev] = Nil;
-    for (; isCons(list); list = list.tail) {
-      list.tail[prev] = list;
-    }
-    let acc = seed;
-    for (let curr = list[prev]; isCons(curr); curr = curr[prev]) {
-      acc = f(curr.head, acc);
-    }
-    return acc;
-  };
+  (xs: List<a>): b =>
+    [...toIterable(xs)].reduceRight((acc, x) => f(x, acc), seed);
