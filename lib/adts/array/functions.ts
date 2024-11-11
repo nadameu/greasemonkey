b7855adfc6115abc5d7948d61e9e63229a53f336
Paulo R. Maurici Jr. <paulo@nadameu.com.br>
2024-11-11 17:45:05 -0300

    Types

--- lib/adts/array/functions.ts
+++ lib/adts/array/functions.ts
@@ -5,8 +5,6 @@ import { filterMap as deriveFilterMap } from '../typeclasses/functions';
 import { ArrayF } from './internal';
 export { fromIterable, fromList };
 
-type Transform<a, b> = (xs: ArrayLike<a>) => b;
-
 export const forEach = <a>(
   xs: ArrayLike<a>,
   f: (a: a, i: number) => void
@@ -15,7 +13,10 @@ export const forEach = <a>(
 };
 
 export const foldLeft =
-  <a, b>(seed: b, f: (acc: b, a: a, i: number) => b): Transform<a, b> =>
+  <a, b>(
+    seed: b,
+    f: (acc: b, a: a, i: number) => b
+  ): ((xs: ArrayLike<a>) => b) =>
   xs => {
     let acc = seed;
     forEach(xs, (a, i) => {
@@ -27,7 +28,7 @@ export const foldLeft =
 const _foldLeftToNewArray =
   <a, b>(
     f: (push: (_: b) => void) => (a: a, i: number) => void
-  ): Transform<a, b[]> =>
+  ): ((xs: ArrayLike<a>) => b[]) =>
   xs => {
     const result: b[] = [];
     const g = f(b => {
@@ -38,7 +39,7 @@ const _foldLeftToNewArray =
   };
 
 export const foldRight =
-  <a, b>(seed: b, f: (a: a, acc: b) => b): Transform<a, b> =>
+  <a, b>(seed: b, f: (a: a, acc: b) => b): ((xs: ArrayLike<a>) => b) =>
   xs => {
     let acc = seed;
     for (let i = xs.length - 1; i >= 0; i--) acc = f(xs[i]!, acc);
@@ -47,14 +48,18 @@ export const foldRight =
 
 export const flatMap = <a, b>(
   f: (a: a, i: number) => ArrayLike<b>
-): Transform<a, b[]> =>
+): ((xs: ArrayLike<a>) => b[]) =>
   _foldLeftToNewArray(push => (a, i) => forEach(f(a, i), push));
-export const map = <a, b>(f: (a: a, i: number) => b): Transform<a, b[]> =>
+export const map = <a, b>(
+  f: (a: a, i: number) => b
+): ((xs: ArrayLike<a>) => b[]) =>
   _foldLeftToNewArray(push => (a, i) => push(f(a, i)));
 
 export const filter: {
-  <a, b extends a>(pred: (a: a, i: number) => a is b): Transform<a, b[]>;
-  <a>(pred: (a: a, i: number) => boolean): Transform<a, a[]>;
+  <a, b extends a>(
+    pred: (a: a, i: number) => a is b
+  ): (xs: ArrayLike<a>) => b[];
+  <a>(pred: (a: a, i: number) => boolean): (xs: ArrayLike<a>) => a[];
 } = <a>(pred: (a: a, i: number) => boolean) =>
   _foldLeftToNewArray<a, a>(push => (a, i) => {
     if (pred(a, i)) push(a);
