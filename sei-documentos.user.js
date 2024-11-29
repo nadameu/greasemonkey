56e6764cb033963b864cbc956c79a840e4016e05
Paulo R. Maurici Jr. <paulo@nadameu.com.br>
2024-11-29 16:01:02 -0300

    SEI! - Documentos

---
+++ sei-documentos.user.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name        sei-documentos
+// @name:pt-BR  SEI! - Documentos
+// @namespace   http://nadameu.com.br
+// @match       https://sei.trf4.jus.br/controlador.php?acao=editor_montar&*
+// @match       https://sei.trf4.jus.br/controlador.php?acao=documento_visualizar&*
+// @grant       GM_addStyle
+// @version     1.0.0
+// @author      Paulo R. Maurici Jr.
+// @description Limita a largura máxima dos documentos do SEI! para refletir a aparência do PDF correspondente
+// ==/UserScript==
+
+function main() {
+  GM_addStyle(/* css */ `
+  html {
+    --largura-pdf: 18cm;
+  }
+  `);
+  const url = new URL(document.location.href);
+  switch (url.searchParams.get("acao")) {
+    case "editor_montar":
+      GM_addStyle(/* css */ `
+  .infra-editor__editor {
+    max-width: calc(var(--largura-pdf) + 2 * 2em);
+    margin-inline: auto;
+  }
+  `);
+      break;
+
+    case "documento_visualizar":
+      GM_addStyle(/* css */ `
+  body {
+    max-width: var(--largura-pdf);
+    margin-inline: auto;
+  }
+    `);
+      break;
+
+    default:
+      throw new Error(`Página desconhecida: ${document.location.href}.`);
+  }
+}
+
+try {
+  main();
+} catch (e) {
+  console.group("<SEI! - Documentos>");
+  console.error(e);
+  console.groupEnd();
+}
