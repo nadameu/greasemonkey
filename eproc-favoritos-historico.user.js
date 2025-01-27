a3059b26b7d7265d41aeb4aebe5a767c8c37fa28
Paulo R. Maurici Jr. <paulo@nadameu.com.br>
2025-01-27 17:25:42 -0300

    Versão 1.3.0: Importação e exportação de favoritos

--- eproc-favoritos-historico.user.js
+++ eproc-favoritos-historico.user.js
@@ -2,7 +2,7 @@
 // @name         eproc-favoritos-historico
 // @name:pt-BR   eproc - favoritos e histórico
 // @namespace    http://nadameu.com.br
-// @version      1.2.0
+// @version      1.3.0
 // @author       nadameu
 // @description  Permite definir processos favoritos e visualizar o histórico de processos acessados
 // @match        https://eproc.jfpr.jus.br/eprocV2/controlador.php?acao=*
@@ -18,10 +18,10 @@
     GM_addStyle(o);
     return;
   }
-  const _ = document.createElement('style');
-  (_.textContent = o), document.head.append(_);
+  const t = document.createElement('style');
+  (t.textContent = o), document.head.append(t);
 })(
-  ' .bootstrap-styles #divCapaProcesso .row{align-items:center}.bootstrap-styles #divCapaProcesso ._div_g5qzc_4 a._link_g5qzc_4{--cor: hsl(333, 75%, 25%)}.bootstrap-styles #divCapaProcesso ._div_g5qzc_4 a._link_g5qzc_4._added_g5qzc_7{--cor: hsl(333, 75%, 35%)}.bootstrap-styles #divCapaProcesso ._div_g5qzc_4 a._link_g5qzc_4._wait_g5qzc_10{--cor: hsl(332, 15%, 35%)}.bootstrap-styles #divCapaProcesso ._div_g5qzc_4 a._link_g5qzc_4 ._icon_g5qzc_13{color:var(--cor)}.bootstrap-styles .navbar ._link_g5qzc_4 .navbar-icons._icon_g5qzc_13{color:#eed8e2}.bootstrap-styles .navbar ._link_g5qzc_4 .navbar-icons._icon_g5qzc_13:hover{background-color:#ff80b980!important}.bootstrap-styles ._dialogo_g5qzc_22{min-width:35%;height:90%;border-radius:7px;box-shadow:10px 20px 30px #2c212680}.bootstrap-styles ._dialogo_g5qzc_22::backdrop{background-color:#2c2126cc}.bootstrap-styles ._dialogo_g5qzc_22 h1{text-align:center}.bootstrap-styles ._dialogo_g5qzc_22 ._barra_g5qzc_34{text-align:right}.bootstrap-styles ._dialogo_g5qzc_22 ._aviso_g5qzc_37{background:#fdd;border:1px solid #caa;font-size:90%;margin-block:1em;padding-inline:2ex}.bootstrap-styles ._dialogo_g5qzc_22 ._aviso_g5qzc_37 p{margin-block:.5em} '
+  ' .bootstrap-styles #divCapaProcesso .row{align-items:center}.bootstrap-styles #divCapaProcesso ._div_ecv0l_4 a._link_ecv0l_4{--cor: hsl(333, 75%, 25%)}.bootstrap-styles #divCapaProcesso ._div_ecv0l_4 a._link_ecv0l_4._added_ecv0l_7{--cor: hsl(333, 75%, 35%)}.bootstrap-styles #divCapaProcesso ._div_ecv0l_4 a._link_ecv0l_4._wait_ecv0l_10{--cor: hsl(332, 15%, 35%)}.bootstrap-styles #divCapaProcesso ._div_ecv0l_4 a._link_ecv0l_4 ._icon_ecv0l_13{color:var(--cor)}.bootstrap-styles .navbar ._link_ecv0l_4 .navbar-icons._icon_ecv0l_13{color:#eed8e2}.bootstrap-styles .navbar ._link_ecv0l_4 .navbar-icons._icon_ecv0l_13:hover{background-color:#ff80b980!important}.bootstrap-styles ._dialogo_ecv0l_22{min-width:35%;height:90%;border-radius:7px;box-shadow:10px 20px 30px #2c212680;background:#fbf4f7}.bootstrap-styles ._dialogo_ecv0l_22::backdrop{background-color:#2c2126cc}.bootstrap-styles ._dialogo_ecv0l_22 h1{text-align:center}.bootstrap-styles ._dialogo_ecv0l_22 q{font-style:italic}.bootstrap-styles ._dialogo_ecv0l_22 button{border:1px outset hsl(333,50%,75%);padding:.1em 1ch;border-radius:4px;color:#290012;background:#e2cfd8}.bootstrap-styles ._dialogo_ecv0l_22 table thead tr,.bootstrap-styles ._dialogo_ecv0l_22 table tbody tr:nth-child(2n){background:#ebe0e5}.bootstrap-styles ._dialogo_ecv0l_22 table th,.bootstrap-styles ._dialogo_ecv0l_22 table td{border:0 solid black;text-align:center;padding:2px 6px}.bootstrap-styles ._dialogo_ecv0l_22 table th{border-bottom-width:2px}.bootstrap-styles ._dialogo_ecv0l_22 table th+th,.bootstrap-styles ._dialogo_ecv0l_22 table td+td{border-left-width:1px}.bootstrap-styles ._dialogo_ecv0l_22 ._div_download_ecv0l_62,.bootstrap-styles ._dialogo_ecv0l_22 ._div_upload_ecv0l_63{background:transparent;transition:1s background 2s;padding:1em 3ch}.bootstrap-styles ._dialogo_ecv0l_22 ._div_download_ecv0l_62._hidden_ecv0l_68,.bootstrap-styles ._dialogo_ecv0l_22 ._div_upload_ecv0l_63._hidden_ecv0l_68{position:absolute;width:1px;height:1px;overflow:hidden;top:-3em;left:-7ch;background:#ffeedb;transition:none}.bootstrap-styles ._dialogo_ecv0l_22 ._barra_ecv0l_79{text-align:right}.bootstrap-styles ._dialogo_ecv0l_22 ._aviso_ecv0l_82{background:#ffeedb;border:1px solid hsl(31,25%,75%);font-size:90%;margin-block:1em;padding-inline:2ex}.bootstrap-styles ._dialogo_ecv0l_22 ._aviso_ecv0l_82 p{margin-block:.5em} '
 );
 
 (function () {
@@ -50,24 +50,44 @@
   function isOfType(typeRepresentation) {
     return value => typeof value === typeRepresentation;
   }
+  const isOfTypeObject = /* @__PURE__ */ isOfType('object');
   const isString = /* @__PURE__ */ isOfType('string');
   function isLiteral(literal) {
     return value => value === literal;
   }
+  const isUndefined = /* @__PURE__ */ isLiteral(void 0);
   const isNull = /* @__PURE__ */ isLiteral(null);
   function negate(predicate) {
     return value => !predicate(value);
   }
   const isNotNull = /* @__PURE__ */ negate(isNull);
+  const isDefined = /* @__PURE__ */ negate(isUndefined);
   function refine(...predicates) {
     return value => predicates.every(p => p(value));
   }
+  const isObject = /* @__PURE__ */ refine(isOfTypeObject, isNotNull);
+  const isInteger = x => Number.isInteger(x);
+  const isNonNegativeInteger = /* @__PURE__ */ refine(isInteger, x => x > -1);
   const isNonEmptyString = /* @__PURE__ */ refine(
     isString,
     x => x.trim().length > 0
   );
+  function isAnyOf(...predicates) {
+    return value => predicates.some(p => p(value));
+  }
   const isNullish = x => x == null;
   const isNotNullish = /* @__PURE__ */ negate(isNullish);
+  const isArray = x => Array.isArray(x);
+  function isTypedArray(predicate) {
+    return refine(isArray, xs => xs.every(predicate));
+  }
+  function hasShape(predicates) {
+    return refine(isObject, obj =>
+      Object.entries(predicates).every(([key, predicate]) =>
+        key in obj ? predicate(obj[key]) : predicate.optional === true
+      )
+    );
+  }
   function promisifyRequest(request) {
     return new Promise((resolve, reject) => {
       request.oncomplete = request.onsuccess = () => resolve(request.result);
@@ -173,6 +193,12 @@
     await Store.getInstance().set(numproc, {
       favorito: { motivo, prioridade, timestamp },
       acesso: timestamp,
+    });
+  }
+  async function importar_favorito({ numproc, ...favorito }) {
+    Store.getInstance().update(numproc, item => {
+      const acesso = item === void 0 ? favorito.timestamp : item.acesso;
+      return { acesso, favorito };
     });
   }
   async function remover_favorito(numproc) {
@@ -298,14 +324,17 @@
       )
     );
   }
-  const div = '_div_g5qzc_4';
-  const link = '_link_g5qzc_4';
-  const added = '_added_g5qzc_7';
-  const wait = '_wait_g5qzc_10';
-  const icon = '_icon_g5qzc_13';
-  const dialogo = '_dialogo_g5qzc_22';
-  const barra = '_barra_g5qzc_34';
-  const aviso = '_aviso_g5qzc_37';
+  const div = '_div_ecv0l_4';
+  const link = '_link_ecv0l_4';
+  const added = '_added_ecv0l_7';
+  const wait = '_wait_ecv0l_10';
+  const icon = '_icon_ecv0l_13';
+  const dialogo = '_dialogo_ecv0l_22';
+  const div_download = '_div_download_ecv0l_62';
+  const div_upload = '_div_upload_ecv0l_63';
+  const hidden = '_hidden_ecv0l_68';
+  const barra = '_barra_ecv0l_79';
+  const aviso = '_aviso_ecv0l_82';
   const classes = {
     div,
     link,
@@ -313,9 +342,38 @@
     wait,
     icon,
     dialogo,
+    div_download,
+    div_upload,
+    hidden,
     barra,
     aviso,
   };
+  const unidades = [
+    { max_exclusive: 1e3, format: _ms => 'Agora' },
+    { max_exclusive: 60, format: s => `Há ${formatar_plural(s, 'segundo')}` },
+    { max_exclusive: 60, format: s => `Há ${formatar_plural(s, 'minuto')}` },
+    { max_exclusive: 24, format: s => `Há ${formatar_plural(s, 'hora')}` },
+    { max_exclusive: 365, format: s => `Há ${formatar_plural(s, 'dia')}` },
+    {
+      max_exclusive: Number.POSITIVE_INFINITY,
+      format: (_, d) => d.toLocaleDateString(),
+    },
+  ];
+  function formatar_intervalo(inicio, fim) {
+    const intervalo_ms = fim.getTime() - inicio.getTime();
+    let current = intervalo_ms;
+    for (const unidade of unidades) {
+      if (current < unidade.max_exclusive) {
+        return unidade.format(current, inicio);
+      } else {
+        current = Math.floor(current / unidade.max_exclusive);
+      }
+    }
+    throw new Error('Erro inesperado.');
+  }
+  function formatar_plural(n, singular) {
+    return `${n.toString()} ${singular}${n > 1 ? 's' : ''}`;
+  }
   function formatar_numproc(numproc) {
     let index = 0;
     return '#######-##.####.#.##.####'.replace(/#/g, () => numproc[index++]);
@@ -323,9 +381,14 @@
   const mensagem_aviso_favoritos = [
     'Os favoritos são salvos apenas neste navegador e computador.',
     'Cabe ao navegador determinar por quanto tempo estes dados serão mantidos e quando precisam ser excluídos.',
-    'Para evitar perda de dados, adicione processos importantes a um localizador destinado a este fim.',
+    'Para evitar perda de dados, adicione processos importantes a um localizador destinado a este fim ou exporte regularmente os favoritos para um arquivo e salve-o em local seguro.',
   ];
   const Prioridade = { BAIXA: 1, MEDIA: 2, ALTA: 3 };
+  const isPrioridade = isAnyOf(
+    isLiteral(Prioridade.BAIXA),
+    isLiteral(Prioridade.MEDIA),
+    isLiteral(Prioridade.ALTA)
+  );
   async function query_first(selector, parentNode = document) {
     const element = parentNode.querySelector(selector);
     if (element === null) {
@@ -374,7 +437,14 @@
     link_casa.parentNode.insertBefore(div2, link_casa);
     const dialogo_historico = criar_dialogo_historico(abrir_processo);
     const dialogo_favoritos = criar_dialogo_favoritos(abrir_processo);
-    document.body.append(dialogo_historico.dialogo, dialogo_favoritos.dialogo);
+    document.body.append(
+      h(
+        'div',
+        { className: 'bootstrap-styles' },
+        dialogo_historico.dialogo,
+        dialogo_favoritos.dialogo
+      )
+    );
     const criar_handler_abertura =
       ({ dialogo: dialogo2, update: update2 }, obter_dados) =>
       evt => {
@@ -426,6 +496,7 @@
           return;
         }
         output.textContent = '';
+        const data_agora = /* @__PURE__ */ new Date();
         output.append(
           criar_tabela(
             ['Favorito?', 'Processo', 'Último acesso'],
@@ -435,7 +506,15 @@
                   ? ''
                   : criar_icone_material('star', favorito.motivo);
               const c1 = criar_links_numproc(numproc);
-              const c2 = new Date(acesso).toLocaleString('pt-BR');
+              const data_acesso = new Date(acesso);
+              const c2 = h(
+                'time',
+                {
+                  dateTime: data_acesso.toISOString(),
+                  title: data_acesso.toLocaleString('pt-BR'),
+                },
+                formatar_intervalo(data_acesso, data_agora)
+              );
               return [c0, c1, c2];
             })
           )
@@ -448,9 +527,139 @@
       dialogo: dialogo2,
       aviso: aviso2,
       output,
+      barras,
     } = criar_dialogo('Favoritos', classes);
     aviso2.append(...mensagem_aviso_favoritos.map(x => h('p', {}, x)));
     const criar_links_numproc = criar_links_dialogo(dialogo2, abrir_processo);
+    const botoes_exportar = [];
+    barras.forEach(barra2 => {
+      const botao_exportar = h(
+        'button',
+        {
+          type: 'button',
+          onclick: function exportar(evt) {
+            evt.preventDefault();
+            div_upload2.classList.toggle(classes.hidden, true);
+            const new_hidden_state = !div_download2.classList.contains(
+              classes.hidden
+            );
+            div_download2.classList.toggle(classes.hidden, new_hidden_state);
+          },
+        },
+        'Exportar...'
+      );
+      botoes_exportar.push(botao_exportar);
+      const botao_importar = h(
+        'button',
+        {
+          type: 'button',
+          onclick: function importar(evt) {
+            evt.preventDefault();
+            div_download2.classList.toggle(classes.hidden, true);
+            const new_hidden_state = !div_upload2.classList.contains(
+              classes.hidden
+            );
+            div_upload2.classList.toggle(classes.hidden, new_hidden_state);
+          },
+        },
+        'Importar...'
+      );
+      barra2.prepend(botao_importar, ' ', botao_exportar, ' ');
+    });
+    const link_download = h(
+      'a',
+      {
+        href: `data:application/json,${window.encodeURIComponent('[]')}`,
+        download: 'processos_favoritos.json',
+      },
+      'Clique aqui com o botão direito do mouse'
+    );
+    obter_favoritos()
+      .then(favoritos => {
+        link_download.href = `data:application/json,${window.encodeURIComponent(JSON.stringify(favoritos))}`;
+      })
+      .catch(err => {
+        div_download2.classList.toggle(classes.hidden, true);
+        botoes_exportar.forEach(botao_exportar => {
+          botao_exportar.onclick = evt => {
+            evt.preventDefault();
+            log_error(err);
+            window.alert('Erro ao obter os favoritos.');
+          };
+        });
+      });
+    const div_download2 = h(
+      'div',
+      {
+        classList: [classes.div_download, classes.hidden],
+      },
+      h(
+        'p',
+        {},
+        link_download,
+        ' e selecione “',
+        h('samp', {}, 'Salvar link como...'),
+        '” para salvar os favoritos em um arquivo.'
+      ),
+      h(
+        'p',
+        {},
+        'Posteriormente você poderá usar o botão “',
+        h('samp', {}, 'Importar...'),
+        '” para utilizá-lo em outro computador ou navegador.'
+      )
+    );
+    const arquivo = h('input', {
+      type: 'file',
+      accept: 'application/json',
+      onchange(evt) {
+        evt.preventDefault();
+        if ((arquivo.files?.length ?? 0) === 1) {
+          (async () => {
+            const file = arquivo.files.item(0);
+            console.log({ file });
+            const text = await file.text();
+            const novos_favoritos = check(
+              isTypedArray(
+                hasShape({
+                  numproc: refine(isString, isNumProc),
+                  prioridade: isPrioridade,
+                  motivo: isString,
+                  timestamp: isNonNegativeInteger,
+                })
+              ),
+              JSON.parse(text)
+            );
+            const resposta = window.confirm(
+              'ATENÇÃO: TODOS OS FAVORITOS SERÃO EXCLUÍDOS E SUBSTITUÍDOS PELO ARQUIVO IMPORTADO. DESEJA CONTINUAR?'
+            );
+            if (resposta === true) {
+              for (const { numproc } of await obter_favoritos()) {
+                await remover_favorito(numproc);
+              }
+              for (const fav of novos_favoritos) {
+                await importar_favorito(fav);
+              }
+              window.alert(
+                'Favoritos importados. Atualize a página para que as alterações tenham efeito.'
+              );
+              div_upload2.classList.add(classes.hidden);
+              dialogo2.close();
+            }
+          })().catch(err => {
+            log_error(err);
+            window.alert('Erro ao abrir arquivo.');
+          });
+        }
+      },
+    });
+    const div_upload2 = h(
+      'div',
+      { classList: [classes.div_upload, classes.hidden] },
+      h('p', {}, 'Selecione abaixo o arquivo a importar.'),
+      arquivo
+    );
+    barras[0].after(div_download2, div_upload2);
     return {
       dialogo: dialogo2,
       update(dados) {
@@ -459,21 +668,24 @@
           return;
         }
         output.textContent = '';
-        output.append(
-          criar_tabela(
-            ['Prioridade', 'Processo', 'Motivo'],
-            dados.map(({ numproc, motivo, prioridade }) => {
-              const c0 = {
-                [Prioridade.ALTA]: 'Alta',
-                [Prioridade.MEDIA]: 'Média',
-                [Prioridade.BAIXA]: 'Baixa',
-              }[prioridade];
-              const c1 = criar_links_numproc(numproc);
-              const c2 = motivo;
-              return [c0, c1, c2];
-            })
-          )
+        const tabela = criar_tabela(
+          ['Prioridade', 'Processo', 'Motivo'],
+          dados.map(({ numproc, motivo, prioridade }) => {
+            const c0 = {
+              [Prioridade.ALTA]: 'Alta',
+              [Prioridade.MEDIA]: 'Média',
+              [Prioridade.BAIXA]: 'Baixa',
+            }[prioridade];
+            const c1 = criar_links_numproc(numproc);
+            const c2 = motivo;
+            return [c0, c1, c2];
+          })
         );
+        const linhas = check(isDefined, tabela.tBodies[0]?.rows);
+        for (const linha of linhas) {
+          check(isDefined, linha.cells[2]).style.textAlign = 'start';
+        }
+        output.append(tabela);
       },
     };
   }
@@ -495,15 +707,20 @@
         formatar_numproc(numproc)
       );
       const link_nova = h(
-        'a',
-        { href: '#', onclick: criar_onclick('NOVA_ABA') },
+        'small',
+        {},
         h(
-          'i',
-          {
-            classList: ['material-icons'],
-            title: 'Abrir em nova aba',
-          },
-          'open_in_new'
+          'a',
+          { href: '#', onclick: criar_onclick('NOVA_ABA') },
+          h(
+            'i',
+            {
+              classList: ['material-icons'],
+              title: 'Abrir em nova aba',
+              style: { fontSize: '17px' },
+            },
+            'open_in_new'
+          )
         )
       );
       const frag = document.createDocumentFragment();
