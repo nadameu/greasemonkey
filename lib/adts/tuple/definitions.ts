fd92c07d67c0df5bb12920c6052ca11981e47a97
Paulo R. Maurici Jr. <paulo@nadameu.com.br>
2025-03-06 18:16:55 -0300

    Distributive tuple types

--- lib/adts/tuple/definitions.ts
+++ lib/adts/tuple/definitions.ts
@@ -1,5 +1,15 @@
 export const tuple: {
-  <a, b>(a: a, b: b): [a, b];
+  <a>(a: a): a extends infer fst ? [fst] : [a];
+  <a, b>(
+    a: a,
+    b: b
+  ): a extends infer fst
+    ? b extends infer snd
+      ? [fst, snd]
+      : [fst, b]
+    : b extends infer snd
+      ? [a, snd]
+      : [a, b];
   <T extends unknown[]>(...values: T): T;
 } = <T extends unknown[]>(...values: T): T => values;
 export const fst = <a, b>(tuple: [a, b]): a => tuple[0];
