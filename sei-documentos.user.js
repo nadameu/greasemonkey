f875dcf66c36d93ee0155951215e9406a9bff3f1
Paulo R. Maurici Jr. <paulo@nadameu.com.br>
2025-05-13 12:41:19 -0300

    URL alternativo

--- sei-documentos.user.js
+++ sei-documentos.user.js
@@ -3,9 +3,11 @@
 // @name:pt-BR  SEI! - Documentos
 // @namespace   http://nadameu.com.br
 // @match       https://sei.trf4.jus.br/controlador.php?acao=editor_montar&*
+// @match       https://sei.trf4.jus.br/sei/controlador.php?acao=editor_montar&*
 // @match       https://sei.trf4.jus.br/controlador.php?acao=documento_visualizar&*
+// @match       https://sei.trf4.jus.br/sei/controlador.php?acao=documento_visualizar&*
 // @grant       GM_addStyle
-// @version     1.1.0
+// @version     1.2.0
 // @author      Paulo R. Maurici Jr.
 // @description Limita a largura máxima dos documentos do SEI! para refletir a aparência do PDF correspondente
 // ==/UserScript==
