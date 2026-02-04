6800fd132bb12885b8996bcd9a802cf7293b0019
Paulo R. Maurici Jr. <paulo@nadameu.com.br>
2026-02-04 18:52:50 -0300

    Refatoração

--- userscripts/verificar-agendamento/src/index.ts
+++ userscripts/verificar-agendamento/src/index.ts
@@ -1,69 +1,64 @@
-main().catch(error => {
-  console.error(error);
-});
+import * as P from '@nadameu/predicates';
 
-async function main() {
-  const [botao, ehLote] = await validate([obterBotao(), obterEhLote()]);
+try {
+  main();
+} catch (error) {
+  console.error(error);
+}
+
+function main() {
+  const botao = obterBotao();
+  const ehLote = obterEhLote();
 
   if (ehLote) {
     botao.click();
   }
 }
 
-async function obterBotao() {
-  const botoes = document.querySelectorAll<HTMLElement>(
-    '#btnManterAgendamento'
-  );
-  assert(botoes.length === 1, 'Mais de um botão encontrado.');
-  const botao = botoes[0]!;
-  return botao;
+function obterBotao() {
+  return P.check(
+    P.arrayHasLength(1),
+    document.querySelectorAll<HTMLElement>('#btnManterAgendamento'),
+    'Mais de um botão encontrado.'
+  )[0];
 }
 
-async function obterEhLote() {
-  const headings = document.querySelectorAll<HTMLHeadingElement>(
-    '#divInfraBarraLocalizacao h4'
-  );
-  assert(headings.length === 1, 'Mais de um título encontrado.');
-  const minuta = headings[0]!.textContent.match(
-    /^Agendamento da Minuta .*(\d+)$/
-  )?.[1];
-  assert(minuta != null, 'Número da minuta não encontrado.');
+function obterEhLote() {
+  const heading = P.check(
+    P.arrayHasLength(1),
+    document.querySelectorAll<HTMLHeadingElement>(
+      '#divInfraBarraLocalizacao h4'
+    ),
+    'Mais de um título encontrado.'
+  )[0];
 
-  const linhas = Array.from(
-    window.top?.document.body.querySelectorAll<HTMLTableRowElement>(
-      '#fldMinutas tr'
-    ) ?? []
-  )
-    .filter(row => row.cells.length >= 2)
-    .filter(row => RegExp(minuta).test(row.cells[1]!.textContent));
-  assert(linhas.length === 1, 'Quantidade inesperada de linhas encontradas.');
-  const linha = linhas[0]!;
-  const ehLote =
-    (linha.cells[linha.cells.length - 1]!.querySelectorAll(
-      '#divListaRecursosMinuta > a img[src$="imagens/minuta_editar_lote.gif"]'
-    ).length ?? 0) > 0;
-  return ehLote;
-}
+  const minuta = P.check(
+    P.isNotNull,
+    heading.textContent.match(/^Agendamento da Minuta .*(\d+)$/) as
+      | [string, string]
+      | null,
+    'Número da minuta não encontrado.'
+  )[1];
 
-function assert(condition: boolean, message?: string): asserts condition {
-  if (!condition) throw new Error(message);
-}
-
-function validate<T, U>(promises: [Promise<T>, Promise<U>]): Promise<[T, U]>;
-function validate<T>(promises: Promise<T>[]): Promise<Awaited<T>[]> {
-  return Promise.allSettled(promises).then(results => {
-    if (
-      results.every(
-        (x): x is PromiseFulfilledResult<Awaited<T>> => x.status === 'fulfilled'
+  const linha = P.check(
+    P.arrayHasLength(1),
+    window.top?.document.body
+      .querySelectorAll<HTMLTableRowElement>('#fldMinutas tr')
+      .values()
+      .filter(
+        row =>
+          P.arrayHasAtLeastLength(2)(row.cells) &&
+          RegExp(minuta).test(row.cells[1].textContent)
       )
-    ) {
-      return results.map(x => x.value);
-    } else {
-      throw new AggregateError(
-        results
-          .filter((x): x is PromiseRejectedResult => x.status === 'rejected')
-          .map(x => x.reason)
-      );
-    }
-  });
+      .toArray() ?? [],
+    'Quantidade inesperada de linhas encontradas.'
+  )[0];
+
+  const ehLote =
+    P.check(P.arrayHasAtLeastLength(1), linha.cells)[
+      linha.cells.length - 1
+    ]!.querySelectorAll(
+      '#divListaRecursosMinuta > a img[src$="imagens/minuta_editar_lote.gif"]'
+    ).length > 0;
+  return ehLote;
 }
--- verificar-agendamento.user.js
+++ verificar-agendamento.user.js
@@ -2,7 +2,7 @@
 // @name         verificar-agendamento
 // @name:pt-BR   Verificar agendamento
 // @namespace    http://nadameu.com.br
-// @version      1.1.1
+// @version      1.1.2
 // @author       Paulo R. Maurici Jr.
 // @description  Fecha a tela de alteração de agendamento quando a minuta foi criada em bloco
 // @match        https://eproc.jfpr.jus.br/eprocV2/controlador.php?acao=minuta_verificar_agendamento&*
@@ -14,53 +14,78 @@
 (function () {
   'use strict';
 
-  main().catch(error => {
+  class AssertionError extends Error {
+    name = 'AssertionError';
+    constructor(message) {
+      super(message);
+    }
+  }
+  function assert(condition, message) {
+    if (!condition) throw new AssertionError(message);
+  }
+  function check(predicate, value, message) {
+    assert(predicate(value), message);
+    return value;
+  }
+  function isLiteral(literal) {
+    return value => value === literal;
+  }
+  const isNull = /* @__PURE__ */ isLiteral(null);
+  function negate(predicate) {
+    return value => !predicate(value);
+  }
+  const isNotNull = /* @__PURE__ */ negate(isNull);
+  const arrayHasLength = num => obj => obj.length === num;
+  const arrayHasAtLeastLength = num => array => array.length >= num;
+  try {
+    main();
+  } catch (error) {
     console.error(error);
-  });
-  async function main() {
-    const [botao, ehLote] = await validate([obterBotao(), obterEhLote()]);
+  }
+  function main() {
+    const botao = obterBotao();
+    const ehLote = obterEhLote();
     if (ehLote) {
       botao.click();
     }
   }
-  async function obterBotao() {
-    const botoes = document.querySelectorAll('#btnManterAgendamento');
-    assert(botoes.length === 1, 'Mais de um botão encontrado.');
-    const botao = botoes[0];
-    return botao;
+  function obterBotao() {
+    return check(
+      arrayHasLength(1),
+      document.querySelectorAll('#btnManterAgendamento'),
+      'Mais de um botão encontrado.'
+    )[0];
   }
-  async function obterEhLote() {
-    const headings = document.querySelectorAll('#divInfraBarraLocalizacao h4');
-    assert(headings.length === 1, 'Mais de um título encontrado.');
-    const minuta = headings[0].textContent.match(
-      /^Agendamento da Minuta .*(\d+)$/
-    )?.[1];
-    assert(minuta != null, 'Número da minuta não encontrado.');
-    const linhas = Array.from(
-      window.top?.document.body.querySelectorAll('#fldMinutas tr') ?? []
-    )
-      .filter(row => row.cells.length >= 2)
-      .filter(row => RegExp(minuta).test(row.cells[1].textContent));
-    assert(linhas.length === 1, 'Quantidade inesperada de linhas encontradas.');
-    const linha = linhas[0];
+  function obterEhLote() {
+    const heading = check(
+      arrayHasLength(1),
+      document.querySelectorAll('#divInfraBarraLocalizacao h4'),
+      'Mais de um título encontrado.'
+    )[0];
+    const minuta = check(
+      isNotNull,
+      heading.textContent.match(/^Agendamento da Minuta .*(\d+)$/),
+      'Número da minuta não encontrado.'
+    )[1];
+    const linha = check(
+      arrayHasLength(1),
+      window.top?.document.body
+        .querySelectorAll('#fldMinutas tr')
+        .values()
+        .filter(
+          row =>
+            arrayHasAtLeastLength(2)(row.cells) &&
+            RegExp(minuta).test(row.cells[1].textContent)
+        )
+        .toArray() ?? [],
+      'Quantidade inesperada de linhas encontradas.'
+    )[0];
     const ehLote =
-      (linha.cells[linha.cells.length - 1].querySelectorAll(
+      check(arrayHasAtLeastLength(1), linha.cells)[
+        linha.cells.length - 1
+      ].querySelectorAll(
         '#divListaRecursosMinuta > a img[src$="imagens/minuta_editar_lote.gif"]'
-      ).length ?? 0) > 0;
+      ).length > 0;
     return ehLote;
-  }
-  function assert(condition, message) {
-    if (!condition) throw new Error(message);
-  }
-  function validate(promises) {
-    return Promise.allSettled(promises).then(results => {
-      if (results.every(x => x.status === 'fulfilled')) {
-        return results.map(x => x.value);
-      } else {
-        throw new AggregateError(
-          results.filter(x => x.status === 'rejected').map(x => x.reason)
-        );
-      }
-    });
   }
 })();
