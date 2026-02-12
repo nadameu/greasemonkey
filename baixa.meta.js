a28b18155e5a87dbf80b9f7d29a23269262cf63b
Paulo R. Maurici Jr. <paulo@nadameu.com.br>
2026-02-12 16:00:46 -0300

    Desativação

--- baixa.meta.js
+++ baixa.meta.js
@@ -1,10 +1,8 @@
 // ==UserScript==
 // @name Preenchimento dados baixa
-// @version 6.0.2
+// @version 7.0.0
 // @author nadameu
 // @namespace http://nadameu.com.br/baixa
-// @include /^https:\/\/eproc\.(jf(pr|rs|sc)|trf4)\.jus\.br\/eproc(V2|2trf4)\/controlador\.php\?acao=baixa_arquivamento_processo_etapa_(1|3)&/
 // @updateUrl https://github.com/nadameu/greasemonkey/raw/master/baixa.meta.js
 // @downloadUrl https://github.com/nadameu/greasemonkey/raw/master/baixa.user.js
-// @grant window.close
 // ==/UserScript==
