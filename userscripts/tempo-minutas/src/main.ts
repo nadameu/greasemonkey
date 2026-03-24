724f4c6b38f85381aca4b213b713c94508c62eb4
Paulo R. Maurici Jr. <paulo@nadameu.com.br>
2026-03-24 17:12:46 -0300

    Projeto vazio

--- userscripts/tempo-minutas/src/main.ts
+++
@@ -1,9 +0,0 @@
-export function main() {
-  const divInfraBarraComandosSuperior = document.querySelectorAll(
-    '[id="divInfraBarraComandosSuperior"]'
-  );
-  if (divInfraBarraComandosSuperior.length !== 1)
-    throw new Error(
-      `Erro ao obter elemento \`#divInfraBarraComandosSuperior\`.`
-    );
-}
