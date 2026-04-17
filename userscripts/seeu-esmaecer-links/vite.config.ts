799e3d9a7a4f2d93c5f65e0ebe1d989540cdd450
Paulo R. Maurici Jr. <paulo@nadameu.com.br>
2026-04-17 16:20:23 -0300

    Configuração do projeto

--- userscripts/seeu-esmaecer-links/vite.config.ts
+++ userscripts/seeu-esmaecer-links/vite.config.ts
@@ -7,14 +7,16 @@ import * as pkg from './package.json';
 export default defineConfig({
   build: { outDir: '../..', emptyOutDir: false, target },
   esbuild: { charset: 'utf8' },
+  server: { hmr: false },
 
   plugins: [
     monkey({
       entry: 'src/index.ts',
+      server: { prefix: name => `dev:${name}` },
       userscript: {
         'name': { 'pt-BR': pkg.gm_name },
         'namespace': 'http://nadameu.com.br',
-        'match': ['https://seeu.pje.jus.br/seeu/*'],
+        'match': ['https://seeu.pje.jus.br/seeu/usuario/mesa*'],
         'run-at': 'document-idle',
       },
     }),
