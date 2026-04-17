57a14a7fbbbe2ffbb3ee56fa590b6c22d8719c7d
Paulo R. Maurici Jr. <paulo@nadameu.com.br>
2026-04-17 16:30:13 -0300

    lift_throwable permite usar funções inseguras assincronamente

--- template/package-name/src/try_catch.ts
+++ template/package-name/src/try_catch.ts
@@ -10,15 +10,23 @@ export class CustomError<T = {}> extends Error {
 }
 CustomError.prototype.name = 'CustomError';
 
-export function try_catch(fn: () => void): void {
-  try {
-    fn();
-  } catch (err) {
-    console.group(`<${GM_info.script.name}>`);
-    console.error(err);
-    if (err instanceof CustomError) {
-      console.debug(err.payload);
+export function lift_throwable<Args extends unknown[]>(
+  fn: (...args: Args) => void
+) {
+  return (...args: Args) => {
+    try {
+      fn(...args);
+    } catch (err) {
+      console.group(`<${GM_info.script.name}>`);
+      console.error(err);
+      if (err instanceof CustomError) {
+        console.debug(err.payload);
+      }
+      console.groupEnd();
     }
-    console.groupEnd();
-  }
+  };
+}
+
+export function try_catch(fn: () => void): void {
+  return lift_throwable(fn)();
 }
