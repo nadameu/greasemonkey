2069ab2559d52bf67ac18bf45485b58259fdf79e
Paulo R. Maurici Jr. <paulo@nadameu.com.br>
2026-05-06 02:59:21 -0300

    Adequação do projeto

--- userscripts/serh-frequencia/vite.config.ts
+++ userscripts/serh-frequencia/vite.config.ts
@@ -1,14 +1,17 @@
+import target from '@nadameu/esbuild-target';
 import { defineConfig } from 'vite';
 import monkey from 'vite-plugin-monkey';
 import * as pkg from './package.json';
-import target from '@nadameu/esbuild-target';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: { outDir: '../..', emptyOutDir: false, target },
+  esbuild: { charset: 'utf8' },
+  server: { hmr: false },
   plugins: [
     monkey({
       entry: 'src/index.ts',
+      server: { prefix: name => `dev:${name}` },
       userscript: {
         name: { 'pt-BR': pkg.gm_name },
         namespace: 'http://nadameu.com.br',
