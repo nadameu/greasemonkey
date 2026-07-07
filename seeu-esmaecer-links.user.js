2cb3628554762f720bb2f8711f79b04afd3b9567
Paulo R. Maurici Jr. <paulo@nadameu.com.br>
2026-07-07 16:04:43 -0300

    Versão 1.1.0

--- seeu-esmaecer-links.user.js
+++ seeu-esmaecer-links.user.js
@@ -2,7 +2,7 @@
 // @name         seeu-esmaecer-links
 // @name:pt-BR   SEEU - Esmaecer links
 // @namespace    http://nadameu.com.br
-// @version      1.0.2
+// @version      1.1.0
 // @author       nadameu
 // @description  Diminui o destaque para links de contagem cuja quantidade é 0 (zero)
 // @match        https://seeu.pje.jus.br/seeu/usuario/mesa*
@@ -12,18 +12,62 @@
 
 (function () {
   'use strict';
-
   var _GM_addStyle = (() =>
     typeof GM_addStyle != 'undefined' ? GM_addStyle : void 0)();
   var _unsafeWindow = (() =>
     typeof unsafeWindow != 'undefined' ? unsafeWindow : void 0)();
-  const name = 'seeu-esmaecer-links';
-  class CustomError extends Error {
+  var name = 'seeu-esmaecer-links';
+  var IMaybe = class {
+    ap(ff) {
+      return this.chain(a => ff.map(f => f(a)));
+    }
+    map(f) {
+      return this.chain(x => new Just(f(x)));
+    }
+    safeMap(f) {
+      return this.chain(x => maybe(f(x)));
+    }
+  };
+  var Just = class extends IMaybe {
+    value;
+    empty = false;
+    constructor(value) {
+      super();
+      this.value = value;
+    }
+    chain(f) {
+      return f(this.value);
+    }
+  };
+  function just(value) {
+    return new Just(value);
+  }
+  var Nothing = class extends IMaybe {
+    empty = true;
+    chain(_) {
+      return this;
+    }
+  };
+  var _Nothing = null;
+  function nothing() {
+    return (_Nothing ??= new Nothing());
+  }
+  function maybe(value) {
+    return value == null ? nothing() : just(value);
+  }
+  function combine(maybes) {
+    let result = just([]);
+    for (let i = 0; i < maybes.length; i += 1)
+      result = maybes[i].ap(result.map(xs => x => (xs.push(x), xs)));
+    return result;
+  }
+  var CustomError = class extends Error {
+    payload;
     constructor(message, payload = {}) {
       super(message);
       this.payload = payload;
     }
-  }
+  };
   CustomError.prototype.name = 'CustomError';
   function lift_throwable(fn) {
     return (...args) => {
@@ -32,9 +76,7 @@
       } catch (err) {
         console.group(`<${name}>`);
         console.error(err);
-        if (err instanceof CustomError) {
-          console.debug(err.payload);
-        }
+        if (err instanceof CustomError) console.debug(err.payload);
         console.groupEnd();
       }
     };
@@ -42,158 +84,212 @@
   function try_catch(fn) {
     return lift_throwable(fn)();
   }
-  const NOME_CLASSE_CONTAGEM_ZERADA = `gm-${name}-vazio`;
-  let observado = false;
+  var NOME_CLASSE_CONTAGEM_ZERADA = `gm-${name}-vazio`;
   function main() {
-    const linhas = obter_linhas_rotulo_valor();
-    const tabelas_pecas = obter_tabelas_outros_cumprimentos();
-    if (tabelas_pecas.length === 1) {
-      linhas.push(...tabelas_pecas[0].linhas);
-    }
-    const celulas_com_script = linhas.flatMap(l =>
-      l.celulas.filter(c => c.contem_scripts).map(c => c.celula)
+    const linhas = obter_linhas_rotulo_valor(document).concat(
+      obter_tabelas_outros_cumprimentos(document) ?? []
     );
-    if (!observado && celulas_com_script.length > 0) {
+    const celulas_com_script = parse_non_empty(
+      linhas.flatMap(l =>
+        l.celulas.filter(c => c.contem_scripts).map(c => c.celula)
+      )
+    );
+    if (celulas_com_script != null) {
       const common = common_ancestor(celulas_com_script);
-      const observer = new MutationObserver(debounce(lift_throwable(main)));
-      observer.observe(common, { childList: true, subtree: true });
-      observado = true;
+      new MutationObserver(
+        debounce(
+          lift_throwable(() => {
+            const linhas = obter_linhas_rotulo_valor(common);
+            for (const linha of linhas) aplicar_alteracoes_linha(linha);
+          })
+        )
+      ).observe(common, {
+        childList: true,
+        subtree: true,
+      });
     }
-    for (const { linha, celulas } of linhas) {
-      linha.classList.remove(NOME_CLASSE_CONTAGEM_ZERADA);
-      const celulas_vazias = [];
-      for (const { celula, qtd } of celulas) {
-        celula.classList.remove(NOME_CLASSE_CONTAGEM_ZERADA);
-        if (qtd === 0) {
-          celulas_vazias.push(celula);
-        }
-      }
-      if (celulas_vazias.length === celulas.length) {
-        linha.classList.add(NOME_CLASSE_CONTAGEM_ZERADA);
-      } else {
-        for (const celula_vazia of celulas_vazias) {
-          celula_vazia.classList.add(NOME_CLASSE_CONTAGEM_ZERADA);
-        }
-      }
-    }
+    for (const linha of linhas) aplicar_alteracoes_linha(linha);
     _GM_addStyle(`.${NOME_CLASSE_CONTAGEM_ZERADA} { opacity: 0.3; }`);
   }
-  function obter_linhas_rotulo_valor() {
+  function aplicar_alteracoes_linha({ linha, celulas }) {
+    linha.classList.remove(NOME_CLASSE_CONTAGEM_ZERADA);
+    const celulas_vazias = [];
+    for (const { celula, qtd } of celulas) {
+      celula.classList.remove(NOME_CLASSE_CONTAGEM_ZERADA);
+      if (qtd === 0) celulas_vazias.push(celula);
+    }
+    if (celulas_vazias.length === celulas.length)
+      linha.classList.add(NOME_CLASSE_CONTAGEM_ZERADA);
+    else
+      for (const celula_vazia of celulas_vazias)
+        celula_vazia.classList.add(NOME_CLASSE_CONTAGEM_ZERADA);
+  }
+  function parse_linha_com_mais_de_duas_celulas(qtd) {
+    return linha => {
+      return linha.cells.length === qtd ? linha : null;
+    };
+  }
+  function parse_maior_que_dois(value) {
+    return value > 2 ? value : null;
+  }
+  function parse_non_empty(xs) {
+    return xs.length > 0 ? xs : null;
+  }
+  function obter_linhas_rotulo_valor(context) {
     return [
-      ...document.querySelectorAll(
+      ...context.querySelectorAll(
         'tr:has(> td:first-child.label + td:last-child)'
       ),
     ].flatMap(linha => {
       const celula = linha.cells[1];
       const { texto, contem_scripts } = extrair_dados_celula(celula);
       const qtd = texto.trim().match(/^\d+$/);
-      if (qtd === null) {
-        if (contem_scripts) {
-          return [{ linha, celulas: [{ celula, qtd: -1, contem_scripts }] }];
-        } else {
-          return [];
-        }
-      } else {
+      if (qtd === null)
+        if (contem_scripts)
+          return [
+            {
+              linha,
+              celulas: [
+                {
+                  celula,
+                  qtd: -1,
+                  contem_scripts,
+                },
+              ],
+            },
+          ];
+        else return [];
+      else
         return [
-          { linha, celulas: [{ celula, qtd: Number(qtd[0]), contem_scripts }] },
+          {
+            linha,
+            celulas: [
+              {
+                celula,
+                qtd: Number(qtd[0]),
+                contem_scripts,
+              },
+            ],
+          },
         ];
-      }
     });
   }
   function extrair_dados_celula(celula) {
     if (celula.querySelector('script') === null)
-      return { texto: celula.textContent, contem_scripts: false };
-    else {
-      const texto = [...celula.childNodes]
-        .filter(
-          node =>
-            !(node instanceof HTMLScriptElement) && !(node instanceof Comment)
-        )
-        .map(x => x.textContent ?? '')
-        .join('');
-      return { texto, contem_scripts: true };
-    }
+      return {
+        texto: celula.textContent,
+        contem_scripts: false,
+      };
+    else
+      return {
+        texto: [...celula.childNodes]
+          .filter(
+            node =>
+              !(node instanceof HTMLScriptElement) && !(node instanceof Comment)
+          )
+          .map(x => x.textContent ?? '')
+          .join(''),
+        contem_scripts: true,
+      };
   }
-  function obter_tabelas_outros_cumprimentos() {
-    return [...document.querySelectorAll('table.resultTable')]
-      .map(tabela => {
-        const from_nullish = x => (x == null ? [] : [x]);
-        const first = xs => xs[0] ?? null;
-        const colunas = first(
-          from_nullish(tabela.tHead)
-            .map(thead => thead.rows)
-            .filter(linhas2 => linhas2.length === 1)
-            .map(linhas2 => linhas2[0])
-            .map(primeira_linha => primeira_linha.cells.length)
-            .filter(qtd => qtd > 2)
-        );
-        if (colunas === null) return null;
-        const tBody = first(
-          [tabela.tBodies]
-            .filter(tbodies => tbodies.length === 1)
-            .map(tb => tb[0])
-        );
-        if (tBody === null) return null;
-        const linhas = [...tBody.rows].filter(
-          linha => linha.cells.length === colunas
-        );
-        const linha_total = linhas
-          .slice(-1)
-          .map(linha => {
-            if (linha.querySelector('a[href]') !== null) return null;
-            const [primeira, ...outras] = linha.cells;
-            if (primeira.textContent.trim() !== 'Total') return null;
-            return { linha, outras };
-          })
-          .filter(x => x !== null);
-        if (linha_total.length === 0) return null;
-        const linhas_dados = linhas
-          .slice(0, -1)
-          .map(linha => {
-            const [primeira, ...outras] = linha.cells;
-            if (primeira.querySelector('a[href]') !== null) return null;
-            if (!outras.some(c => c.querySelector('a[href]') !== null)) {
-              return null;
-            }
-            return { linha, outras };
-          })
-          .filter(x => x !== null);
-        if (linhas_dados.length === 0) return null;
-        return {
-          tabela,
-          linhas: linhas_dados
-            .concat(linha_total)
-            .map(({ linha, outras }) => {
-              const celulas = outras
-                .map(celula => {
-                  const texto = celula.textContent.trim();
-                  let match;
-                  if ((match = texto.match(/^\d+$/)) !== null) {
-                    return {
-                      celula,
-                      qtd: Number(match[0]),
-                      contem_scripts: false,
-                    };
-                  } else if (
-                    (match = texto.match(/^(\d+)\s+\/\s+(\d+)$/)) !== null
-                  ) {
-                    return {
-                      celula,
-                      qtd: Number(match[1]) + Number(match[2]),
-                      contem_scripts: false,
-                    };
-                  } else {
-                    return null;
-                  }
-                })
-                .filter(x => x !== null);
-              if (celulas.length === 0) return null;
-              return { linha, celulas };
-            })
-            .filter(x => x !== null),
-        };
-      })
-      .filter(x => x !== null);
+  function obter_tabelas_outros_cumprimentos(context) {
+    return (
+      single_element(
+        [...context.querySelectorAll('table.resultTable')]
+          .map(extrair_dados_tabela_cumprimentos)
+          .filter(x => x !== null)
+      )?.linhas ?? null
+    );
+  }
+  function extrair_dados_tabela_cumprimentos(tabela) {
+    const colunas = parse_thead(tabela);
+    if (colunas === null) return null;
+    const secoes = parse_tbodies(colunas)(tabela);
+    if (secoes === null) return null;
+    const footer = parse_footer(secoes.footer);
+    if (footer === null) return null;
+    const linhas_dados = secoes.linhas.map(parse_linha).filter(x => x !== null);
+    if (linhas_dados.length === 0) return null;
+    return {
+      tabela,
+      linhas: linhas_dados
+        .concat([footer])
+        .map(parse_linha_dados)
+        .filter(x => x !== null),
+    };
+  }
+  function parse_thead(tabela) {
+    return (
+      maybe(tabela.tHead)
+        .map(t => t.rows)
+        .safeMap(single_element)
+        .map(r => r.cells.length)
+        .safeMap(parse_maior_que_dois).value ?? null
+    );
+  }
+  function parse_tbodies(colunas) {
+    return tabela => {
+      const tbody = single_element(tabela.tBodies);
+      if (!tbody) return null;
+      const linhas = combine(
+        [...tbody.rows]
+          .map(parse_linha_com_mais_de_duas_celulas(colunas))
+          .map(maybe)
+      );
+      if (linhas.empty) return null;
+      if (linhas.value.length < 2) return null;
+      return {
+        linhas: linhas.value.slice(0, -1),
+        footer: linhas.value.slice(-1)[0],
+      };
+    };
+  }
+  function parse_linha(linha) {
+    const [primeira, ...outras] = linha.cells;
+    if (primeira.querySelector('a[href]') !== null) return null;
+    if (!outras.some(c => c.querySelector('a[href]') !== null)) return null;
+    return {
+      linha,
+      celulas: outras,
+    };
+  }
+  function parse_footer(linha) {
+    if (linha.querySelector('a[href]') !== null) return null;
+    const [primeira, ...outras] = linha.cells;
+    if (primeira.textContent.trim() !== 'Total') return null;
+    return {
+      linha,
+      celulas: outras,
+    };
+  }
+  function parse_linha_dados(dados) {
+    const celulas = dados.celulas.map(parse_celula).filter(x => x !== null);
+    if (celulas.length === 0) return null;
+    return {
+      linha: dados.linha,
+      celulas,
+    };
+  }
+  function parse_celula(celula) {
+    const texto = celula.textContent.trim();
+    let match;
+    if ((match = texto.match(/^\d+$/)) !== null)
+      return {
+        celula,
+        qtd: Number(match[0]),
+        contem_scripts: false,
+      };
+    else if ((match = texto.match(/^(\d+)\s+\/\s+(\d+)$/)) !== null)
+      return {
+        celula,
+        qtd: Number(match[1]) + Number(match[2]),
+        contem_scripts: false,
+      };
+    else return null;
+  }
+  function single_element(xs) {
+    if (xs.length === 1) return xs[0];
+    return null;
   }
   function common_ancestor(nodes) {
     const [first, ...rest] = [...nodes];
@@ -201,16 +297,14 @@
     range.setStart(first, 0);
     for (const curr of rest) {
       range.setEnd(curr, 0);
-      if (range.collapsed) {
-        reverse(range);
-      }
+      if (range.collapsed) reverse(range);
       range.setStart(range.commonAncestorContainer, 0);
     }
     return range.startContainer;
-    function reverse(range2) {
-      const temp = range2.startContainer;
-      range2.setStart(range2.endContainer, 0);
-      range2.setEnd(temp, 0);
+    function reverse(range) {
+      const temp = range.startContainer;
+      range.setStart(range.endContainer, 0);
+      range.setEnd(temp, 0);
     }
   }
   function debounce(fn, timeout_ms = 200) {
