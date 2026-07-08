4bc1ff94b217a0dfe3ccffb3d3478a0f17c3b869
Paulo R. Maurici Jr. <paulo@nadameu.com.br>
2026-07-08 03:45:32 -0300

    State machine

--- userscripts/eproc-area-de-trabalho-exportar/src/parseAreaDeTrabalho.ts
+++ userscripts/eproc-area-de-trabalho-exportar/src/parseAreaDeTrabalho.ts
@@ -1,99 +1,95 @@
-import { GM } from '$';
+import { GM_deleteValue, GM_setValue } from '$';
 import { ZipWriter } from '@zip.js/zip.js';
-import { err, ok } from './Result';
+import { is_mensagem, Mensagem } from './Mensagem';
+import { map2, ok } from './Result';
 import { queryUnique } from './Result/functions';
 import classes from './area_de_trabalho.module.css';
 import { lift_throwable } from './try_catch';
 
+type State =
+  | { _tag: 'Init'; init(): void }
+  | { _tag: 'Ocioso'; onminutasclicadas(): void; onbotaoclicado(): void }
+  | {
+      _tag: 'Aguarda_resposta';
+      onmessage(_: { data: unknown }): void;
+      ontimeout(): void;
+    };
+
 export function parseAreaDeTrabalho() {
   return ok(null).chain(() => {
-    let imprimir: HTMLButtonElement;
-    {
-      const resultado = ok(document).chain(
-        queryUnique<HTMLButtonElement>('button[id="btnImprimir"]')
-      );
-      if (!resultado.ok) return err(resultado.reason);
-      else imprimir = resultado.value;
-    }
+    const imprimir = ok(document).chain(
+      queryUnique<HTMLButtonElement>('button[id="btnImprimir"]')
+    );
 
-    let tabela: HTMLTableElement;
-    {
-      const resultado = ok(document).chain(
-        queryUnique<HTMLTableElement>('table[id="tabelaMinutas"]')
-      );
-      if (!resultado.ok) return err(resultado.reason);
-      else tabela = resultado.value;
-    }
+    const tabela = ok(document).chain(
+      queryUnique<HTMLTableElement>('table[id="tabelaMinutas"]')
+    );
 
-    return ok(() => {
-      const salvar = Object.assign(document.createElement('button'), {
-        className: 'infraButton ' + classes.button,
-        type: 'button',
-        textContent: 'Fazer download das minutas selecionadas',
-        disabled: true,
-      });
-      habilitar_botao_se_minutas_selecionadas();
-      salvar.addEventListener(
-        'click',
-        lift_throwable(async () => {
-          salvar.disabled = true;
-          await GM.setValue('salvar', true);
-          imprimir.click();
-        })
-      );
+    return map2(tabela, imprimir, (tabela, imprimir) => () => {
+      const salvar = criar_botao();
       imprimir.after(' ', salvar);
-      tabela.addEventListener('click', () => {
-        habilitar_botao_se_minutas_selecionadas();
-      });
-      window.addEventListener('message', async ({ data }) => {
-        try {
-          if (!(Array.isArray(data) && data.every(s => typeof s === 'string')))
-            return;
-          // Creates a TransformStream object, the zip content will be written in the
-          // `writable` property.
-          const zipFileStream = new TransformStream();
-          // Creates a Promise object resolved to the zip content returned as a Blob
-          // object retrieved from `zipFileStream.readable`.
-          const zipFileBlobPromise = new Response(
-            zipFileStream.readable
-          ).blob();
 
-          // Creates a ZipWriter object writing data into `zipFileStream.writable`, adds
-          // the entry "hello.txt" containing the text "Hello world!" retrieved from
-          // `helloWorldReadable`, and closes the writer.
-          const zipWriter = new ZipWriter(zipFileStream.writable);
-          for (const [i, minuta] of data.entries()) {
-            await zipWriter.add(
-              `minuta_${String(i + 1).padStart(3, '0')}.html`,
-              new Blob([minuta]).stream()
-            );
+      let state: State = (() => {
+        const init: State = {
+          _tag: 'Init',
+          init() {
+            habilitar_botao_se_minutas_selecionadas();
+            state = ocioso;
+          },
+        };
+        const ocioso: State = {
+          _tag: 'Ocioso',
+          onminutasclicadas: habilitar_botao_se_minutas_selecionadas,
+          onbotaoclicado() {
+            state = {
+              _tag: 'Aguarda_resposta',
+              onmessage: lift_throwable(async ({ data }) => {
+                window.clearTimeout(timer);
+                GM_deleteValue('salvar');
+                if (is_mensagem(data) && data.segredo === segredo) {
+                  const url = await gerar_zip(data);
+                  if (window.confirm('Fazer download?')) {
+                    window.open(url);
+                  }
+                } else {
+                  console.debug('Mensagem recebida:', data);
+                }
+                habilitar_botao_se_minutas_selecionadas();
+                state = ocioso;
+              }),
+              ontimeout() {
+                window.clearTimeout(timer);
+                GM_deleteValue('salvar');
+                habilitar_botao_se_minutas_selecionadas();
+                state = ocioso;
+              },
+            };
+            salvar.disabled = true;
+            const segredo = Math.random().toString(36).slice(2);
+            GM_setValue('salvar', segredo);
+            const timer = window.setTimeout(() => {
+              if (state._tag === 'Aguarda_resposta') {
+                state.ontimeout();
+              }
+            }, 30_000);
+            imprimir.click();
+          },
+        };
+        tabela.addEventListener('click', () => {
+          if (state._tag === 'Ocioso') state.onminutasclicadas();
+        });
+        salvar.addEventListener('click', () => {
+          if (state._tag === 'Ocioso') state.onbotaoclicado();
+        });
+        window.addEventListener('message', evt => {
+          if (state._tag === 'Aguarda_resposta') {
+            state.onmessage(evt);
           }
-          /*
-          await Promise.all(
-            data.map((minuta, i) =>
-              zipWriter.add(
-                `minuta_${String(i + 1).padStart(3, '0')}.html`,
-                new Blob([minuta]).stream()
-              )
-            )
-          );
-          */
-          await zipWriter.close();
-          console.log('created');
+        });
+        return init;
+      })();
+      state.init();
 
-          // Retrieves the Blob object containing the zip content into `zipFileBlob`.
-          const zipFileBlob = await zipFileBlobPromise;
-
-          if (window.confirm('Fazer download?')) {
-            console.log('Mensagem recebida:', zipFileBlob);
-            window.open(
-              URL.createObjectURL(new File([zipFileBlob], 'minutas.zip'))
-            );
-          }
-        } catch (err) {
-          console.log(err);
-        }
-      });
       function habilitar_botao_se_minutas_selecionadas() {
         const selecionados = tabela.querySelectorAll<HTMLInputElement>(
           'input[type="checkbox"]:checked'
@@ -102,4 +98,41 @@ export function parseAreaDeTrabalho() {
       }
     });
   });
+}
+
+function criar_botao(): HTMLButtonElement {
+  return Object.assign(document.createElement('button'), {
+    className: 'infraButton ' + classes.button,
+    type: 'button',
+    textContent: 'Fazer download das minutas selecionadas',
+    disabled: true,
+  });
+}
+
+async function gerar_zip(mensagem: Mensagem) {
+  const zipFileStream = new TransformStream();
+  const zipFileBlobPromise = new Response(zipFileStream.readable).blob();
+  const zipWriter = new ZipWriter(zipFileStream.writable);
+  await Promise.all(
+    mensagem.minutas.map(async minuta => {
+      const nome = `${minuta.titulo}_${minuta.codigo}`;
+      const nome_arquivo = `${nome}.html`;
+      const codigo_html = `<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>${nome}</title>
+${mensagem.estilos.join('\n')}
+</head>
+<body>
+${minuta.html}    
+</body>
+</html>`;
+      await zipWriter.add(nome_arquivo, new Blob([codigo_html]).stream());
+    })
+  );
+  await zipWriter.close();
+  const zipFileBlob = await zipFileBlobPromise;
+  return URL.createObjectURL(new File([zipFileBlob], 'minutas.zip'));
 }
