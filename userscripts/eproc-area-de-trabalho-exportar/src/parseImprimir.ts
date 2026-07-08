a0d7e54827ec16675c5144bd1c4c7e8834fcafd1
Paulo R. Maurici Jr. <paulo@nadameu.com.br>
2026-07-08 02:55:19 -0300

    Protocolo

--- userscripts/eproc-area-de-trabalho-exportar/src/parseImprimir.ts
+++ userscripts/eproc-area-de-trabalho-exportar/src/parseImprimir.ts
@@ -1,18 +1,47 @@
-import { GM_getValue, GM_setValue } from '$';
+import { GM_deleteValue, GM_getValue } from '$';
+import { Mensagem } from './Mensagem';
 import { ok } from './Result';
 
 export function parseImprimir() {
   return ok(null).chain(() => {
-    if (GM_getValue<boolean>('salvar', false)) {
-      GM_setValue('salvar', false);
+    const segredo = GM_getValue('salvar') as string | undefined;
+    GM_deleteValue('salvar');
+    if (segredo !== undefined) {
       window.print = () => undefined;
 
-      const articles = document
+      const estilos = [
+        ...document.querySelectorAll<HTMLLinkElement>(
+          'link[rel="stylesheet"][href^="css/estilos-editor"]'
+        ),
+      ].map(e => {
+        const clone = e.cloneNode(true) as HTMLLinkElement;
+        clone.href = new URL(clone.href, document.location.href).href;
+        return clone.outerHTML;
+      });
+
+      const minutas = document
         .querySelectorAll<HTMLElement>('#toPrint > #Body > #Content > article')
         .values()
-        .map(x => x.innerHTML)
+        .map((article, index) => {
+          const titulo =
+            article
+              .querySelector<HTMLElement>(
+                'section[data-nome="titulo"] + section'
+              )
+              ?.dataset.nome_apresentacao?.replace(/(\/|\s|-)+/g, '_') ??
+            'Minuta';
+          const codigo =
+            article.querySelector<HTMLElement>(
+              'footer span[data-codigo_documento_rodape]'
+            )?.dataset.codigo_documento_rodape ??
+            (index + 1).toString().padStart(12, '0');
+          const html = article.innerHTML;
+          return { titulo, codigo, html };
+        })
         .toArray();
-      window.opener?.postMessage(articles);
+      const mensagem: Mensagem = { segredo, estilos, minutas };
+
+      window.opener?.postMessage(mensagem);
       window.close();
     }
     return ok(() => {});
